80c1807eea95a67b6c00008a0d9a861f222e47ca
Balazs Racz <balazs.racz@gmail.com>
2013-04-27 17:23:32 +0200

    Adds alternate armgcc toolchains.

--- etc/armgcc.mk
+++ etc/armgcc.mk
@@ -1,6 +1,8 @@
 TOOLPATH ?= $(shell \
 sh -c "if [ \"X`printenv TOOLPATH`\" != \"X\" ]; then printenv TOOLPATH; \
      elif [ -d /opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin ]; then echo /opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI; \
+     elif [ -d /opt/armgcc/default/bin ]; then echo /opt/armgcc/default; \
+     elif [ -d /usr/local/lpcxpresso_*/lpcxpresso/tools/bin ]; then ls -d /usr/local/lpcxpresso_*/lpcxpresso/tools | tail -n 1; \
      else echo TOOLPATH not found; fi" \
 )
 
