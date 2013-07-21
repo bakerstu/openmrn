e4033417cde55fbe4e1e906f2ee25e13b14766df
Balazs Racz <balazs.racz@gmail.com>
2013-07-21 16:40:05 +0200

    Makes stellarisware be part of the dependency detection system.

--- etc/path.mk
+++ etc/path.mk
@@ -27,6 +27,18 @@ findfirst=$(firstword $(foreach dir,$(2),$(if $(wildcard $(dir)/$(1)),$(wildcard
 # endif
 find_missing_deps=$(strip $(foreach depvar,$(1),$(if $(value $(depvar)),,$(depvar))))
 
+################ stellarisware ##################
+ifndef STELLARISWAREPATH
+SEARCHPATH := \
+  /opt/StellarisWare \
+  $(HOME)/StellarisWare
+
+TRYPATH:=$(call findfirst,driverlib,$(SEARCHPATH))
+ifneq ($(TRYPATH),)
+STELLARISWAREPATH:=$(TRYPATH)
+endif
+endif #FREERTOSPATH
+
 ################# mbed library ##################
 
 ifndef MBEDPATH
