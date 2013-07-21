685cbc5d84b70e7d451ff01c09892efd06de9f03
Balazs Racz <balazs.racz@gmail.com>
2013-07-21 17:00:41 +0200

    Adds two new core subdirectories, for utilities and the future code of the "cue node" implementation.

--- etc/config.mk
+++ etc/config.mk
@@ -1,10 +1,10 @@
 ifndef MAKE_INC_CONFIG_MK
 MAKE_INC_CONFIG_MK := 1
 
-#CORELIBS := utils os if core cue
-CORELIBS := os if core
+CORELIBS := utils os if core cue
+#CORELIBS := os if core
 
-#LINKCORELIBS = -lcue -lif -lcore  $(STARTGROUP) -lutils -los $(ENDGROUP)
-LINKCORELIBS = -lif -lcore  $(STARTGROUP) -los $(ENDGROUP)
+LINKCORELIBS = -lcue -lif -lcore  $(STARTGROUP) -lutils -los $(ENDGROUP)
+#LINKCORELIBS = -lif -lcore  $(STARTGROUP) -los $(ENDGROUP)
 
 endif # MAKE_INC_CONFIG_MK
