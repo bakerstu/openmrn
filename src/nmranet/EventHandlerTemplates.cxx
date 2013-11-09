75d4550de74d19114118c8fc8f2ce3a560f17289
Balazs Racz <balazs.racz@gmail.com>
2013-11-09 16:41:02 +0100

    Adds a common event handler for exporting a bitmap on pairs of events.

---
+++ src/nmranet/EventHandlerTemplates.cxx
@@ -0,0 +1,176 @@
+
+/** \copyright
+ * Copyright (c) 2013, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file EventHandlerTemplates.cxx
+ *
+ * Implementations of common event handlers.
+ *
+ * @author Balazs Racz
+ * @date 6 November 2013
+ */
+
+#include "nmranet/EventHandlerTemplates.hxx"
+#include "if/nmranet_if.h"  // for MTI values
+
+BitRangeEventPC::BitRangeEventPC(WriteHelper::node_type node,
+                                 uint64_t event_base,
+                                 uint32_t* backing_store,
+                                 unsigned size)
+    : event_base_(event_base), node_(node), data_(backing_store), size_(size) {}
+
+void BitRangeEventPC::GetBitAndMask(unsigned bit,
+                                    uint32_t** data,
+                                    uint32_t* mask) const {
+  *data = nullptr;
+  if ((bit >= size_) || (bit < 0))
+    return;
+  *data = data_ + (bit >> 5);
+  *mask = 1 << (bit & 31);
+}
+
+bool BitRangeEventPC::Get(unsigned bit) const {
+  uint32_t* ofs;
+  uint32_t mask;
+  GetBitAndMask(bit, &ofs, &mask);
+  if (!ofs)
+    return false;
+  return (*ofs) & mask;
+}
+
+void BitRangeEventPC::Set(unsigned bit,
+                          bool new_value,
+                          WriteHelper* writer,
+                          Notifiable* done) {
+  uint32_t* ofs;
+  uint32_t mask;
+  GetBitAndMask(bit, &ofs, &mask);
+  bool old_value = new_value;
+  if (ofs)
+    old_value = (*ofs) & mask;
+  if (old_value != new_value) {
+    uint64_t event = event_base_ + bit * 2;
+    if (!new_value)
+      event++;
+    writer->WriteAsync(node_,
+                       MTI_EVENT_REPORT,
+                       WriteHelper::Global(),
+                       EventIdToBuffer(event),
+                       done);
+  } else {
+    if (done)
+      done->Notify();
+  }
+}
+
+void BitRangeEventPC::HandleEventReport(EventReport* event, Notifiable* done) {
+  done->Notify();
+  if (event->event < event_base_)
+    return;
+  uint64_t d = (event->event - event_base_);
+  bool new_value = !(d & 1);
+  d >>= 1;
+  if (d >= size_)
+    return;
+  uint32_t* ofs;
+  uint32_t mask;
+  GetBitAndMask(d, &ofs, &mask);
+  if (new_value) {
+    *ofs |= mask;
+  } else {
+    *ofs &= ~mask;
+  }
+}
+
+void BitRangeEventPC::HandleIdentifyProducer(EventReport* event,
+                                             Notifiable* done) {
+  HandleIdentifyBase(MTI_PRODUCER_IDENTIFIED_VALID, event, done);
+}
+
+void BitRangeEventPC::HandleIdentifyConsumer(EventReport* event,
+                                             Notifiable* done) {
+  HandleIdentifyBase(MTI_CONSUMER_IDENTIFIED_VALID, event, done);
+}
+void BitRangeEventPC::HandleIdentifyBase(int mti_valid,
+                                         EventReport* event,
+                                         Notifiable* done) {
+  if (event->event < event_base_)
+    return done->Notify();
+  uint64_t d = (event->event - event_base_);
+  bool new_value = !(d & 1);
+  d >>= 1;
+  if (d >= size_)
+    return done->Notify();
+  uint32_t* ofs;
+  uint32_t mask;
+  GetBitAndMask(d, &ofs, &mask);
+  int mti;
+  bool old_value = *ofs & mask;
+  if (old_value == new_value) {
+    mti = MTI_PRODUCER_IDENTIFIED_VALID;
+  } else {
+    mti = MTI_PRODUCER_IDENTIFIED_INVALID;
+  }
+
+  event_write_helper1.WriteAsync(
+      node_, mti, WriteHelper::Global(), EventIdToBuffer(event->event), done);
+}
+
+uint64_t EncodeRange(uint64_t begin, unsigned size) {
+  // We assemble a valid event range identifier that covers our block.
+  uint64_t end = begin + size * 2;
+  uint64_t shift = 1;
+  while (begin + shift <= end) {
+    begin &= ~shift;
+    shift <<= 1;
+  }
+  shift <<= 1;
+  if (begin & shift) {
+    // last real bit is 1 => range ends with zero.
+    return begin;
+  } else {
+    // last real bit is zero. Set all lower bits to 1.
+    begin |= shift - 1;
+    return begin;
+  }
+}
+
+void BitRangeEventPC::HandleIdentifyGlobal(EventReport* event,
+                                           Notifiable* done) {
+  uint64_t range = EncodeRange(event_base_, size_);
+  event_barrier.Reset(done);
+  event_write_helper1.WriteAsync(node_,
+                                 MTI_PRODUCER_IDENTIFIED_RANGE,
+                                 WriteHelper::Global(),
+                                 EventIdToBuffer(range),
+                                 event_barrier.NewChild());
+  event_write_helper2.WriteAsync(node_,
+                                 MTI_CONSUMER_IDENTIFIED_RANGE,
+                                 WriteHelper::Global(),
+                                 EventIdToBuffer(range),
+                                 event_barrier.NewChild());
+  event_barrier.MaybeDone();
+}
--- src/nmranet/EventHandlerTemplates.hxx
+++ src/nmranet/EventHandlerTemplates.hxx
@@ -30,13 +30,14 @@
  * multiple event handler types.
  *
  * @author Balazs Racz
- * @date 19 Octover 2013
+ * @date 19 October 2013
  */
 
 #ifndef _NMRAnet_EventHandlerTemplates_hxx_
 #define _NMRAnet_EventHandlerTemplates_hxx_
 
 #include "nmranet/NMRAnetEventRegistry.hxx"
+#include "nmranet/WriteFlow.hxx"
 
 typedef void (NMRAnetEventHandler::*EventHandlerFunction)(EventReport* event,
                                                           Notifiable* done);
@@ -47,7 +48,6 @@ typedef void (NMRAnetEventHandler::*EventHandlerFunction)(EventReport* event,
 class ProxyEventHandler : public NMRAnetEventHandler {
  public:
   virtual ~ProxyEventHandler() {}
-
 
   // This function will be called for any other incoming event handler
   // function.
@@ -98,14 +98,22 @@ class BitEventProducer : public SimpleEventHandler {
 
   // Requests the event associated with the current value of the bit to be
   // produced (unconditionally).
-  void Update();
+  //
+  // @param node specifies the source node from which to produce the event.
+  //
+  // @param writer is the output flow to be used.
+  //
+  // @param done is the notification callback. If it is NULL, the writer will
+  // be invoked inline and potentially block the calling thread.
+  void Update(WriteHelper::node_type node,
+              WriteHelper* writer,
+              Notifiable* done);
 
   virtual void HandleIdentifyGlobal(EventReport* event, Notifiable* done);
 
   // Called on another node sending IdentifyProducer. Filled: src_node, event,
   // mask=1. Not filled: state.
-  virtual void HandleIdentifyProducer(EventReport* event,
-                                      Notifiable* done);
+  virtual void HandleIdentifyProducer(EventReport* event, Notifiable* done);
 
  private:
   uint64_t event_on_;
@@ -127,12 +135,57 @@ class BitEventConsumer : public SimpleEventHandler {
 
   virtual void HandleIdentifyGlobal(EventReport* event, Notifiable* done);
 
-  virtual void HandleIdentifyConsumer(EventReport* event,
-                                      Notifiable* done);
+  virtual void HandleIdentifyConsumer(EventReport* event, Notifiable* done);
 
  private:
   uint64_t event_on_;
   uint64_t event_off_;
+};
+
+class BitRangeEventPC : public SimpleEventHandler {
+ public:
+  // Creates a new bit range listener. backing store points to memory of at
+  // least size bits (round up to multiple of 32). This class will advertise
+  // producing and consuming size * 2 events contiguous from
+  // event_base. event_base will turn bit 0 on, event_base + 1 will turn bit 0
+  // off, event_base + 2 will turn bit 1 on, event_base + 3 will turn bit 1
+  // off, etc.
+  BitRangeEventPC(WriteHelper::node_type node,
+                  uint64_t event_base,
+                  uint32_t* backing_store,
+                  unsigned size);
+
+  // Requests the event associated with the current value of the bit to be
+  // produced (unconditionally).
+  //
+  // @param node specifies the source node from which to produce the event.
+  //
+  // @param bit is the offset of the bit to set (0 <= bit < size)
+  //
+  // @param new_value is the new value of the bit
+  //
+  // @param writer is the output flow to be used.
+  //
+  // @param done is the notification callback. If it is NULL, the writer will
+  // be invoked inline and potentially block the calling thread.
+  void Set(unsigned bit, bool new_value, WriteHelper* writer, Notifiable* done);
+
+  //! @returns the value of a given bit. 0 <= bit < size_.
+  bool Get(unsigned bit) const;
+
+  virtual void HandleEventReport(EventReport* event, Notifiable* done);
+  virtual void HandleIdentifyProducer(EventReport* event, Notifiable* done);
+  virtual void HandleIdentifyConsumer(EventReport* event, Notifiable* done);
+  virtual void HandleIdentifyGlobal(EventReport* event, Notifiable* done);
+
+ private:
+  void HandleIdentifyBase(int mti_valid, EventReport* event, Notifiable* done);
+  void GetBitAndMask(unsigned bit, uint32_t** data, uint32_t* mask) const;
+
+  uint64_t event_base_;
+  WriteHelper::node_type node_;
+  uint32_t* data_;
+  unsigned size_;
 };
 
 #endif  // _NMRAnet_EventHandlerTemplates_hxx_
