12d32449dfe999f3fd8a515ee8ce8edec78cf685
Balazs Racz <balazs.racz@gmail.com>
2013-12-03 14:42:10 +0100

    Fixes if_test_test for the c++ api.

--- test/tests/if_test_test.cc
+++ test/tests/if_test_test.cc
@@ -3,14 +3,16 @@
 #include "utils/if_test_helper.hxx"
 
 #include "nmranet/NMRAnetEventRegistry.hxx"
-#include "if/nmranet_if.h"
+#include "nmranet/NMRAnetIf.hxx"
+
+namespace NMRAnet {
 
 TEST_F(IfTest, Setup) {}
 
 TEST_F(IfTest, WriteMessageSync) {
   // We write a message using the WriteFlow class directly into the interface.
   ExpectPacket(":X195B412DN0102030405060708;");
-  event_write_helper1.WriteAsync(node_, MTI_EVENT_REPORT, WriteHelper::Global(),
+  event_write_helper1.WriteAsync(node_, If::MTI_EVENT_REPORT, WriteHelper::global(),
                                  EventIdToBuffer(0x0102030405060708ULL),
                                  nullptr);
 }
@@ -19,7 +21,7 @@ TEST_F(IfTest, WriteMessageASync) {
   // We write a message using the WriteFlow class asynchronously.
   ExpectPacket(":X195B412DN0102030405060708;");
   SyncNotifiable n;
-  event_write_helper1.WriteAsync(node_, MTI_EVENT_REPORT, WriteHelper::Global(),
+  event_write_helper1.WriteAsync(node_, If::MTI_EVENT_REPORT, WriteHelper::global(),
                                  EventIdToBuffer(0x0102030405060708ULL),
                                  &n);
   n.WaitForNotification();
@@ -28,5 +30,7 @@ TEST_F(IfTest, WriteMessageASync) {
 TEST_F(IfTest, ReadMessageAndReply) {
   // We send an alias mapping enquiry frame and expect the node ID back.
   ExpectPacket(":X1070112DN02010d000003;");
-  SendPacket(  ":X10702000N;");
+  SendPacket(  ":X10702001N;");
 }
+
+}  // namespace NMRAnet
