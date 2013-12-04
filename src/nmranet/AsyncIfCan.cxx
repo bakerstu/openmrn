f78891f76bc86b19c0c39dc592bff90161d69722
Balazs Racz <balazs.racz@gmail.com>
2013-12-04 23:37:39 +0100

    Adds part of the implementation for the translation of messages into can frames.

--- src/nmranet/AsyncIfCan.cxx
+++ src/nmranet/AsyncIfCan.cxx
@@ -33,6 +33,9 @@
  */
 
 #include "nmranet/AsyncIfCan.hxx"
+
+#include "nmranet/AsyncIfImpl.hxx"
+#include "nmranet/NMRAnetIfCan.hxx"
 #include "nmranet_can.h"
 
 namespace NMRAnet
@@ -130,6 +133,124 @@ private:
 
     //! Parent interface. Owned externlly.
     AsyncIfCan* if_can_;
+};
+
+/** Implements the write-side conversion logic from generic messages to CAN
+ * frames. */
+class CanMessageWriteFlow : public WriteFlowBase
+{
+public:
+    CanMessageWriteFlow(AsyncIfCan* if_can, Executor* e, Notifiable* done)
+        : WriteFlowBase(e, done), if_can_(if_can)
+    {
+    }
+
+protected:
+    /// @returns the allocator that this flow belongs to.
+    virtual TypedAllocator<WriteFlow>* allocator() = 0;
+
+    AsyncIfCan* if_can_; ///< The physical interface.
+
+    unsigned src_alias_ : 12;  ///< Source node alias.
+    unsigned dst_alias_ : 12;  ///< Destination node alias.
+    unsigned data_offset_ : 8; /**< for continuation frames: which offset in
+                                * the Buffer should we start the payload at. */
+
+private:
+    virtual ControlFlowAction send_to_hardware()
+    {
+        data_offset_ = 0;
+        src_alias_ = 0;
+        dst_alias_ = 0;
+        if (data_)
+        {
+            // We have limited space for counting offsets. In practice this
+            // valaue will be max 10 for certain traction control protocol
+            // values. Longer data usually travels via datagrams or streams.
+            HASSERT(data_->used() < 256);
+        }
+        // @TODO(balazs.racz): need to add alias lookups.
+        return CallImmediately(ST(get_can_frame_buffer));
+    }
+
+    ControlFlowAction get_can_frame_buffer()
+    {
+        return Allocate(if_can_->write_allocator(), ST(fill_can_frame_buffer));
+    }
+
+    ControlFlowAction fill_can_frame_buffer()
+    {
+        CanFrameWriteFlow* write_flow;
+        GetAllocationResult(&write_flow);
+        struct can_frame* f = write_flow->mutable_frame();
+        if (mti_ & (If::MTI_DATAGRAM_MASK | If::MTI_SPECIAL_MASK |
+                    If::MTI_RESERVED_MASK))
+        {
+            // We don't know how to handle such an MTI in a generic way.
+            write_flow->Cancel();
+            return CallImmediately(ST(finalize));
+        }
+        HASSERT(!(mti_ & ~0xfff));
+
+        // Sets the CAN id.
+        uint32_t can_id = 0;
+        IfCan::set_fields(&can_id, src_alias_, mti_,
+                                 IfCan::GLOBAL_ADDRESSED, IfCan::NMRANET_MSG,
+                                 IfCan::NORMAL_PRIORITY);
+        SET_CAN_FRAME_ID_EFF(*f, can_id);
+
+        bool need_more_frames = false;
+        // Sets the destination bytes if needed. Adds the payload.
+        if (If::get_mti_address(mti_))
+        {
+            f->data[0] = dst_alias_ >> 8;
+            f->data[1] = dst_alias_ & 0xff;
+            if (data_)
+            {
+                if (data_offset_)
+                {
+                    // This is not the first frame.
+                    f->data[1] |= 0x20;
+                }
+                uint8_t* b = static_cast<uint8_t*>(data_->start());
+                unsigned len = data_->used() - data_offset_;
+                if (len > 6)
+                {
+                    len = 6;
+                    // This is not the last frame.
+                    need_more_frames = true;
+                    f->data[1] |= 0x10;
+                }
+                memcpy(f->data + 2, b + data_offset_, len);
+                data_offset_ += len;
+                f->can_dlc = 2 + len;
+            }
+        }
+        else
+        {
+            if (data_)
+            {
+                HASSERT(data_->used() <= 8);
+                memcpy(f->data, data_->start(), data_->used());
+                f->can_dlc = data_->used();
+            }
+        }
+        write_flow->Send(nullptr);
+        if (need_more_frames)
+        {
+            return CallImmediately(ST(get_can_frame_buffer));
+        }
+        else
+        {
+            return CallImmediately(ST(finalize));
+        }
+    }
+
+    ControlFlowAction finalize()
+    {
+        cleanup(); // Will release the buffer.
+        return ReleaseAndExit(allocator(), this);
+    }
 };
 
 AsyncIfCan::AsyncIfCan(Executor* executor, Pipe* device)
