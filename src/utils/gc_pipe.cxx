fb1a533574dd29679c729caeabc953409954b7b6
Balazs Racz <balazs.racz@gmail.com>
2013-12-12 01:58:49 +0100

    Starts fixing the gc-pipe control flow.

--- src/utils/gc_pipe.cxx
+++ src/utils/gc_pipe.cxx
@@ -4,7 +4,7 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are  permitted provided that the following conditions are met:
- * 
+ *
  *  - Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
@@ -36,7 +36,6 @@
 #include "utils/gc_format.h"
 #include "nmranet_can.h"
 
-
 class GCAdapter : public GCAdapterBase
 {
 public:
@@ -52,19 +51,20 @@ public:
     {
         parser_.destination()->UnregisterMember(&formatter_);
         formatter_.destination()->UnregisterMember(&parser_);
-    }    
+    }
 
 private:
-    class BinaryToGCMember : public PipeMember
+    class BinaryToGCMember : public PipeMember, public ControlFlow
     {
     public:
-        BinaryToGCMember(Pipe* destination,
-                         PipeMember* skip_member,
+        BinaryToGCMember(Pipe* destination, PipeMember* skip_member,
                          int double_bytes)
-            : destination_(destination),
+            : ControlFlow(destination->executor(), nullptr),
+              destination_(destination),
               skip_member_(skip_member),
               double_bytes_(double_bytes)
         {
+            StartFlowAt(ST(Terminated));
         }
 
         Pipe* destination()
@@ -72,24 +72,66 @@ private:
             return destination_;
         }
 
-        virtual void write(const void* buf, size_t count)
+        virtual void write_async(const void* buf, size_t count,
+                                 Notifiable* done)
         {
-            const struct can_frame* frame =
-                static_cast<const struct can_frame*>(buf);
-            char dbuf[56];
-            while (count >= sizeof(*frame))
+            HASSERT(IsDone());
+            buf_ = static_cast<const struct can_frame*>(buf);
+            byte_count_ = count;
+            Restart(done);
+            StartFlowAt(ST(next_frame));
+        }
+
+        ControlFlowAction next_frame()
+        {
+            if (byte_count_ < sizeof(struct can_frame))
             {
-                char* end = gc_format_generate(frame, dbuf, double_bytes_);
-                int len = (end - dbuf);
-                if (len) {
-                    destination_->WriteToAll(skip_member_, dbuf, len);
-                }
-                count -= sizeof(*frame);
-                frame++;
+                return Exit();
+            }
+
+            char* end = gc_format_generate(buf_, dbuf_, double_bytes_);
+            len_ = (end - dbuf_);
+
+            count_ -= sizeof(*buf_);
+            buf_++;
+
+            if (len)
+            {
+                return Allocate(destination_->allocator(),
+                                ST(handle_pipe_allocated));
+            }
+            else
+            {
+                return RetryCurrentState();
             }
         }
 
+        ControlFlowAction handle_pipe_allocated()
+        {
+            PipeBuffer* b = GetTypedAllocationResult(destination_->allocator());
+            b->data = &frame_;
+            b->size = sizeof(frame_);
+            b->skipMember = if_can_->pipe_member();
+            b->done = notifiable_.NewCallback(this);
+            destination_->SendBuffer(b);
+            return WaitAndCall(ST(wait_for_sent));
+        }
+
+        ControlFlowAction wait_for_sent()
+        {
+            if (!notifiable_.HasBeenNotified())
+                return WaitForNotification();
+            return CallImmediately(ST(next_frame));
+        }
+
     private:
+        ProxyNotifiable notifiable_;
+        const struct can_frame* buf_;
+        size_t byte_count_;
+        //! Destination buffer.
+        char dbuf_[56];
+        //! Number of valid bytes in dest buffer.
+        int len_;
         //! Pipe to send data to.
         Pipe* destination_;
         //! The pipe member that should be sent as "source".
@@ -98,15 +140,11 @@ private:
         int double_bytes_;
     };
 
-
     class GCToBinaryMember : public PipeMember
     {
     public:
-        GCToBinaryMember(Pipe* destination,
-                         PipeMember* skip_member)
-            : offset_(-1),
-              destination_(destination),
-              skip_member_(skip_member)
+        GCToBinaryMember(Pipe* destination, PipeMember* skip_member)
+            : offset_(-1), destination_(destination), skip_member_(skip_member)
         {
         }
 
@@ -134,17 +172,18 @@ private:
             }
             if (c == ';')
             {
-              if (offset_ < 0) {
-                return;
-              }
+                if (offset_ < 0)
+                {
+                    return;
+                }
                 // Frame ends here.
                 cbuf_[offset_] = 0;
                 struct can_frame frame;
                 int ret = gc_format_parse(cbuf_, &frame);
                 if (!ret)
                 {
-                    destination_->WriteToAll(skip_member_,
-                                             &frame, sizeof(frame));
+                    destination_->WriteToAll(skip_member_, &frame,
+                                             sizeof(frame));
                 }
                 offset_ = -1;
                 return;
@@ -156,10 +195,14 @@ private:
                 offset_ = -1;
                 return;
             }
-            if (offset_ >= 0) {
-              cbuf_[offset_++] = c;
-            } else {
-              // Drop byte to the floor -- we're not in the middle of a packet.
+            if (offset_ >= 0)
+            {
+                cbuf_[offset_++] = c;
+            }
+            else
+            {
+                // Drop byte to the floor -- we're not in the middle of a
+                // packet.
             }
         }
 
@@ -179,9 +222,10 @@ private:
     //! PipeMember doing the formatting.
     BinaryToGCMember formatter_;
 };
-  
 
-GCAdapterBase* GCAdapterBase::CreateGridConnectAdapter(Pipe* gc_side, Pipe* can_side, bool double_bytes)
+GCAdapterBase* GCAdapterBase::CreateGridConnectAdapter(Pipe* gc_side,
+                                                       Pipe* can_side,
+                                                       bool double_bytes)
 {
     return new GCAdapter(gc_side, can_side, double_bytes);
 }
