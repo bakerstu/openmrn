48e4ad7da755a1c43cc4643f2bad48bed99e1467
Balazs Racz <balazs.racz@gmail.com>
2014-02-16 15:50:19 +0100

    Adds some more checks to allocator.

--- src/executor/allocator.cxx
+++ src/executor/allocator.cxx
@@ -39,6 +39,7 @@ AllocatorBase::AllocatorBase()
 
 void AllocatorBase::Release(QueueMember* entry) {
   hasEverSeenFreeEntries_ = 1;
+  HASSERT(entry->next_ == NULL);
   AllocationResult* caller = nullptr;
   {
     LockHolder l(this);
@@ -56,6 +57,7 @@ void AllocatorBase::Release(QueueMember* entry) {
 
 void AllocatorBase::ReleaseBack(QueueMember* entry) {
   hasEverSeenFreeEntries_ = 1;
+  HASSERT(entry->next_ == NULL);
   AllocationResult* caller = nullptr;
   {
     LockHolder l(this);
