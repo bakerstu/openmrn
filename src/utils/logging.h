9327b0cac0331fe606711d49d63800cad626dec8
Balazs Racz <balazs.racz@gmail.com>
2014-03-12 00:53:16 -0500

    Fixes style of logging.h

--- src/utils/logging.h
+++ src/utils/logging.h
@@ -14,29 +14,42 @@ static const int WARNING = 2;
 static const int INFO = 3;
 static const int VERBOSE = 4;
 
-#define LOG(level, message...) do { if (LOGLEVEL >= level) { int sret = snprintf(logbuffer, sizeof(logbuffer), message); if (sret > (int)sizeof(logbuffer)) sret = sizeof(logbuffer); log_output(logbuffer, sret); } } while(0)
+#define LOG(level, message...)                                                 \
+    do                                                                         \
+    {                                                                          \
+        if (LOGLEVEL >= level)                                                 \
+        {                                                                      \
+            int sret = snprintf(logbuffer, sizeof(logbuffer), message);        \
+            if (sret > (int)sizeof(logbuffer))                                 \
+                sret = sizeof(logbuffer);                                      \
+            log_output(logbuffer, sret);                                       \
+        }                                                                      \
+    } while (0)
 
 extern char logbuffer[256];
-
 
 #ifndef LOGLEVEL
 #ifdef __FreeRTOS__
 #define LOGLEVEL FATAL
 #else
 #define LOGLEVEL INFO
-#endif  // not FreeRTOS
-#endif  // ifndef LOGLEVEL
+#endif // not FreeRTOS
+#endif // ifndef LOGLEVEL
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-void log_output(char* buf, int size);
-void PrintErrnoAndExit(const char* where);
+void log_output(char *buf, int size);
+void PrintErrnoAndExit(const char *where);
 #ifdef __cplusplus
 }
 #endif
 
-
-#define ERRNOCHECK(where, x...) do { if ((x) < 0) PrintErrnoAndExit(where); } while(0)
+#define ERRNOCHECK(where, x...)                                                \
+    do                                                                         \
+    {                                                                          \
+        if ((x) < 0)                                                           \
+            PrintErrnoAndExit(where);                                          \
+    } while (0)
 
 #endif // _LOGGING_H_
