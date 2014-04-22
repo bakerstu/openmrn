c9555c01c893b3e92ac1649b1e9f5fdbca139cb0
Balazs Racz <balazs.racz@gmail.com>
2014-04-22 18:28:53 +0200

    Fixes if_test_test.

--- test/tests/if_test_test.cc
+++ test/tests/if_test_test.cc
@@ -14,20 +14,21 @@ TEST_F(AsyncNodeTest, WriteMessageSync) {
   expect_packet(":X195B422AN0102030405060708;");
   event_write_helper1.WriteAsync(node_, If::MTI_EVENT_REPORT, WriteHelper::global(),
                                  EventIdToBuffer(0x0102030405060708ULL),
-                                 EmptyNotifiable::DefaultInstance());
+                                 get_notifiable());
 }
 
 TEST_F(AsyncNodeTest, WriteMessageASync) {
   // We write a message using the WriteFlow class asynchronously.
   expect_packet(":X195B422AN0102030405060708;");
-  SyncNotifiable n;
   event_write_helper1.WriteAsync(node_, If::MTI_EVENT_REPORT, WriteHelper::global(),
                                  EventIdToBuffer(0x0102030405060708ULL),
-                                 &n);
-  n.WaitForNotification();
+                                 get_notifiable());
+  wait_for_notification();
 }
 
-/** This is disabled because AME frmaes are not yet supported. */
+/** This is disabled because AME frmaes are not yet supported. 
+ * @TODO(balazs.racz): implement AME support and reenable.
+ */
 TEST_F(AsyncNodeTest, DISABLED_ReadMessageAndReply) {
   // We send an alias mapping enquiry frame and expect the node ID back.
   expect_packet(":X1070122AN02010d000003;");
