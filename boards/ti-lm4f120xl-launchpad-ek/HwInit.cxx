13c44422b4512f517c242198cd0acd08adc09e24
Balazs Racz <balazs.racz@gmail.com>
2014-05-01 00:55:13 +0200

    Fixes style of HwInit.cxx

--- boards/ti-lm4f120xl-launchpad-ek/HwInit.cxx
+++ boards/ti-lm4f120xl-launchpad-ek/HwInit.cxx
@@ -4,7 +4,7 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are  permitted provided that the following conditions are met:
- * 
+ *
  *  - Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
@@ -84,21 +84,23 @@ void timer5a_interrupt_handler(void)
     //
     MAP_TimerIntClear(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
     // Set output LED.
-    MAP_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, (rest_pattern & 1) ? GPIO_PIN_1 : 0);
+    MAP_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,
+                     (rest_pattern & 1) ? GPIO_PIN_1 : 0);
     // Shift and maybe reset pattern.
     rest_pattern >>= 1;
     if (!rest_pattern)
         rest_pattern = blinker_pattern;
 }
 
-void diewith(uint32_t pattern) {
-    vPortClearInterruptMask( 0x20 );
+void diewith(uint32_t pattern)
+{
+    vPortClearInterruptMask(0x20);
     asm("cpsie i\n");
 
     resetblink(pattern);
-    while(1);
+    while (1)
+        ;
 }
-
 }
 
 /** Initialize the processor hardware.
@@ -109,7 +111,8 @@ void hw_init(void)
     asm("cpsid i\n");
 
     /* Setup the system clock. */
-    MAP_SysCtlClockSet( SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ );
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
 
     /* Red LED pin initialization */
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -142,22 +145,18 @@ void hw_init(void)
     MAP_GPIOPinConfigure(GPIO_PE4_CAN0RX);
     MAP_GPIOPinConfigure(GPIO_PE5_CAN0TX);
     MAP_GPIOPinTypeCAN(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
-    
+
     /* Blue LED pin initialization */
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     MAP_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
     MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-    
+
     /* Green LED pin initialization */
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     MAP_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
     MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
-    
+
     /* Initialize timer0a interrupt */
 
     MAP_IntPrioritySet(INT_USB0, 0xff); // USB interrupt low priority
-
-    
-
 }
-
