0af811f67e5f30f6d6b08e3b5e728cb09162f9d5
Stuart W Baker <stuart_w_baker@ymail.com>
2014-05-05 21:35:48 -0500

    Add an objdump at the end of a program build.  This is nice information.

--- etc/prog.mk
+++ etc/prog.mk
@@ -107,6 +107,9 @@ $(EXECUTABLE)$(EXTENTION): $(OBJS) $(FULLPATHLIBS) $(LIBDIR)/timestamp lib/times
 ifdef SIZE
 	$(SIZE) $@
 endif
+ifdef OBJDUMP
+	$(OBJDUMP) -h $@
+endif
 
 $(EXECUTABLE).lst: $(EXECUTABLE)$(EXTENTION)
 	$(OBJDUMP) -d $< > $@
