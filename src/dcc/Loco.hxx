b8bfb3c451f6e03f8ce2ee350335c62b5c952686
Balazs Racz <balazs.racz@gmail.com>
2014-05-10 19:18:29 +0200

    Adds a simple dcc locomotive (incomplete)

---
+++ src/dcc/Loco.hxx
@@ -0,0 +1,91 @@
+/** \copyright
+ * Copyright (c) 2014, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file Loco.hxx
+ *
+ * Defines a simple DCC locomotive.
+ *
+ * @author Balazs Racz
+ * @date 10 May 2014
+ */
+
+#ifndef _DCC_LOCO_HXX_
+#define _DCC_LOCO_HXX_
+
+#include "dcc/Packet.hxx"
+#include "dcc/PacketSource.hxx"
+
+namespace dcc
+{
+
+class Dcc28Train : public PacketSource
+{
+public:
+    Dcc28Train(DccShortAddress a)
+    {
+        memset(this, 0, sizeof(*this));
+        isShortAddress_ = 1;
+        dccAddress_ = a.value;
+    }
+
+    Dcc28Train(DccLongAddress a)
+    {
+        memset(this, 0, sizeof(*this));
+        isShortAddress_ = 0;
+        dccAddress_ = a.value;
+    }
+
+    void set_speed(SpeedType speed) OVERRIDE;
+    virtual SpeedType get_speed() OVERRIDE;
+    virtual SpeedType get_commanded_speed() OVERRIDE {
+        return get_speed();
+    }
+    virtual void set_emergencystop() OVERRIDE;
+    virtual void set_fn(uint32_t address, uint16_t value) OVERRIDE;
+    virtual uint16_t get_fn(uint32_t address) OVERRIDE;
+    virtual uint32_t legacy_address() OVERRIDE;
+
+    // Generates next outgoing packet.
+    void get_next_packet(unsigned code, Packet* packet) OVERRIDE;
+
+private:
+    // largest address allowed is 10239.
+    unsigned dccAddress_ : 14;
+    unsigned isShortAddress_ : 1;
+    // 1: forward, 0: reverse
+    unsigned direction_ : 1;
+    unsigned directionChanged_ : 1;
+    unsigned speed_ : 5;
+    // Which refresh packet should go out next.
+    unsigned nextRefresh_ : 2;
+    // functions f0-f4.
+    unsigned fn_ : 5;
+    unsigned lastSetSpeed_ : 16;
+};
+
+} // namespace dcc
+
+#endif // _DCC_LOCO_HXX_
---
+++ src/dcc/PacketSource.hxx
@@ -0,0 +1,60 @@
+/** \copyright
+ * Copyright (c) 2014, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file PacketSource.hxx
+ *
+ * Defines an abstract TrainImpl class that is able to provide packets for
+ * refresh.
+ *
+ * @author Balazs Racz
+ * @date 10 May 2014
+ */
+
+#ifndef _DCC_PACKETSOURCE_HXX_
+#define _DCC_PACKETSOURCE_HXX_
+
+#include "nmranet/TrainInterface.hxx"
+
+namespace dcc {
+
+class Packet;
+typedef NMRAnet::SpeedType SpeedType;
+
+class PacketSource : public NMRAnet::TrainImpl {
+public:
+
+    /** Generates the next packet to send out to the track. 
+     * @param code if 0, then the next background refresh packet shold be
+     * generated. If non-zero, then it specifies a train-specific value that
+     * tells which recently changed value should be generated. 
+     * @param packet is the storage to set the outgoing packet in. */
+    virtual void get_next_packet(unsigned code, Packet* packet) = 0;
+};
+
+}  // namespace dcc
+
+
+#endif // _DCC_PACKETSOURCE_HXX_
