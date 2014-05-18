0c8c0da93a1dd880b7386d68c135e82a9b806161
Balazs Racz <balazs.racz@gmail.com>
2014-05-18 23:32:09 +0200

    Moves tractiontest fixture class and mock train class to a reusable header.

--- test/tests/traction_test.cc
+++ test/tests/traction_test.cc
@@ -1,4 +1,4 @@
-#include "utils/async_if_test_helper.hxx"
+#include "utils/async_traction_test_helper.hxx"
 
 #include "nmranet/TractionTrain.hxx"
 #include "nmranet/TractionDefs.hxx"
@@ -77,30 +77,6 @@ TEST(Fp16Test, ThirtySevenAndAHalf)
     EXPECT_EQ(37.5, s.speed());
 }
 
-class MockTrain : public TrainImpl
-{
-public:
-    MOCK_METHOD1(set_speed, void(SpeedType speed));
-    MOCK_METHOD0(get_speed, SpeedType());
-    MOCK_METHOD0(get_commanded_speed, SpeedType());
-    MOCK_METHOD0(get_actual_speed, SpeedType());
-    MOCK_METHOD0(set_emergencystop, void());
-    MOCK_METHOD2(set_fn, void(uint32_t address, uint16_t value));
-    MOCK_METHOD1(get_fn, uint16_t(uint32_t address));
-    MOCK_METHOD0(legacy_address, uint32_t());
-};
-
-class TractionTest : public AsyncNodeTest
-{
-protected:
-    TractionTest()
-        : trainService_(ifCan_.get())
-    {
-    }
-
-    TrainService trainService_;
-    StrictMock<MockTrain> m1_, m2_;
-};
 
 class TractionSingleMockTest : public TractionTest
 {
