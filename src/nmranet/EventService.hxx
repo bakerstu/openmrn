d3c61c989a13048239e4e1d0a39a75529a70929e
Balazs Racz <balazs.racz@gmail.com>
2014-05-22 22:54:55 +0200

    updates eventservice with proper include guard and disclaimer.

--- src/nmranet/EventService.hxx
+++ src/nmranet/EventService.hxx
@@ -1,7 +1,42 @@
+/** \copyright
+ * Copyright (c) 2013, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file EventService.hxx
+ *
+ * A service that takes incoming event messages, owns the event registry, and
+ * dispatches the incoming event messages to the registered handlers. This
+ * service can be registered to multiple NMRAnet::If interfaces at the same
+ * time.
+ *
+ * @author Balazs Racz
+ * @date 22 May 2014
+ */
 
-
-#ifndef _NMRANET_GLOBAL_EVENT_HANDLER_
-#define _NMRANET_GLOBAL_EVENT_HANDLER_
+#ifndef _NMRANET_EVENTSERVICE_HXX_
+#define _NMRANET_EVENTSERVICE_HXX_
 
 // This is a workaround for missing shared_ptr.h causing compilation errors. We
 // do not use shared_ptr.
@@ -12,7 +47,7 @@
 #include <memory>
 
 #include "utils/macros.h"
-#include "executor/StateFlow.hxx"
+#include "executor/Service.hxx"
 #include "nmranet/Defs.hxx"
 #include "nmranet/If.hxx"
 
@@ -55,4 +90,4 @@ private:
 
 }; /* namespace nmranet */
 
-#endif // _NMRANET_GLOBAL_EVENT_HANDLER_
+#endif // _NMRANET_EVENTSERVICE_HXX_
