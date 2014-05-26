c745caf0a37619e59aec857bd6fce78942a4cc16
Balazs Racz <balazs.racz@gmail.com>
2014-05-27 01:35:33 +0200

    Adds the 'cov' target for computing coverage of test executions in an efficient manner.
    The difference to the old test.mk is the following:
    - tests are built and executed in parallel
    - tests that are not rebuilt do not need to be re-run just to collect coverage
    - separately compiles all the sources with -O0 to get more accurate line information
    - runs GCC in deterministic mode (using hashes for random seeds)
    - md5sum-s the test binaries and if the binary didn't change, avoids re-running the test to save time.
    - collects coverage data in a separate directory per test.

---
+++ etc/cov.mk
@@ -0,0 +1,31 @@
+# Special target for building with the host GCC in coverage collecting mode.
+
+TOOLPATH := /usr/bin
+# Get the $(CFLAGSENV), $(CXXFLAGSENV), $(LDFLAGSENV)
+include $(OPENMRNPATH)/etc/env.mk
+
+CC = gcc
+CXX = g++
+AR = ar
+LD = g++
+
+HOST_TARGET := 1
+
+STARTGROUP := -Wl,--start-group
+ENDGROUP := -Wl,--end-group
+
+ARCHOPTIMIZATION = -g -O0 -m32 -fprofile-arcs -ftest-coverage
+
+CSHAREDFLAGS = -c -frandom-seed=$(shell echo $(abspath $<) | md5sum  | sed 's/\(.*\) .*/\1/') $(ARCHOPTIMIZATION) $(INCLUDES) -Wall -Werror -MD -MP -m32 -fno-stack-protector -D_GNU_SOURCE
+
+CFLAGS = $(CSHAREDFLAGS) -std=gnu99
+
+CXXFLAGS = $(CSHAREDFLAGS) -std=c++0x -D__STDC_FORMAT_MACROS \
+           -D__STDC_LIMIT_MACROS #-D__LINEAR_MAP__
+
+LDFLAGS = -g -m32 -pg -Wl,-Map="$(@:%=%.map)" -fprofile-arcs -ftest-coverage -O0
+SYSLIB_SUBDIRS += console
+SYSLIBRARIES = -lrt -lpthread -lconsole -lgcov
+
+EXTENTION =
+
