ff732eed48dbe158dcd6e755801f5ffdf37b9904
Balazs Racz <balazs.racz@gmail.com>
2014-06-09 19:51:33 +0200

    Adds more search path to armgcc in path.mk Switches over armgcc.mk to use path.mk instead of a manual path search.

--- etc/armgcc.mk
+++ etc/armgcc.mk
@@ -1,14 +1,8 @@
-TOOLPATH ?= $(shell \
-sh -c "if [ \"X`printenv TOOLPATH`\" != \"X\" ]; then printenv TOOLPATH; \
-     elif [ -d /usr/local/lpcxpresso_*/lpcxpresso/tools/bin ]; then ls -d /usr/local/lpcxpresso_*/lpcxpresso/tools | tail -n 1; \
-     elif [ -d /opt/lpcxpresso/default/lpcxpresso/tools/bin ]; then echo /opt/lpcxpresso/default/lpcxpresso/tools;  \
-     elif [ -d /opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin ]; then echo /opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI; \
-     elif [ -d /opt/armgcc/default/bin ]; then echo /opt/armgcc/default; \
-     else echo TOOLPATH not found; fi" \
-)
+include $(OPENMRNPATH)/etc/path.mk
 
-ifneq ($(TOOLPATH),TOOLPATH not found)
+ifneq ($(strip $(ARMGCCPATH)),)
 HAVE_ARMGCC = 1
+TOOLPATH:=$(ARMGCCPATH)
 else
 TOOLPATH=
 endif
--- etc/path.mk
+++ etc/path.mk
@@ -120,6 +120,9 @@ endif
 ################### ARM-GCC #####################
 ifndef ARMGCCPATH
 SEARCHPATH := \
+  /opt/lpcxpresso/default/lpcxpresso/tools \
+  /usr/local/lpcxpresso_*/lpcxpresso/tools \
+  /opt/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI \
   /opt/armgcc/default \
 
 TRYPATH:=$(call findfirst,bin/arm-none-eabi-g++,$(SEARCHPATH))
