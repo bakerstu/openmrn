1c48921c2921f71c629bb7eab1ec3b15f25dd3f5
Balazs Racz <balazs.racz@gmail.com>
2014-06-14 13:11:15 +0200

    Fixes to the stm32 port of OpenMRN: - forks the interrupt vector table to override the FreeRTOS port's handler names. - adds default interrupt handler - copies over hardfault handler from TI-tiva port - adds weak aliases for all interrupt handlers to the default handler - initializes the two hardware LEDs - adds a simple diewith logic for crashes. - adds flash command to the makefile.

--- boards/st-stm32f103rb-maple/hw_init.cxx
+++ boards/st-stm32f103rb-maple/hw_init.cxx
@@ -13,21 +13,34 @@ __attribute__((constructor)) void premain()
 {
     init();
     setblink(0);
+
+    /* Set up the LED to blink  */
+    gpio_set_mode(GPIOA, 5, GPIO_MODE_OUTPUT);
+    gpio_set_mode(GPIOA, 1, GPIO_MODE_OUTPUT);
+    gpio_write_bit(GPIOA, 5, 0);
+    gpio_write_bit(GPIOA, 1, 0);
+
+    while(0) {
+        gpio_write_bit(GPIOA, 5, 0);
+        gpio_write_bit(GPIOA, 1, 1);
+        for (volatile int i = 0; i < 300000; i++);
+        gpio_write_bit(GPIOA, 5, 1);
+        gpio_write_bit(GPIOA, 1, 0);
+        for (volatile int i = 0; i < 300000; i++);
+    }
+
 }
 
 void hw_init(void)
 {
     // This is needed for proper bootup of the FreeRTOS scheduler.
     nvic_globalirq_disable();
-
-    /* Set up the LED to blink  */
-    //pinMode(BOARD_LED_PIN, OUTPUT);
 }
 
 uint32_t blinker_pattern;
 uint32_t current_pattern;
-#define BLINK_GPIO LPC_GPIO1
-#define BLINK_BIT (1 << 23)
+#define BLINK_GPIO GPIOA
+#define BLINK_BIT 5
 
 extern "C" {
 extern void vPortClearInterruptMask( unsigned long ulNewMaskValue );
@@ -50,7 +63,9 @@ void _fail(const char* file, int line, const char* exp) {
     diewith(0x80CCACCA);
 }
 
-
+void default_interrupt_handler() {
+    diewith(BLINK_DIE_UNEXPIRQ);
+}
 
 /** Initializes the timer responsible for the blinking hardware and sets
  *   initial pattern.
@@ -60,6 +75,7 @@ void _fail(const char* file, int line, const char* exp) {
  */
 void setblink(uint32_t pattern)
 {
+    gpio_write_bit(ERROR_LED_PORT, ERROR_LED_PIN, pattern ? 1 : 0);
 /*    blinker_pattern = pattern;
     current_pattern = 0;
     BLINK_GPIO->FIODIR |= BLINK_BIT;
@@ -88,6 +104,7 @@ void setblink(uint32_t pattern)
 void resetblink(uint32_t pattern)
 {
     blinker_pattern = pattern;
+    setblink(pattern);
     // Makes a timer event trigger immediately.
     //LPC_TIM1->TC = LPC_TIM1->MR0 - 2;
 }
@@ -101,6 +118,18 @@ void resetblink(uint32_t pattern)
  */
 void diewith(unsigned long pattern)
 {
+    nvic_globalirq_disable();
+    while(1) {
+        gpio_write_bit(GPIOA, 5, current_pattern & 1 ? 1 : 0);
+        gpio_write_bit(GPIOA, 1, current_pattern & 1 ? 1 : 0);
+        current_pattern >>= 1;
+        if (!current_pattern)
+        {
+            current_pattern = pattern;
+        }
+        for (volatile int i = 0; i < 450000; i++);
+    }
+
     enable_fiq_only();
     setblink(pattern);
     for (;;)
@@ -128,4 +157,159 @@ void TIMER1_IRQHandler(void)
 //    LPC_TIM1->IR = 1;
 //    NVIC_ClearPendingIRQ(TIMER1_IRQn);
 }
+
+__attribute__((__naked__)) void hard_fault_handler_c( unsigned long *hardfault_args )
+{
+    /* These are volatile to try and prevent the compiler/linker optimising them
+    away as the variables never actually get used.  If the debugger won't show the
+    values of the variables, make them global my moving their declaration outside
+    of this function. */
+    volatile unsigned long stacked_r0 ;
+    volatile unsigned long stacked_r1 ;
+    volatile unsigned long stacked_r2 ;
+    volatile unsigned long stacked_r3 ;
+    volatile unsigned long stacked_r12 ;
+    volatile unsigned long stacked_lr ;
+    volatile unsigned long stacked_pc ;
+    volatile unsigned long stacked_psr ;
+    volatile unsigned long _CFSR ;
+    volatile unsigned long _HFSR ;
+    volatile unsigned long _DFSR ;
+    volatile unsigned long _AFSR ;
+    volatile unsigned long _BFAR ;
+    volatile unsigned long _MMAR ;
+
+    stacked_r0 = ((unsigned long)hardfault_args[0]) ;
+    stacked_r1 = ((unsigned long)hardfault_args[1]) ;
+    stacked_r2 = ((unsigned long)hardfault_args[2]) ;
+    stacked_r3 = ((unsigned long)hardfault_args[3]) ;
+    stacked_r12 = ((unsigned long)hardfault_args[4]) ;
+    stacked_lr = ((unsigned long)hardfault_args[5]) ;
+    stacked_pc = ((unsigned long)hardfault_args[6]) ;
+    stacked_psr = ((unsigned long)hardfault_args[7]) ;
+
+    // Configurable Fault Status Register
+    // Consists of MMSR, BFSR and UFSR
+    _CFSR = (*((volatile unsigned long *)(0xE000ED28))) ;   
+                                                                                    
+    // Hard Fault Status Register
+    _HFSR = (*((volatile unsigned long *)(0xE000ED2C))) ;
+
+    // Debug Fault Status Register
+    _DFSR = (*((volatile unsigned long *)(0xE000ED30))) ;
+
+    // Auxiliary Fault Status Register
+    _AFSR = (*((volatile unsigned long *)(0xE000ED3C))) ;
+
+    // Read the Fault Address Registers. These may not contain valid values.
+    // Check BFARVALID/MMARVALID to see if they are valid values
+    // MemManage Fault Address Register
+    _MMAR = (*((volatile unsigned long *)(0xE000ED34))) ;
+    // Bus Fault Address Register
+    _BFAR = (*((volatile unsigned long *)(0xE000ED38))) ;
+
+    __asm("BKPT #0\n") ; // Break into the debugger
+
+    /* When the following line is hit, the variables contain the register values. */
+    if (stacked_r0 || stacked_r1 || stacked_r2 || stacked_r3 || stacked_r12 ||
+        stacked_lr || stacked_pc || stacked_psr || _CFSR || _HFSR || _DFSR ||
+        _AFSR || _MMAR || _BFAR)
+    {
+        resetblink(BLINK_DIE_HARDFAULT);
+        for( ;; );
+    }
 }
+
+/** The fault handler implementation.  This code is inspired by FreeRTOS.
+ */
+static void hard_fault_handler(void)
+{
+    __asm volatile
+    (
+        "BKPT  #0 \n"
+        "MOVS   R0, #4                  \n"
+        "MOV    R1, LR                  \n"
+        "TST    R0, R1                  \n"
+        "BEQ    _MSP                    \n"
+        "MRS    R0, PSP                 \n"
+        "B      hard_fault_handler_c    \n"
+        "_MSP:                          \n"
+        "MRS    R0, MSP                 \n"
+        "B      hard_fault_handler_c    \n"
+        "BX    LR\n"
+    );
+}
+
+void __exc_nmi(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __exc_hardfault(void) __attribute__ ((weak, alias ("hard_fault_handler")));
+void __exc_memmanage(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __exc_busfault(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __exc_usagefault(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __stm32reservedexception7(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __stm32reservedexception8(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __stm32reservedexception9(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __stm32reservedexception10(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __exc_debug_monitor(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __stm32reservedexception13(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_wwdg(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_pvd(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tamper(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_rtc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_flash(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_rcc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti0(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti4(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel4(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel5(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel6(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma1_channel7(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_adc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usb_hp_can_tx(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usb_lp_can_rx0(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_can_rx1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_can_sce(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti9_5(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim1_brk(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim1_up(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim1_trg_com(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim1_cc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim4(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_i2c1_ev(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_i2c1_er(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_i2c2_ev(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_i2c2_er(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_spi1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_spi2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usart1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usart2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usart3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_exti15_10(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_rtcalarm(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_usbwakeup(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim8_brk(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim8_up(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim8_trg_com(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim8_cc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_adc3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_fsmc(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_sdio(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim5(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_spi3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_uart4(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_uart5(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim6(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_tim7(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma2_channel1(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma2_channel2(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma2_channel3(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+void __irq_dma2_channel4_5(void) __attribute__ ((weak, alias ("default_interrupt_handler")));
+
+}  // extern C
