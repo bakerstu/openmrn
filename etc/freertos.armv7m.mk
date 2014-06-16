fea7a9352b2341a4c21904673658ec58664dfc12
Balazs Racz <balazs.racz@gmail.com>
2014-06-17 00:14:05 +0200

    At link-time removes the dependencies on exception handling.

--- etc/freertos.armv7m.mk
+++ etc/freertos.armv7m.mk
@@ -55,7 +55,47 @@ LDFLAGS = -g -fdata-sections -ffunction-sections -T target.ld \
           -Wl,-Map="$(@:%.elf=%.map)" -Wl,--gc-sections \
           $(LDFLAGSEXTRA) $(LDFLAGSENV) 
 
-SYSLIBRARIES += $(SYSLIBRARIESEXTRA) -Wl,--wrap=__cxa_pure_virtual  -Wl,--wrap=__cxa_atexit -Wl,--wrap=__aeabi_atexit -Wl,--defsym=__wrap___cxa_pure_virtual=abort -Wl,--defsym=__wrap___cxa_atexit=ignore_fn -Wl,--defsym=__wrap___aeabi_atexit=ignore_fn
+# We disable linking against certain components from libc that we don't need
+# and pull in a lot of code dependencies (typically 50-100 kbytes), like
+# exception unwinding, or global destructor handling. On any exception
+# triggering call we just crash. This way we simulate as if newlib had been
+# compiled with -fno-exception. Most of these can be removed once we remove
+# usage of std::string.
+SYSLIBRARIES += $(SYSLIBRARIESEXTRA) \
+          -Wl,--wrap=__cxa_pure_virtual \
+          -Wl,--defsym=__wrap___cxa_pure_virtual=abort \
+          -Wl,--wrap=__cxa_atexit \
+          -Wl,--defsym=__wrap___cxa_atexit=ignore_fn \
+          -Wl,--wrap=__aeabi_atexit \
+          -Wl,--defsym=__wrap___aeabi_atexit=ignore_fn \
+          -Wl,--wrap=_ZSt20__throw_length_errorPKc \
+          -Wl,--defsym=__wrap__ZSt20__throw_length_errorPKc=abort \
+          -Wl,--wrap=__cxa_throw   \
+          -Wl,--defsym=__wrap___cxa_throw=abort \
+          -Wl,--wrap=_ZSt19__throw_logic_errorPKc   \
+          -Wl,--defsym=__wrap__ZSt19__throw_logic_errorPKc=abort \
+          -Wl,--wrap=__cxa_allocate_exception   \
+          -Wl,--defsym=__wrap___cxa_allocate_exception=abort \
+          -Wl,--wrap=__gxx_personality_v0   \
+          -Wl,--defsym=__wrap___gxx_personality_v0=abort \
+          -Wl,--wrap=__cxa_begin_catch   \
+          -Wl,--defsym=__wrap___cxa_begin_catch=abort \
+          -Wl,--wrap=__cxa_end_cleanup   \
+          -Wl,--defsym=__wrap___cxa_end_cleanup=abort \
+          -Wl,--wrap=__cxa_end_catch   \
+          -Wl,--defsym=__wrap___cxa_end_catch=abort \
+          -Wl,--wrap=__cxa_free_exception   \
+          -Wl,--defsym=__wrap___cxa_free_exception=abort \
+          -Wl,--wrap=__cxa_call_unexpected   \
+          -Wl,--defsym=__wrap___cxa_call_unexpected=abort \
+          -Wl,--wrap=__aeabi_unwind_cpp_pr0   \
+          -Wl,--defsym=__wrap___aeabi_unwind_cpp_pr0=abort \
+          -Wl,--wrap=__aeabi_unwind_cpp_pr1   \
+          -Wl,--defsym=__wrap___aeabi_unwind_cpp_pr1=abort \
+
+
+
+
 
 EXTENTION = .elf
 
