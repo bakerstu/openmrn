0034c80c5e5ff18fc4d6caeb038dbeafcda235e3
Balazs Racz <balazs.racz@gmail.com>
2014-06-24 18:43:01 +0200

    Makes the weak definition of stack_malloc always have a stack frame.

--- src/os/stack_malloc.c
+++ src/os/stack_malloc.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 
 #if defined (__FreeRTOS__)
-const void* __attribute__((weak)) stack_malloc(unsigned long length)
+const void* __attribute__((weak)) stack_malloc(unsigned long length);
+
+
+const void* stack_malloc(unsigned long length)
 {
-    return malloc(length);
+    /* We do a trick here to ensure that the compiler will output a stack frame
+     * for this function. We want to avoid tail-chain optimization in this
+     * function or else it disappears from the stack traces done for memory
+     * tracing. */
+    void* volatile v = malloc(length);
+    return v;
 }
 #endif
