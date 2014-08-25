ee3b32300006a70487e3ed72d16938362bc3bb49
Balazs Racz <balazs.racz@gmail.com>
2014-08-25 10:58:03 +0200

    Adds a state flow that writes packets to the /dev/mainline fd (aka TivaDCC).

---
+++ src/dcc/LocalTrackIf.cxx
@@ -0,0 +1,74 @@
+/** \copyright
+ * Copyright (c) 2014, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file LocakTrackIf.hxx
+ *
+ * Control flow that acts as a trackInterface and sends all packets to a local
+ * fd that represents the DCC mainline, such as TivaDCC.
+ *
+ * @author Balazs Racz
+ * @date 24 Aug 2014
+ */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+
+#define LOGLEVEL INFO
+
+#include "freertos/can_ioctl.h"
+#include "dcc/LocalTrackIf.hxx"
+
+namespace dcc
+{
+
+LocalTrackIf::LocalTrackIf(Service *service, int pool_size)
+    : StateFlow<Buffer<dcc::Packet>, QList<1>>(service)
+    , fd_(-1)
+    , pool_(sizeof(Buffer<dcc::Packet>), pool_size)
+{
+}
+
+StateFlowBase::Action LocalTrackIf::entry()
+{
+    HASSERT(fd_ >= 0);
+    auto *p = message()->data();
+    // Marklin packets are not currently supported by tiva mainline.
+    if (p->packet_header.is_marklin)
+    {
+        return finish();
+    }
+    int ret =
+        write(fd_, p->payload, p->dlc - 1 /*remove the xor byte for now*/);
+    if (ret < 0) {
+        HASSERT(errno == ENOSPC);
+        ::ioctl(fd_, CAN_IOC_WRITE_ACTIVE, this);
+        return wait();
+    }
+    return finish();
+}
+
+} // namespace dcc
---
+++ src/dcc/LocalTrackIf.hxx
@@ -0,0 +1,86 @@
+/** \copyright
+ * Copyright (c) 2014, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file LocakTrackIf.hxx
+ *
+ * Control flow that acts as a trackInterface and sends all packets to a local
+ * fd that represents the DCC mainline, such as TivaDCC.
+ *
+ * @author Balazs Racz
+ * @date 23 Aug 2014
+ */
+
+#ifndef _DCC_LOCALTRACKIF_HXX_
+#define _DCC_LOCALTRACKIF_HXX_
+
+#include "executor/Executor.hxx"
+#include "executor/StateFlow.hxx"
+#include "dcc/Packet.hxx"
+
+namespace dcc
+{
+
+class LocalTrackIf : public StateFlow<Buffer<dcc::Packet>, QList<1>>
+{
+public:
+    /** Constructs a TrackInterface from an fd to the mainline.
+     *
+     * This class currently does synchronous writes to the device. In order not
+     * to block the executor, you have to create a new threadexecutor.
+     *
+     * @param service THE EXECUTOR OF THIS SERVICE WILL BE BLOCKED.
+     * @param pool_size will determine how many packets the current flow's
+     * alloc() will have.
+     */
+    LocalTrackIf(Service *service, int pool_size);
+
+    FixedPool *pool() OVERRIDE
+    {
+        return &pool_;
+    }
+
+    /** You must call this function before sending any packets. @param fd is
+     * the file descriptor to /dev/mainline. */
+    void set_fd(int fd)
+    {
+        fd_ = fd;
+    }
+
+private:
+    Action entry() OVERRIDE;
+
+    Action finish()
+    {
+        return release_and_exit();
+    }
+
+    int fd_;
+    FixedPool pool_;
+};
+
+} // namespace dcc
+
+#endif // _DCC_LOCALTRACKIF_HXX_
