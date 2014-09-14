63d2d98e0c15e0d0fb0d0e46588aeb55b8bc933a
Balazs Racz <balazs.racz@gmail.com>
2014-09-14 17:53:36 +0200

    Adds a simple class to log device driver events into a single int32 or int64.

---
+++ src/freertos_drivers/common/SimpleLog.hxx
@@ -0,0 +1,55 @@
+/** \copyright
+ * Copyright (c) 2014, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file SimpleLog.hxx
+ *
+ * A very simple logging mechanism of driver events that is capable of logging
+ * a few entries of an 8-byte enum value, in a gdb-friendly way.
+ *
+ * @author Balazs Racz
+ * @date 14 September 2014
+ */
+
+#ifndef _FREERTOS_DRIVERS_COMMON_SIMPLELOG_HXX_
+#define _FREERTOS_DRIVERS_COMMON_SIMPLELOG_HXX_
+
+template<typename C> class SimpleLog {
+public:
+  SimpleLog()
+    : log_(0) {}
+
+  void log(uint8_t value) {
+    log_ <<= 8;
+    log_ |= value;
+  }
+
+private:
+  C log_;
+};
+
+typedef SimpleLog<uint64_t> LogBuffer;
+
+#endif // _FREERTOS_DRIVERS_COMMON_SIMPLELOG_HXX_
