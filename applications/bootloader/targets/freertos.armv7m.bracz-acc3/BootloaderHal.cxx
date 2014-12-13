cd907ad540a6655aaca354712336c71207a3faba
Balazs Racz <balazs.racz@gmail.com>
2014-12-13 14:30:14 +0100

    Implements the bootloader HAL functions for the tiva CM3 chip.

--- applications/bootloader/targets/freertos.armv7m.bracz-acc3/BootloaderHal.cxx
+++ applications/bootloader/targets/freertos.armv7m.bracz-acc3/BootloaderHal.cxx
@@ -7,8 +7,12 @@
 #include "driverlib/rom_map.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/can.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/pin_map.h"
+
+#include "nmranet_config.h"
+#include "nmranet/Defs.hxx"
 
 extern "C" {
 
@@ -52,48 +56,124 @@ void checksum_data(const void *data, uint32_t size, uint32_t *checksum)
 }
 
 extern void bootloader_reset_segments(void);
+extern unsigned long cm3_cpu_clock_hz;
 
-
-void bootloader_hw_init() {
+void bootloader_hw_init()
+{
     bootloader_reset_segments();
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
                        SYSCTL_XTAL_20MHZ);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
     ROM_GPIOPinConfigure(GPIO_PB4_CAN0RX);
     ROM_GPIOPinConfigure(GPIO_PB5_CAN0TX);
     ROM_GPIOPinTypeCAN(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_5);
-    
+
+    ROM_CANInit(CAN0_BASE);
+    ROM_CANBitRateSet(CAN0_BASE, cm3_cpu_clock_hz,
+                      config_nmranet_can_bitrate());
+
+    // Sets up CAN message receiving object.
+    tCANMsgObject can_message;
+    can_message.ui32MsgID = 0;
+    can_message.ui32MsgIDMask = 0;
+    can_message.ui32Flags = MSG_OBJ_RX_INT_ENABLE | MSG_OBJ_USE_ID_FILTER;
+    can_message.ui32MsgLen = 8;
+    ROM_CANMessageSet(CAN0_BASE, 1, &can_message, MSG_OBJ_TYPE_RX);
+
+    ROM_CANEnable(CAN0_BASE);
 }
 
-bool request_bootloader() {
+bool request_bootloader()
+{
     return false;
 }
 
-uint16_t nmranet_alias() {
+uint16_t nmranet_alias()
+{
     /// TODO(balazs.racz):  fix this
     return 0x428;
 }
 
-bool read_can_frame(struct can_frame *frame) {
-    return false;
+extern const nmranet::NodeID NODE_ID;
+
+uint64_t nmranet_nodeid()
+{
+    /// TODO(balazs.racz):  fix this
+    return NODE_ID;
 }
 
-bool try_send_can_frame(const struct can_frame &frame) {
-    return false;
+bool read_can_frame(struct can_frame *frame)
+{
+    uint32_t regbits = ROM_CANStatusGet(CAN0_BASE, CAN_STS_NEWDAT);
+    if (!(regbits & 1))
+    {
+        return false;
+    }
+
+    tCANMsgObject can_message;
+    can_message.pui8MsgData = frame->data;
+
+    /* Read a message from CAN and clear the interrupt source */
+    MAP_CANMessageGet(CAN0_BASE, 1, &can_message, 1 /* clear interrupt */);
+
+    frame->can_id = can_message.ui32MsgID;
+    frame->can_rtr = (can_message.ui32Flags & MSG_OBJ_REMOTE_FRAME) ? 1 : 0;
+    frame->can_eff = (can_message.ui32Flags & MSG_OBJ_EXTENDED_ID) ? 1 : 0;
+    frame->can_err = 0;
+    frame->can_dlc = can_message.ui32MsgLen;
+    return true;
 }
 
-void bootloader_reboot(void) {
+bool try_send_can_frame(const struct can_frame &frame)
+{
+    // Checks if previous frame is out yet.
+    uint32_t regbits = ROM_CANStatusGet(CAN0_BASE, CAN_STS_TXREQUEST);
+    if (regbits & 2)
+    {
+        return false;
+    }
+
+    /* load the next message to transmit */
+    tCANMsgObject can_message;
+    can_message.ui32MsgID = frame.can_id;
+    can_message.ui32MsgIDMask = 0;
+    can_message.ui32Flags = 0;
+    if (frame.can_eff)
+    {
+        can_message.ui32Flags |= MSG_OBJ_EXTENDED_ID;
+    }
+    if (frame.can_rtr)
+    {
+        can_message.ui32Flags |= MSG_OBJ_REMOTE_FRAME;
+    }
+    can_message.ui32MsgLen = frame.can_dlc;
+    can_message.pui8MsgData = (uint8_t *)frame.data;
+    ROM_CANMessageSet(CAN0_BASE, 2, &can_message, MSG_OBJ_TYPE_TX);
+    return true;
+}
+
+void bootloader_reboot(void)
+{
     ROM_SysCtlReset();
 }
 
-void application_entry(void) {
-    typedef void fun_t(void);
-    fun_t *fun;
-    fun = (fun_t*)(0x180);
-    fun();
+void application_entry(void)
+{
+    extern char __flash_start;
+    // We store the application reset in interrupt vecor 13, which is reserved
+    // / unused on all Cortex_M3 processors.
+    asm volatile(" mov   r3, %[flash_addr] \n"
+                 :
+                 : [flash_addr] "r"(&__flash_start));
+    asm volatile(" ldr r0, [r3]\n"
+                 " mov sp, r0\n"
+                 " ldr r0, [r3, #52]\n"
+                 " bx  r0\n");
 }
 
-void ignore_fn(void) {}
-
+void ignore_fn(void)
+{
+}
 
 } // extern "C"
