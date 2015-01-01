518dbb601cfa101e936f27787c21d3b896f4b52a
Balazs Racz <balazs.racz@gmail.com>
2015-01-01 18:49:44 +0100

    Adds commandline arguments parsing to the hub main. Adds the ability to talk to a can-usb device as well in addition to serving a hub.

--- applications/hub/main.cxx
+++ applications/hub/main.cxx
@@ -32,7 +32,10 @@
  * @date 3 Aug 2013
  */
 
+#include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <termios.h> /* tc* functions */
 #include <unistd.h>
 
 #include <memory>
@@ -52,6 +55,50 @@ GcPacketPrinter packet_printer(&can_hub0);
 
 OVERRIDE_CONST(gc_generate_newlines, 1);
 
+int port = 12021;
+const char *device_path = nullptr;
+
+#ifdef __linux__
+
+void usage(const char *e)
+{
+    fprintf(stderr, "Usage: %s [-p port] [-d device_path]\n\n", e);
+    fprintf(stderr, "GridConnect CAN HUB.\nListens to a specific TCP port, "
+                    "reads CAN packets from the incoming connections using "
+                    "the GridConnect protocol, and forwards all incoming "
+                    "packets to all other participants.\n\nArguments:\n");
+    fprintf(stderr, "\t-p port     specifies the port number to listen on, "
+                    "default is 12021.\n");
+    fprintf(stderr, "\t-d device   is a path to a physical device doing "
+                    "serial-CAN or USB-CAN. If specified, opens device and "
+                    "adds it to the hub.\n");
+    exit(1);
+}
+
+void parse_args(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, "hp:d:n:a:s:f:c:")) >= 0)
+    {
+        switch (opt)
+        {
+            case 'h':
+                usage(argv[0]);
+                break;
+            case 'd':
+                device_path = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Unknown option %c\n", opt);
+                usage(argv[0]);
+        }
+    }
+}
+#endif
+
 /** Entry point to application.
  * @param argc number of command line arguments
  * @param argv array of command line arguments
@@ -59,9 +106,33 @@ OVERRIDE_CONST(gc_generate_newlines, 1);
  */
 int appl_main(int argc, char *argv[])
 {
+    parse_args(argc, argv);
     GcTcpHub hub(&can_hub0, 12021);
+    int dev_fd = 0;
     while (1)
     {
+        if (device_path && !dev_fd)
+        {
+            dev_fd = ::open(device_path, O_RDWR);
+            if (dev_fd > 0)
+            {
+                // Sets up the terminal in raw mode. Otherwise linux might echo
+                // characters coming in from the device and that will make
+                // packets go back to where they came from.
+                HASSERT(!tcflush(dev_fd, TCIOFLUSH));
+                struct termios settings;
+                HASSERT(!tcgetattr(dev_fd, &settings));
+                cfmakeraw(&settings);
+                HASSERT(!tcsetattr(dev_fd, TCSANOW, &settings));
+                LOG(INFO, "Opened device %s.\n", device_path);
+                create_gc_port_for_can_hub(&can_hub0, dev_fd);
+            }
+            else
+            {
+                LOG(ERROR, "Failed to open device %s: %s\n", device_path,
+                    strerror(errno));
+            }
+        }
         sleep(1);
     }
     return 0;
