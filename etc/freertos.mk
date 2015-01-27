e935120616b18342b6eab7b7fed101fdd44cb8df
Stuart Baker <stuart_w_baker@ymail.com>
2015-01-26 23:30:44 -0600

    Add in event flag support

--- etc/freertos.mk
+++ etc/freertos.mk
@@ -1,5 +1,5 @@
 include $(OPENMRNPATH)/etc/path.mk
 
 SYSLIB_SUBDIRS += freertos freertos_drivers
-SYSLIBRARIES += -lfreertos -lfreertos_drivers
+SYSLIBRARIES += -lfreertos -lfreertos_drivers $(OPENMRNPATH)/targets/$(TARGET)/freertos/event_groups.o
 DEPS += FREERTOSPATH
