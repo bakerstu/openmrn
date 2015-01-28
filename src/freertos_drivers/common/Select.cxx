cddd935a1e285755777ef25e0f902f17bfdfa4f0
Stuart Baker <stuart_w_baker@ymail.com>
2015-01-27 22:08:19 -0600

    Trivial Doxygen comment fix.

--- src/freertos_drivers/common/Select.cxx
+++ src/freertos_drivers/common/Select.cxx
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * \file select.h
+ * \file Select.cxx
  * This file imlements POSIX select().
  *
  * @author Stuart W. Baker
