2bfa111553a7f81f9566d5e16d6bed2f195a6399
Balazs Racz <balazs.racz@gmail.com>
2015-01-31 21:39:03 -0600

    Changes the link order on the core and application libs so that they all fall into a link group. This is needed if the appl_main is inside an application library instead of a cxx file that's directly linked on the commandline.

--- etc/config.mk
+++ etc/config.mk
@@ -3,6 +3,6 @@ MAKE_INC_CONFIG_MK := 1
 
 CORELIBS := utils executor os if core cue dcc nmranet
 
-LINKCORELIBS = -lnmranet -ldcc -lcue -lif -lcore $(STARTGROUP) -lexecutor -lutils -los $(ENDGROUP)
+LINKCORELIBS = -lnmranet -ldcc -lcue -lif -lcore -lexecutor -lutils -los
 
 endif # MAKE_INC_CONFIG_MK
