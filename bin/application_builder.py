01647168db2a754caf43cba5862ced85d42d7647
Stuart Baker <stuart_w_baker@ymail.com>
2015-02-25 18:13:09 -0600

    continued progress on GUI

--- bin/application_builder.py
+++ bin/application_builder.py
@@ -6,8 +6,18 @@ import serial
 import glob
 from Tkinter import *
 from ttk import *
+from tkFileDialog import *
 import Image
+from functools import partial
 
+FreertosArmv7mList = [['ek-lm4f120xl (legacy)', None, None, None],
+                      ['ek-tm4c123gxl', None, None, None],
+                      ['ek-tm4c1294xl', None, None, None],
+                      ['Generic Tiva TM4C123', None, None, None],
+                      ['Generic Tiva TM4C129', None, None, None],
+                      ['Custom', None, None, None]]
+
+# display help information in form of text
 class ToolTip(object):
 
     def __init__(self, widget):
@@ -54,6 +64,7 @@ def createToolTip(widget, text, y):
     widget.bind('<Enter>', enter)
     widget.bind('<Leave>', leave)
 
+# display help information in form of an image
 class ToolImage(object):
 
     def __init__(self, widget):
@@ -107,6 +118,11 @@ class App:
         master.title("OpenMRN Application Builder")
         frame = Frame(master)
         frame.pack(fill=BOTH, expand=1)
+        self.dir_opt = options = {}
+        options['initialdir'] = 'C:\\'
+        options['mustexist'] = False
+        options['parent'] = root
+        options['title'] = 'This is a title'
         ROW = 0
 
         #frame.columnconfigure(1, weight=1)
@@ -131,10 +147,11 @@ class App:
                                font="Arial 12 bold")
         self.ApplLabel.grid(row=ROW, column=2, columnspan=2, sticky=S+W, padx=10)
         self.ApplPath = Entry(frame, width=64)
-        self.ApplPath.grid(row=ROW+1, column=2, columnspan=2, sticky=E+W, padx=5, pady=(0,20))
+        self.ApplPath.grid(row=ROW+1, column=2, columnspan=1, sticky=E+W, padx=5, pady=(0,20))
         self.ApplTip = ('This is the file system path which points to\nthe '
                         'location of the generated application.')
         createToolTip(self.ApplPath, self.ApplTip, 3)
+        Button(frame, text='Browse...', command=partial(self.browse, self.ApplPath)).grid(row=ROW+1, column=3, sticky=E+W, padx=5, pady=(0,20))
 
         ROW +=2
 
@@ -182,54 +199,57 @@ class App:
         # Targets
         self.TargetLabel = Label(frame, text="Targets", font="Arial 12 bold")
         self.TargetLabel.grid(row=ROW, column=0, sticky=S+W, padx=10)
-        self.LinuxX86Val = IntVar()
-        self.LinuxX86 = Checkbutton(frame, text="linux.x86",
-                                    variable=self.LinuxX86Val,
-                                    state=NORMAL)
-        self.LinuxX86.grid(row=ROW, column=0, sticky=W, padx=5)
-        ROW += 1
-        self.MachX86_64Val = IntVar()
-        self.MachX86_64 = Checkbutton(frame, text="mach.x86_64",
-                                       variable=self.MachX86_64Val,
-                                       state=NORMAL)
-        self.MachX86_64.grid(row=ROW, column=0, sticky=W, padx=5)
-        ROW += 1
-        self.FreertosArmv7mVal = IntVar()
-        self.FreertosArmv7m = Checkbutton(frame, text="freertos.armv7m",
-                                          variable=self.FreertosArmv7mVal,
-                                          state=['!selected'])
-        self.FreertosArmv7m.grid(row=ROW, column=0, sticky=W, padx=5)
-        ROW += 1
-        self.Eklm4f120xlVal = IntVar()
-        self.Eklm4f120xl = Checkbutton(frame, text="ek-lm4f120xl (legacy)",
-                                       variable=self.Eklm4f120xlVal,
-                                       state=DISABLED)
-        self.Eklm4f120xl.grid(row=ROW+1, column=0, sticky=W, padx=25)
-        createToolImage(self.Eklm4f120xl, "images/ek-tm4c123gxl.gif")
-        self.Ektm4c123gxlVal = IntVar()
-        self.Ektm4c123gxl = Checkbutton(frame, text="ek-tm4c123gxl",
-                                        variable=self.Ektm4c123gxlVal,
-                                        state=DISABLED)
-        self.Ektm4c123gxl.grid(row=ROW+2, column=0, sticky=W, padx=25)
-        createToolImage(self.Ektm4c123gxl, "images/ek-tm4c123gxl.gif")
-        self.Ektm4c1294xlVal = IntVar()
-        self.Ektm4c1294xl = Checkbutton(frame, text="ek-tm4c1294xl",
-                                        variable=self.Ektm4c1294xlVal,
-                                        state=DISABLED)
-        self.Ektm4c1294xl.grid(row=ROW+3, column=0, sticky=W, padx=25)
-        createToolImage(self.Ektm4c1294xl, "images/ek-tm4c1294xl.gif")
 
-        ROW += 4
+        self.LinuxX86 = ['linux.x86', IntVar(), None]
+        self.add_target(self.LinuxX86, frame, ROW+1, None)
+
+        self.MachX86_64 = ['mach.x86_64', IntVar(), None]
+        self.add_target(self.MachX86_64, frame, ROW+2, None)
+
+        self.FreertosArmv7m = ['freertos.armv7m (Cortex-M3/M4)', IntVar(), None]
+        self.add_subtargets(self.FreertosArmv7m, frame, ROW+3, FreertosArmv7mList)
+
+        ROW += 11
 
         # Quit Button
         self.Exit = Button(frame, text="Quit", command=frame.quit)
-        #self.Exit.pack(side=RIGHT)
         self.Exit.grid(row=ROW-2, column=3, rowspan=3, sticky=S+E, padx=5, pady=5)
 
         # Generate Button
         self.Generate = Button(frame, text="Generate", command=frame.quit)
-        #self.Generate.pack(side=RIGHT)
         self.Generate.grid(row=ROW-2, column=3, rowspan=3, sticky=S, padx=(0,15), pady=5)
+
+    # create a target
+    def add_target(self, target, frame, row, target_list):
+        target[2] = Checkbutton(frame, text=target[0],
+                                variable=target[1], state=NORMAL,
+                                command=partial(self.target_select, target, target_list))
+        target[2].grid(row=row, column=0, sticky=W, padx=5)
+
+    # create a sub-target list
+    def add_subtargets(self, target, frame, row, target_list):
+        self.add_target(target, frame, row, target_list)
+        row += 1
+        for item in target_list:
+            item[1] = IntVar()
+            item[2] = Checkbutton(frame, text=item[0],
+                                  variable=item[1], state=DISABLED)
+            item[2].grid(row=row, column=0, sticky=W, padx=25)
+            row += 1
+
+    def target_select(self, target, target_list):
+        if target_list == None:
+            return
+        if target[1].get():
+            for item in target_list:
+                item[2].configure(state=NORMAL)
+        else:
+            for item in target_list:
+                item[2].configure(state=DISABLED)
+                item[1].set(False)
+
+    def browse(self, box):
+        box.insert(0, askdirectory(**self.dir_opt))
 
 # Start of the application
 root = Tk()
