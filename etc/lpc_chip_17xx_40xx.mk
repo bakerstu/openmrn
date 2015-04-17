4fdc1f346a47c776f77ea4984ffed564e5bad417
Stuart Baker <stuart_w_baker@ymail.com>
2015-04-16 20:49:58 -0500

    Trival comment fixes

--- etc/lpc_chip_17xx_40xx.mk
+++ etc/lpc_chip_17xx_40xx.mk
@@ -6,7 +6,7 @@ INCLUDES += -I$(OPENMRNPATH)/src/freertos_drivers/nxp \
 endif
 
 # unfortunately, the lpc_chip libraries do not compile with no warnings
-# we define __SYS_CONFIG_h_ in order to override the CHIP_LPC* define in a
+# we define __SYS_CONFIG_H_ in order to override the CHIP_LPC* define in a
 # Makefile rather than in sys_config.h
 CFLAGS += -D__SYS_CONFIG_H_ -DCORE_M3 -Wno-error \
           -Wno-strict-prototypes -Wno-uninitialized \
