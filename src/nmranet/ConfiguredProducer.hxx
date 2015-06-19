cef438f843f016fd653c5d1474ee429f3061230c
Balazs Racz <balazs.racz@gmail.com>
2015-06-19 22:16:41 +0200

    Adds shared classes for event producers.

---
+++ src/nmranet/ConfiguredProducer.hxx
@@ -0,0 +1,119 @@
+/** \copyright
+ * Copyright (c) 2015, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file ConfiguredProducer.hxx
+ *
+ * Producer class that uses CDI configuration and a GPIO template structure to
+ * export a single bit as two event producers to OpenLCB.
+ *
+ * @author Balazs Racz
+ * @date 13 June 2015
+ */
+
+#ifndef _NMRANET_CONFIGUREDPRODUCER_HXX_
+#define _NMRANET_CONFIGUREDPRODUCER_HXX_
+
+#include "nmranet/PolledProducer.hxx"
+#include "nmranet/EventHandlerTemplates.hxx"
+#include "nmranet/ConfigRepresentation.hxx"
+#include "utils/ConfigUpdateListener.hxx"
+#include "utils/ConfigUpdateService.hxx"
+#include "utils/Debouncer.hxx"
+
+namespace nmranet
+{
+
+BEGIN_GROUP(ProducerConfig, base);
+EXTEND_GROUP(ProducerConfig, base, debounce, Uint8ConfigEntry);
+EXTEND_GROUP(ProducerConfig, debounce, event_on, EventConfigEntry, //
+    Name("Event On"),
+    Description("This event will be produced when the input goes to HIGH."));
+EXTEND_GROUP(ProducerConfig, event_on, event_off, EventConfigEntry, //
+    Name("Event Off"),
+    Description("This event will be produced when the input goes to LOW."));
+END_GROUP(ProducerConfig, event_off);
+
+extern "C" {
+void ignore_fn();
+}
+
+class ConfiguredProducer : public ConfigUpdateListener
+{
+public:
+    using Impl = GPIOBit;
+    using ProducerClass = PolledProducer<QuiesceDebouncer, Impl>;
+
+    template <class HW>
+    ConfiguredProducer(Node *node, const ProducerConfig &cfg, const HW &)
+        : producer_(QuiesceDebouncer::Options(3), node, 0, 0, &HW::get,
+              (GPIOBit::setter_fn_t)&ignore_fn)
+        , cfg_(cfg)
+    {
+        ConfigUpdateService::instance()->register_update_listener(this);
+    }
+
+    UpdateAction apply_configuration(
+        int fd, bool initial_load, BarrierNotifiable *done)
+    {
+        AutoNotify n(done);
+        uint8_t debounce_arg = cfg_.debounce().read(fd);
+        EventId cfg_event_on = cfg_.event_on().read(fd);
+        EventId cfg_event_off = cfg_.event_off().read(fd);
+        if (cfg_event_off != producer_.event_off() ||
+            cfg_event_on != producer_.event_on())
+        {
+            auto saved_setter = producer_.setter_;
+            auto saved_getter = producer_.getter_;
+            auto saved_node = producer_.node();
+            // Need to reinitialize the producer. We do this with in-place
+            // destruction and construction.
+            producer_.~ProducerClass();
+            new (&producer_) ProducerClass(
+                QuiesceDebouncer::Options(debounce_arg), saved_node,
+                cfg_event_on, cfg_event_off, saved_getter, saved_setter);
+            return REINIT_NEEDED; // Causes events identify.
+        }
+        return UPDATED;
+    }
+
+    ///@TODO(balazs.racz): implement
+    void factory_reset(int fd) OVERRIDE
+    {
+    }
+
+    Polling *polling()
+    {
+        return &producer_;
+    }
+
+private:
+    ProducerClass producer_;
+    const ProducerConfig cfg_;
+};
+
+} // namespace nmranet
+
+#endif // _NMRANET_CONFIGUREDPRODUCER_HXX_
