9981b443b4be43f8c01ce9d1f46d443ff6ffd23d
Balazs Racz <balazs.racz@gmail.com>
2015-06-20 11:49:36 +0200

    Adds a version information segment.

--- applications/io_board/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
+++ applications/io_board/targets/freertos.armv7m.ek-tm4c123gxl/config.hxx
@@ -20,12 +20,19 @@ EXTEND_GROUP(
     IoBoardSegment, consumers, producers, AllProducers, Name("Input buttons"));
 END_GROUP(IoBoardSegment, producers);
 
+BEGIN_GROUP(VersionSeg, base, Segment(MemoryConfigDefs::SPACE_CONFIG),
+    Name("Version information"));
+EXTEND_GROUP(VersionSeg, base, acdi_user_version, Uint8ConfigEntry,
+    Name("ACDI User Data version"), Description("Set to 2 and do not change."));
+END_GROUP(VersionSeg, acdi_user_version);
+
 BEGIN_GROUP(ConfigDef, base, MainCdi());
 EXTEND_GROUP(ConfigDef, base, ident, Identification);
 EXTEND_GROUP(ConfigDef, ident, acdi, Acdi);
 EXTEND_GROUP(ConfigDef, acdi, userinfo, UserInfoSegment);
 EXTEND_GROUP(ConfigDef, userinfo, seg, IoBoardSegment);
-END_GROUP(ConfigDef, seg);
+EXTEND_GROUP(ConfigDef, seg, version, VersionSeg);
+END_GROUP(ConfigDef, version);
 
 } // namespace nmranet
 
