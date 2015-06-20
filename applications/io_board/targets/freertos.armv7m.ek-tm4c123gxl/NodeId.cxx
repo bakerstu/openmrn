1c48153c19e366f51a186153b3fa3fa6e4d98910
Balazs Racz <balazs.racz@gmail.com>
2015-06-20 15:21:19 +0200

    Moves Node ID into main.cxx. Adds a lot of explanation comments to main.cxx.

--- applications/io_board/targets/freertos.armv7m.ek-tm4c123gxl/NodeId.cxx
+++
@@ -1,4 +0,0 @@
-#include "nmranet/If.hxx"
-
-extern const nmranet::NodeID NODE_ID;
-const nmranet::NodeID NODE_ID = 0x050101011412ULL;
--- applications/io_board/targets/freertos.armv7m.ek-tm4c123gxl/main.cxx
+++ applications/io_board/targets/freertos.armv7m.ek-tm4c123gxl/main.cxx
@@ -43,41 +43,87 @@
 #include "freertos_drivers/common/BlinkerGPIO.hxx"
 #include "config.hxx"
 
+// These preprocessor symbols are used to select which physical connections
+// will be enabled in the main(). See @ref appl_main below.
 #define SNIFF_ON_SERIAL
+//#define SNIFF_ON_USB
+//#define HAVE_PHYSICAL_CAN_PORT
 
-extern const nmranet::NodeID NODE_ID;
-
+// Changes the default behavior by adding a newline after each gridconnect
+// packet. Makes it easier for debugging the raw device.
 OVERRIDE_CONST(gc_generate_newlines, 1);
+// Specifies how much RAM (in bytes) we allocate to the stack of the main
+// thread. Useful tuning parameter in case the application runs out of memory.
 OVERRIDE_CONST(main_thread_stack_size, 2500);
 
+// Specifies the 48-bit OpenLCB node identifier. This must be unique for every
+// hardware manufactured, so in production this should be replaced by some
+// easily incrementable method.
+extern const nmranet::NodeID NODE_ID = 0x050101011412ULL;
+
+// Sets up a comprehensive OpenLCB stack for a single virtual node. This stack
+// contains everything needed for a usual peripheral node -- all
+// CAN-bus-specific components, a virtual node, PIP, SNIP, Memory configuration
+// protocol, ACDI, CDI, a bunch of memory spaces, etc.
 nmranet::SimpleCanStack stack(NODE_ID);
 
+// ConfigDef comes from config.hxx and is specific to the particular device and
+// target. It defines the layout of the configuration memory space and is also
+// used to generate the cdi.xml file. Here we instantiate the configuration
+// layout. The argument of offset zero is ignored and will be removed later.
 nmranet::ConfigDef cfg(0);
+// Defines weak constants used by the stack to tell it which device contains
+// the volatile configuration information. This device name appears in
+// HwInit.cxx that creates the device drivers.
 extern const char *const nmranet::CONFIG_FILENAME = "/dev/eeprom";
+// The size of the memory space to export over the above device.
 extern const size_t nmranet::CONFIG_FILE_SIZE =
     cfg.seg().size() + cfg.seg().offset();
 static_assert(nmranet::CONFIG_FILE_SIZE <= 256, "Need to adjust eeprom size");
+// The SNIP user-changeable information in also stored in the above eeprom
+// device. In general this could come from different eeprom segments, but it is
+// simpler to keep them together.
 extern const char *const nmranet::SNIP_DYNAMIC_FILENAME =
     nmranet::CONFIG_FILENAME;
 
+// Defines the GPIO ports used for the producers and the consumers.
+
+// The first LED is driven by the blinker device from BlinkerGPIO.hxx. WE just
+// create an alias for symmetry.
+typedef BLINKER_Pin LED_RED_Pin;
+// These are GPIO output pins from TivaGPIO.hxx
 GPIO_PIN(LED_GREEN, LedPin, F, 3);
 GPIO_PIN(LED_BLUE, LedPin, F, 2);
 
+// These are GPIO input pins from TivaGPIO.hxx
 GPIO_PIN(SW1, GpioInputPU, F, 4);
 GPIO_PIN(SW2, GpioInputPU, F, 0);
 
+// Instantiates the actual producer and consumer objects for the given GPIO
+// pins from above. The ConfiguredConsumer class takes care of most of the
+// complicated setup and operation requirements. We need to give it the virtual
+// node pointer, the configuration configuration from the CDI definition, and
+// the hardware pin definition. The virtual node pointer comes from the stack
+// object. The configuration structure comes from the CDI definition object,
+// segment 'seg', in which there is a repeated group 'consumers', and we assign
+// the individual entries to the individual consumers. Each consumer gets its
+// own GPIO pin.
 nmranet::ConfiguredConsumer consumer_red(
-    stack.node(), cfg.seg().consumers().entry<0>(), BLINKER_Pin());
+    stack.node(), cfg.seg().consumers().entry<0>(), LED_RED_Pin());
 nmranet::ConfiguredConsumer consumer_green(
     stack.node(), cfg.seg().consumers().entry<1>(), LED_GREEN_Pin());
 nmranet::ConfiguredConsumer consumer_blue(
     stack.node(), cfg.seg().consumers().entry<2>(), LED_BLUE_Pin());
 
+// Similar syntax for the producers.
 nmranet::ConfiguredProducer producer_sw1(
     stack.node(), cfg.seg().producers().entry<0>(), SW1_Pin());
 nmranet::ConfiguredProducer producer_sw2(
     stack.node(), cfg.seg().producers().entry<1>(), SW2_Pin());
 
+// The producers need to be polled repeatedly for changes and to execute the
+// debouncing algorithm. This class instantiates a refreshloop and adds the two
+// producers to it.
 nmranet::RefreshLoop loop(
     stack.node(), {producer_sw1.polling(), producer_sw2.polling()});
 
@@ -88,6 +134,15 @@ nmranet::RefreshLoop loop(
  */
 int appl_main(int argc, char *argv[])
 {
+    // The necessary physical ports must be added to the stack.
+    //
+    // It is okay to enable multiple physical ports, in which case the stack
+    // will behave as a bridge between them. For example enabling both the
+    // physical CAN port and the USB port will make this firmware act as an
+    // USB-CAN adapter in addition to the producers/consumers created above.
+    //
+    // If a port is enabled, it must be functional or else the stack will
+    // freeze waiting for that port to send the packets out.
 #if defined(HAVE_PHYSICAL_CAN_PORT)
     stack.add_can_port_select("/dev/can0");
 #endif
@@ -98,6 +153,10 @@ int appl_main(int argc, char *argv[])
     stack.add_gridconnect_port("/dev/ser0");
 #endif
 
+    // This command donates the main thread to the operation of the
+    // stack. Alternatively the stack could be started in a separate stack and
+    // then application-specific business logic could be executed ion a busy
+    // loop in the main thread.
     stack.loop_executor();
     return 0;
 }
