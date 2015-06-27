c390c928300d565e6f6e14a59380f2df106deb69
Balazs Racz <balazs.racz@gmail.com>
2015-06-27 15:59:40 +0200

    Updates to new CDI definition format.

--- applications/io_board/targets/freertos.armv7m.ek-tm4c1294xl/config.hxx
+++ applications/io_board/targets/freertos.armv7m.ek-tm4c1294xl/config.hxx
@@ -22,9 +22,8 @@ namespace nmranet
 /// - the generated cdi.xml will include this data
 /// - the Simple Node Ident Info Protocol will return this data
 /// - the ACDI memory space will contain this data.
-extern const SimpleNodeStaticValues SNIP_STATIC_DATA = {
-    4,               "OpenMRN", "Test IO Board - Tiva Connected Launchpad",
-    "ek-tm4c1294xl", "1.01"};
+extern const SimpleNodeStaticValues SNIP_STATIC_DATA = {4, "OpenMRN",
+    "Test IO Board - Tiva Connected Launchpad", "ek-tm4c1294xl", "1.01"};
 
 /// Declares a repeated group of a given base group and number of repeats. The
 /// ProducerConfig and ConsumerConfig groups represent the configuration layout
@@ -35,41 +34,36 @@ using AllProducers = RepeatedGroup<ProducerConfig, 2>;
 
 /// Defines the main segment in the configuration CDI. This is laid out at
 /// origin 128 to give space for the ACDI user data at the beginning.
-BEGIN_GROUP(
-    IoBoardSegment, base, Segment(MemoryConfigDefs::SPACE_CONFIG), Offset(128));
-EXTEND_GROUP(
-    IoBoardSegment, base, consumers, AllConsumers, Name("Output LEDs"));
-/// The entries are chained: each entry refers to the previous entry, then
-/// declares the name of the current entry, then the type and then optional
-/// arguments list.
-EXTEND_GROUP(
-    IoBoardSegment, consumers, producers, AllProducers, Name("Input buttons"));
-END_GROUP(IoBoardSegment, producers);
+CDI_GROUP(IoBoardSegment, Segment(MemoryConfigDefs::SPACE_CONFIG), Offset(128));
+/// Each entry declares the name of the current entry, then the type and then
+/// optional arguments list.
+CDI_GROUP_ENTRY(consumers, AllConsumers, Name("Output LEDs"));
+CDI_GROUP_ENTRY(producers, AllProducers, Name("Input buttons"));
+CDI_GROUP_END();
 
 /// This segment is only needed temporarily until there is program code to set
 /// the ACDI user data version byte.
-BEGIN_GROUP(VersionSeg, base, Segment(MemoryConfigDefs::SPACE_CONFIG),
+CDI_GROUP(VersionSeg, Segment(MemoryConfigDefs::SPACE_CONFIG),
     Name("Version information"));
-EXTEND_GROUP(VersionSeg, base, acdi_user_version, Uint8ConfigEntry,
+CDI_GROUP_ENTRY(acdi_user_version, Uint8ConfigEntry,
     Name("ACDI User Data version"), Description("Set to 2 and do not change."));
-END_GROUP(VersionSeg, acdi_user_version);
+CDI_GROUP_END();
 
 /// The main structure of the CDI. ConfigDef is the symbol we use in main.cxx
 /// to refer to the configuration defined here.
-BEGIN_GROUP(ConfigDef, base, MainCdi());
+CDI_GROUP(ConfigDef, MainCdi());
 /// Adds the <identification> tag with the values from SNIP_STATIC_DATA above.
-EXTEND_GROUP(ConfigDef, base, ident, Identification);
+CDI_GROUP_ENTRY(ident, Identification);
 /// Adds an <acdi> tag.
-EXTEND_GROUP(ConfigDef, ident, acdi, Acdi);
+CDI_GROUP_ENTRY(acdi, Acdi);
 /// Adds a segment for changing the values in the ACDI user-defined
 /// space. UserInfoSegment is defined in the system header.
-EXTEND_GROUP(ConfigDef, acdi, userinfo, UserInfoSegment);
+CDI_GROUP_ENTRY(userinfo, UserInfoSegment);
 /// Adds the main configuration segment.
-EXTEND_GROUP(ConfigDef, userinfo, seg, IoBoardSegment);
+CDI_GROUP_ENTRY(seg, IoBoardSegment);
 /// Adds the versioning segment.
-EXTEND_GROUP(ConfigDef, seg, version, VersionSeg);
-/// To end the group we have to declare what was the last entry.
-END_GROUP(ConfigDef, version);
+CDI_GROUP_ENTRY(version, VersionSeg);
+CDI_GROUP_END();
 
 } // namespace nmranet
 
