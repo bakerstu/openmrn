ba4280bf1a41343b4f2dd4f3b08919735e011d5d
Balazs Racz <bracz@bracz-macbookpro.roam.corp.google.com>
2015-07-01 11:17:17 -0700

    removes unneeded (and partial) library directory.

--- etc/config.mk
+++ etc/config.mk
@@ -1,8 +1,8 @@
 ifndef MAKE_INC_CONFIG_MK
 MAKE_INC_CONFIG_MK := 1
 
-CORELIBS := utils executor os cue dcc nmranet
+CORELIBS := utils executor os dcc nmranet
 
-LINKCORELIBS = -lnmranet -ldcc -lcue -lexecutor -lutils -los
+LINKCORELIBS = -lnmranet -ldcc -lexecutor -lutils -los
 
 endif # MAKE_INC_CONFIG_MK
