b7bdaab5189ee1b7d63a432bf7a24b78527db655
Balazs Racz <balazs.racz@gmail.com>
2015-08-02 15:34:25 +0200

    Adds a new board bracz-railcom.

---
+++ boards/bracz-railcom/HwInit.cxx
@@ -0,0 +1,250 @@
+/** \copyright
+ * Copyright (c) 2012, Stuart W Baker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file HwInit.cxx
+ * This file represents the hardware initialization for the TI Tiva MCU.
+ *
+ * @author Stuart W. Baker
+ * @date 5 January 2013
+ */
+
+#include <new>
+#include <cstdint>
+
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "os/OS.hxx"
+#include "TivaDev.hxx"
+#include "TivaDCC.hxx"
+#include "TivaEEPROMEmulation.hxx"
+#include "DummyGPIO.hxx"
+#include "hardware.hxx"
+#include "TivaDCCDecoder.hxx"
+#include "TivaRailcom.hxx"
+#include "bootloader_hal.h"
+
+
+/*struct Debug {
+  // High between start_cutout and end_cutout from the TivaRailcom driver.
+  typedef DummyPin RailcomDriverCutout;
+  // Flips every time an uart byte is received error.
+  typedef DummyPin RailcomError;
+  // Flips every time an 'E0' byte is received in the railcom driver.
+  typedef DummyPin RailcomE0;
+  // Sets to 1 if we have seen any data from the railcom uart.
+  typedef DummyPin RailcomAnyData;
+  // Flips for every byte from the railcom uart.
+  typedef DummyPin RailcomDataReceived;
+  // Flipped for every packet that is sent from the railcom layer to the
+  // application.
+  typedef DummyPin RailcomPackets;
+  };*/
+#include "TivaRailcom.hxx"
+
+/** override stdin */
+const char *STDIN_DEVICE = "/dev/ser0";
+
+/** override stdout */
+const char *STDOUT_DEVICE = "/dev/ser0";
+
+/** override stderr */
+const char *STDERR_DEVICE = "/dev/ser0";
+
+/** UART 0 serial driver instance */
+static TivaUart uart0("/dev/ser0", UART0_BASE, INT_RESOLVE(INT_UART0_, 0));
+
+/** CAN 0 CAN driver instance */
+static TivaCan can0("/dev/can0", CAN0_BASE, INT_RESOLVE(INT_CAN0_, 0));
+
+extern const uint16_t __eeprom_start[];
+const uint16_t* const TivaEEPROMEmulation::raw = __eeprom_start;
+extern const uint16_t __eeprom_end[];
+const size_t TivaEEPROMEmulation::FLASH_SIZE = sizeof(__eeprom_end[0])*(__eeprom_end - __eeprom_start);
+const unsigned TivaEEPROMEmulation::FAMILY = TM4C123;
+const size_t TivaEEPROMEmulation::ADDRESS_SPACE = 512;
+const bool TivaEEPROMEmulation::SHADOW_IN_RAM = false;
+
+static TivaEEPROMEmulation eeprom("/dev/eeprom", 256);
+
+const uint32_t RailcomDefs::UART_BASE[] = RAILCOM_BASE;
+const uint32_t RailcomDefs::UART_PERIPH[] = RAILCOM_PERIPH;
+
+static TivaRailcomDriver<RailcomDefs> railcom_driver("/dev/railcom");
+/** The input pin for detecting the DCC signal. */
+static TivaDccDecoder<DCCDecode> nrz0("/dev/nrz0", &railcom_driver);
+
+TivaDAC<DACDefs> dac;
+
+extern "C" {
+/** Blink LED */
+uint32_t blinker_pattern = 0;
+static volatile uint32_t rest_pattern = 0;
+
+void dcc_generator_init(void);
+
+void hw_set_to_safe(void)
+{
+    GpioInit::hw_set_to_safe();
+}
+
+void enter_bootloader()
+{
+    /* Globally disables interrupts. */
+    asm("cpsid i\n");
+    hw_set_to_safe();
+    __bootloader_magic_ptr = REQUEST_BOOTLOADER;
+    extern char __flash_start;
+    asm volatile(" mov   r3, %[flash_addr] \n"
+                 :
+                 : [flash_addr] "r"(&__flash_start));
+    /* Loads SP and jumps to the reset vector. */
+    asm volatile(
+        " ldr r0, [r3]\n"
+        " mov sp, r0\n"
+        " ldr r0, [r3, #4]\n"
+        " bx  r0\n");
+}
+
+void reboot()
+{
+    MAP_SysCtlReset();
+}
+
+void resetblink(uint32_t pattern)
+{
+    blinker_pattern = pattern;
+    /* make a timer event trigger immediately */
+}
+
+void setblink(uint32_t pattern)
+{
+    resetblink(pattern);
+}
+
+void timer5a_interrupt_handler(void)
+{
+    //
+    // Clear the timer interrupt.
+    //
+    MAP_TimerIntClear(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
+    // Set output LED.
+    BLINKER_RAW_Pin::set((rest_pattern & 1));
+
+    // Shift and maybe reset pattern.
+    rest_pattern >>= 1;
+    if (!rest_pattern)
+        rest_pattern = blinker_pattern;
+}
+
+
+void timer2b_interrupt_handler(void)
+{
+  nrz0.interrupt_handler();
+}
+
+void timer2a_interrupt_handler(void)
+{
+  nrz0.os_interrupt_handler();
+}
+
+void uart1_interrupt_handler(void)
+{
+  railcom_driver.os_interrupt_handler();
+}
+
+void diewith(uint32_t pattern)
+{
+    vPortClearInterruptMask(0x20);
+    asm("cpsie i\n");
+
+    resetblink(pattern);
+    while (1)
+        ;
+}
+
+/** Initialize the processor hardware.
+ */
+void hw_preinit(void)
+{
+    /* Globally disables interrupts until the FreeRTOS scheduler is up. */
+    asm("cpsid i\n");
+
+    //
+    // Unlock PF0 so we can change it to a GPIO input
+    // Once we have enabled (unlocked) the commit register then re-lock it
+    // to prevent further changes.  PF0 is muxed with NMI thus a special case.
+    //
+    MAP_SysCtlPeripheralEnable(SW2_Pin::GPIO_PERIPH);
+    HWREG(SW2_Pin::GPIO_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(SW2_Pin::GPIO_BASE + GPIO_O_CR) |= 0x01;
+    HWREG(SW2_Pin::GPIO_BASE + GPIO_O_LOCK) = 0;
+
+    MAP_SysCtlPeripheralEnable(DAC_TIMER_Pin::GPIO_PERIPH);
+    HWREG(DAC_TIMER_Pin::GPIO_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(DAC_TIMER_Pin::GPIO_BASE + GPIO_O_CR) |= 0x80;
+    HWREG(DAC_TIMER_Pin::GPIO_BASE + GPIO_O_LOCK) = 0;
+
+    // Initializes all GPIO and hardware pins.
+    GpioInit::hw_init();
+
+    /* Setup the system clock. */
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    /* Blinker timer initialization. */
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER5);
+    MAP_TimerConfigure(TIMER5_BASE, TIMER_CFG_PERIODIC);
+    MAP_TimerLoadSet(TIMER5_BASE, TIMER_A, MAP_SysCtlClockGet() / 8);
+    MAP_IntEnable(INT_TIMER5A);
+
+    /* This interrupt should hit even during kernel operations. */
+    MAP_IntPrioritySet(INT_TIMER5A, 0);
+    MAP_TimerIntEnable(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
+    MAP_TimerEnable(TIMER5_BASE, TIMER_A);
+
+    /* Checks the SW1 pin at boot time in case we want to allow for a debugger
+     * to connect. */
+    asm volatile ("cpsie i\n");
+    do {
+      if (!SW2_Pin::get()) {
+        blinker_pattern = 0xAAAA;
+      } else {
+        blinker_pattern = 0;
+      }
+    } while (blinker_pattern || rest_pattern);
+    asm volatile ("cpsid i\n");
+}
+
+}
---
+++ boards/bracz-railcom/hardware.hxx
@@ -0,0 +1,227 @@
+#ifndef _HARDWARE_HXX_
+#define _HARDWARE_HXX_
+
+#include "TivaGPIO.hxx"
+#include "driverlib/rom_map.h"
+#include "utils/GpioInitializer.hxx"
+#include "custom/TivaDAC.hxx"
+#include "inc/hw_ints.h"
+#include "DummyGPIO.hxx"
+
+GPIO_PIN(SW1, GpioInputPU, F, 4);
+GPIO_PIN(SW2, GpioInputPU, F, 0);
+
+GPIO_PIN(LED_RED_RAW, LedPin, F, 1);
+GPIO_PIN(LED_GREEN, LedPin, F, 3);
+GPIO_PIN(LED_BLUE, LedPin, F, 2);
+
+typedef LED_RED_RAW_Pin BLINKER_RAW_Pin;
+
+GPIO_HWPIN(UART0RX, GpioHwPin, A, 0, U0RX, UART);
+GPIO_HWPIN(UART0TX, GpioHwPin, A, 1, U0TX, UART);
+
+GPIO_HWPIN(CAN0RX, GpioHwPin, B, 4, CAN0RX, CAN);
+GPIO_HWPIN(CAN0TX, GpioHwPin, E, 5, CAN0TX, CAN);
+
+GPIO_HWPIN(RAILCOM_CH0, GpioHwPin, D, 6, U2RX, UART);
+GPIO_HWPIN(RAILCOM_CH1, GpioHwPin, C, 6, U3RX, UART);
+GPIO_HWPIN(RAILCOM_CH2, GpioHwPin, C, 4, U4RX, UART);
+GPIO_HWPIN(RAILCOM_CH3, GpioHwPin, E, 0, U7RX, UART);
+GPIO_HWPIN(RAILCOM_CH4, GpioHwPin, E, 4, U5RX, UART);
+GPIO_HWPIN(RAILCOM_CH5, GpioHwPin, B, 0, U1RX, UART);
+
+GPIO_PIN(OUTPUT_EN0, GpioOutputSafeLow, A, 3);
+GPIO_PIN(OUTPUT_EN1, GpioOutputSafeLow, A, 4);
+GPIO_PIN(OUTPUT_EN2, GpioOutputSafeLow, C, 7);
+GPIO_PIN(OUTPUT_EN3, GpioOutputSafeLow, C, 5);
+GPIO_PIN(OUTPUT_EN4, GpioOutputSafeLow, E, 1);
+GPIO_PIN(OUTPUT_EN5, GpioOutputSafeLow, B, 5);
+
+typedef LED_GREEN_Pin STAT0_Pin;
+GPIO_PIN(STAT1, LedPin, E, 2);
+GPIO_PIN(STAT2, LedPin, E, 3);
+GPIO_PIN(STAT3, LedPin, A, 7);
+GPIO_PIN(STAT4, LedPin, A, 6);
+GPIO_PIN(STAT5, LedPin, A, 5);
+
+GPIO_HWPIN(DCC_IN, GpioHwPin, B, 1, T2CCP1, Timer);
+
+GPIO_HWPIN(DAC_TIMER, GpioHwPin, D, 7, WT5CCP1, Timer);
+GPIO_PIN(DAC_DIV, GpioOutputSafeHigh, A, 2);
+
+typedef GpioInitializer<                               //
+    SW1_Pin, SW2_Pin,                                  //
+    LED_RED_RAW_Pin, LED_GREEN_Pin, LED_BLUE_Pin,      //
+    UART0RX_Pin, UART0TX_Pin,                          //
+    RAILCOM_CH0_Pin, RAILCOM_CH1_Pin, RAILCOM_CH2_Pin, //
+    RAILCOM_CH3_Pin, RAILCOM_CH4_Pin, RAILCOM_CH5_Pin, //
+    OUTPUT_EN0_Pin, OUTPUT_EN1_Pin, OUTPUT_EN2_Pin,    //
+    OUTPUT_EN3_Pin, OUTPUT_EN4_Pin, OUTPUT_EN5_Pin,    //
+    STAT0_Pin, STAT1_Pin, STAT2_Pin,                   //
+    STAT3_Pin, STAT4_Pin, STAT5_Pin,                   //
+    DCC_IN_Pin,                                        //
+    DAC_TIMER_Pin, DAC_DIV_Pin,                        //
+    CAN0RX_Pin, CAN0TX_Pin> GpioInit;
+
+struct RailcomDefs
+{
+    static const uint32_t CHANNEL_COUNT = 6;
+    static const uint32_t UART_PERIPH[CHANNEL_COUNT];
+    static const uint32_t UART_BASE[CHANNEL_COUNT];
+    // Make sure there are enough entries here for all the channels times a few
+    // DCC packets.
+    static const uint32_t Q_SIZE = 24;
+
+    static const auto OS_INTERRUPT = INT_UART1;
+
+    static void hw_init()
+    {
+        RAILCOM_CH0_Pin::hw_init();
+        RAILCOM_CH1_Pin::hw_init();
+        RAILCOM_CH2_Pin::hw_init();
+        RAILCOM_CH3_Pin::hw_init();
+        RAILCOM_CH4_Pin::hw_init();
+        RAILCOM_CH5_Pin::hw_init();
+    }
+
+    static void set_input()
+    {
+        RAILCOM_CH0_Pin::set_input();
+        RAILCOM_CH1_Pin::set_input();
+        RAILCOM_CH2_Pin::set_input();
+        RAILCOM_CH3_Pin::set_input();
+        RAILCOM_CH4_Pin::set_input();
+        RAILCOM_CH5_Pin::set_input();
+    }
+
+    static void set_hw()
+    {
+        RAILCOM_CH0_Pin::set_hw();
+        RAILCOM_CH1_Pin::set_hw();
+        RAILCOM_CH2_Pin::set_hw();
+        RAILCOM_CH3_Pin::set_hw();
+        RAILCOM_CH4_Pin::set_hw();
+        RAILCOM_CH5_Pin::set_hw();
+    }
+
+    /** @returns a bitmask telling which pins are active. Bit 0 will be set if
+     * channel 0 is active (drawing current).*/
+    static uint8_t sample()
+    {
+        uint8_t ret = 0;
+        if (!RAILCOM_CH0_Pin::get())
+            ret |= 1;
+        if (!RAILCOM_CH1_Pin::get())
+            ret |= 2;
+        if (!RAILCOM_CH2_Pin::get())
+            ret |= 4;
+        if (!RAILCOM_CH3_Pin::get())
+            ret |= 8;
+        if (!RAILCOM_CH4_Pin::get())
+            ret |= 16;
+        if (!RAILCOM_CH5_Pin::get())
+            ret |= 32;
+        return ret;
+    }
+};
+
+#define RAILCOM_BASE                                                           \
+    {                                                                          \
+        UART2_BASE, UART3_BASE, UART4_BASE, UART7_BASE, UART5_BASE, UART1_BASE \
+    }
+
+#define RAILCOM_PERIPH                                                         \
+    {                                                                          \
+        SYSCTL_PERIPH_UART2, SYSCTL_PERIPH_UART3, SYSCTL_PERIPH_UART4,         \
+            SYSCTL_PERIPH_UART7, SYSCTL_PERIPH_UART5, SYSCTL_PERIPH_UART1      \
+    }
+
+struct DCCDecode
+{
+    static const auto TIMER_BASE = TIMER2_BASE;
+    static const auto TIMER_PERIPH = SYSCTL_PERIPH_TIMER2;
+    static const auto TIMER_INTERRUPT = INT_TIMER2B;
+    static const auto TIMER = TIMER_B;
+    static const auto CFG_CAP_TIME_UP =
+        TIMER_CFG_SPLIT_PAIR | TIMER_CFG_B_CAP_TIME_UP | TIMER_CFG_A_PWM;
+    // Interrupt bits.
+    static const auto TIMER_CAP_EVENT = TIMER_CAPB_EVENT;
+    static const auto TIMER_TIM_TIMEOUT = TIMER_TIMB_TIMEOUT;
+
+    static const auto OS_INTERRUPT = INT_TIMER2A;
+    typedef DCC_IN_Pin NRZ_Pin;
+
+    // 16-bit timer max + use 7 bits of prescaler.
+    static const uint32_t TIMER_MAX_VALUE = 0x800000UL;
+    static const uint32_t PS_MAX = 0x80;
+
+    static const int Q_SIZE = 32;
+};
+
+struct DACDefs
+{
+    // If false, the timerconfigure call will be skipped. This is needed when
+    // two halves of the same timer is shared between different devices.
+    static const bool config_timer = true;
+    static const auto TIMER_BASE = WTIMER5_BASE;
+    static const auto TIMER_PERIPH = SYSCTL_PERIPH_WTIMER5;
+    static const auto TIMER = TIMER_B;
+    static const auto TIMER_CFG =
+        TIMER_CFG_SPLIT_PAIR | TIMER_CFG_B_PWM | TIMER_CFG_A_PWM;
+
+    typedef DAC_TIMER_Pin TIMER_Pin;
+    typedef DAC_DIV_Pin DIV_Pin;
+};
+
+struct DACDefsxx
+{
+    // If false, the timerconfigure call will be skipped. This is needed when
+    // two halves of the same timer is shared between different devices.
+    static const bool config_timer = true;
+    static const auto TIMER_BASE = WTIMER1_BASE;
+    static const auto TIMER_PERIPH = SYSCTL_PERIPH_WTIMER1;
+    static const auto TIMER = TIMER_B;
+    static const auto TIMER_CFG =
+        TIMER_CFG_SPLIT_PAIR | TIMER_CFG_B_PWM | TIMER_CFG_A_PWM;
+
+    GPIO_HWPIN(TIMER, GpioHwPin, C, 7, WT1CCP1, Timer);
+
+    //typedef DAC_TIMER_Pin TIMER_Pin;
+    typedef DAC_DIV_Pin DIV_Pin;
+};
+
+extern TivaDAC<DACDefs> dac;
+
+struct Debug
+{
+    // One for the duration of the first byte successfully read from the uart
+    // device for railcom until the next 1 msec when the receiver gets disabled.
+    typedef DummyPin RailcomUartReceived;
+    // Measures the time from the end of a DCC packet until the entry of the
+    // user-space state flow.
+    typedef DummyPin DccPacketDelay;
+
+    // High between start_cutout and end_cutout from the TivaRailcom driver.
+    // typedef DBG_SIGNAL_Pin RailcomDriverCutout;
+    typedef DummyPin RailcomDriverCutout;
+
+    // Flips every timer capture interrupt from the dcc deocder flow.
+    // typedef LED_BLUE_SW_Pin DccDecodeInterrupts;
+    typedef DummyPin DccDecodeInterrupts;
+
+    // Flips every timer capture interrupt from the dcc deocder flow.
+    // typedef DBG_SIGNAL_Pin RailcomE0;
+    typedef LED_GREEN_Pin RailcomE0;
+
+    // Flips every timer capture interrupt from the dcc deocder flow.
+    typedef DummyPin RailcomError;
+    // typedef LED_BLUE_SW_Pin RailcomError;
+
+    typedef DummyPin RailcomDataReceived;
+    typedef DummyPin RailcomAnyData;
+    typedef DummyPin RailcomPackets;
+
+    typedef DummyPin DetectRepeat;
+};
+
+#endif // _HARDWARE_HXX_
