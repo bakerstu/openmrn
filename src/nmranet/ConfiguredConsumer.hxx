62aaf825328cc5dcf05569a338df1c214066a74e
Balazs Racz <balazs.racz@gmail.com>
2015-08-21 18:43:28 -0700

    Adds a consumer that outputs a pulse to the GPIO pin.

--- src/nmranet/ConfiguredConsumer.hxx
+++ src/nmranet/ConfiguredConsumer.hxx
@@ -40,6 +40,7 @@
 #include "nmranet/ConfigRepresentation.hxx"
 #include "utils/ConfigUpdateListener.hxx"
 #include "utils/ConfigUpdateService.hxx"
+#include "nmranet/RefreshLoop.hxx"
 
 namespace nmranet
 {
@@ -52,6 +53,17 @@ CDI_GROUP_ENTRY(event_off, EventConfigEntry, //
     Name("Event Off"),
     Description("Receiving this event ID will turn the output off."));
 CDI_GROUP_END();
+
+
+CDI_GROUP(PulseConsumerConfig);
+CDI_GROUP_ENTRY(event, EventConfigEntry, //
+    Name("Event"),
+    Description("Receiving this event ID will generate a pulso on the output."));
+CDI_GROUP_ENTRY(duration, Uint8ConfigEntry, //
+    Name("Pulse duration"),
+    Description("Length of the pulse to output (unit of 30 msec)."));
+CDI_GROUP_END();
+
 
 class ConfiguredConsumer : public ConfigUpdateListener
 {
@@ -99,6 +111,112 @@ private:
     Impl impl_;
     BitEventConsumer consumer_;
     const ConsumerConfig cfg_;
+};
+
+class ConfiguredPulseConsumer : public ConfigUpdateListener, private SimpleEventHandler, public Polling
+{
+public:
+    template <class HW>
+    ConfiguredPulseConsumer(Node *node, const PulseConsumerConfig &cfg, const HW &)
+        : node_(node), gpio_(HW::instance()), cfg_(cfg)
+    {
+        ConfigUpdateService::instance()->register_update_listener(this);
+    }
+
+    ConfiguredPulseConsumer(Node *node, const PulseConsumerConfig &cfg, Gpio* gpio)
+        : node_(node), gpio_(gpio), cfg_(cfg)
+    {
+        ConfigUpdateService::instance()->register_update_listener(this);
+    }
+
+    ~ConfiguredPulseConsumer() {
+        do_unregister();
+        ConfigUpdateService::instance()->unregister_update_listener(this);
+    }
+
+    UpdateAction apply_configuration(
+        int fd, bool initial_load, BarrierNotifiable *done) OVERRIDE
+    {
+        AutoNotify n(done);
+        EventId cfg_event_on = cfg_.event().read(fd);
+        pulseLength_ = cfg_.duration().read(fd);
+        if (cfg_event_on == event_) return UPDATED; // nothing to do
+        if (!initial_load) {
+            do_unregister();
+        }
+        event_ = cfg_event_on;
+        do_register();
+        return REINIT_NEEDED; // Causes events identify.
+    }
+
+    ///@TODO(balazs.racz): implement
+    void factory_reset(int fd) OVERRIDE
+    {
+    }
+
+private:
+    /// Registers the event handler with the global event registry.
+    void do_register() {
+        EventRegistry::instance()->register_handlerr(this, event_, 0);
+    }
+
+    /// Registers the event handler with the global event registry.
+    void do_unregister() {
+        EventRegistry::instance()->unregister_handlerr(this, event_, 0);
+    }
+    
+    // Implementations for the event handler functions.
+
+    void HandleIdentifyGlobal(EventReport* event,
+                              BarrierNotifiable* done) OVERRIDE {
+        if (event->dst_node && event->dst_node != node_) {
+            return done->notify();
+        }
+        SendConsumerIdentified(done);
+    }
+
+    void SendConsumerIdentified(BarrierNotifiable* done) OVERRIDE {
+        Defs::MTI mti = Defs::MTI_CONSUMER_IDENTIFIED_VALID;
+        if (!pulseRemaining_) {
+            mti++; // INVALID
+        }
+        event_write_helper3.WriteAsync(node_, mti, WriteHelper::global(),
+                                       eventid_to_buffer(event_),
+                                       done);
+    }
+    
+    void HandleIdentifyConsumer(EventReport* event,
+                                BarrierNotifiable* done) OVERRIDE {
+        if (event->event != event_) {
+            return done->notify();
+        }
+        SendConsumerIdentified(done);
+    }
+
+    void HandleEventReport(EventReport* event, BarrierNotifiable* done) OVERRIDE {
+        if (event->event == event_) {
+            pulseRemaining_ = pulseLength_;
+        }
+        done->notify();
+    }
+
+    // Polling interface
+    void poll_33hz(WriteHelper *helper, Notifiable *done) OVERRIDE {
+        if (pulseRemaining_ > 0) {
+            gpio_->set();
+            --pulseRemaining_;
+        } else {
+            gpio_->clr();
+        }
+        done->notify();
+    }
+
+    Node* node_; //< virtual node to export the consumer on
+    Gpio* gpio_; //< hardware output pin to drive
+    EventId event_{0}; //< Event ID to listen for
+    const PulseConsumerConfig cfg_; //< offset to the config in EEPROM
+    uint8_t pulseLength_{1}; //< length of pulse (in polling count)
+    uint8_t pulseRemaining_{0}; //< remaining polling count to keep pulse on
 };
 
 } // namespace nmranet
