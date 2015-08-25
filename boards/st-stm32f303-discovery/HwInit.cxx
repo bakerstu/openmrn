2a909345489ed407337856477a2e6d30d7ffeffa
Stuart Baker <stuart_w_baker@ymail.com>
2015-08-25 10:24:11 -0500

    Blink Raw STM32F3-discovery initial port

--- boards/st-stm32f303-discovery/HwInit.cxx
+++ boards/st-stm32f303-discovery/HwInit.cxx
@@ -34,14 +34,14 @@
 #include <new>
 #include <cstdint>
 
-#include "stm32f0xx_hal_rcc.h"
-#include "stm32f0xx_hal_flash.h"
-#include "stm32f0xx_hal_gpio.h"
-#include "stm32f0xx_hal_gpio_ex.h"
+#include "stm32f3xx_hal_rcc.h"
+#include "stm32f3xx_hal_flash.h"
+#include "stm32f3xx_hal_gpio.h"
+#include "stm32f3xx_hal_gpio_ex.h"
 
 #include "os/OS.hxx"
-#include "Stm32F0xxUart.hxx"
-#include "Stm32F0xxCan.hxx"
+#include "Stm32Uart.hxx"
+#include "Stm32Can.hxx"
 
 /** override stdin */
 const char *STDIN_DEVICE = "/dev/ser0";
@@ -53,10 +53,10 @@ const char *STDOUT_DEVICE = "/dev/ser0";
 const char *STDERR_DEVICE = "/dev/ser0";
 
 /** UART 0 serial driver instance */
-static Stm32Uart uart0("/dev/ser0", USART1, USART1_IRQn);
+//static Stm32Uart uart0("/dev/ser0", USART1, USART1_IRQn);
 
 /** CAN 0 CAN driver instance */
-static Stm32Can can0("/dev/can0");
+//static Stm32Can can0("/dev/can0");
 
 extern "C" {
 
@@ -71,6 +71,15 @@ void hw_set_to_safe(void)
 void resetblink(uint32_t pattern)
 {
     blinker_pattern = pattern;
+    
+    if (pattern)
+    {
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_SET);
+    }
+    else
+    {
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_RESET);
+    }
     /* make a timer event trigger immediately */
 }
 
@@ -106,9 +115,54 @@ void diewith(uint32_t pattern)
         ;
 }
 
-/** Setup the system clock */
+/** CPU clock speed. */
+const unsigned long cm3_cpu_clock_hz = 72000000;
+uint32_t SystemCoreClock = 72000000;
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            HSE PREDIV                     = 1
+  *            PLLMUL                         = 9
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
 static void clock_setup(void)
 {
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    	
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and
+     * PCLK2 clocks dividers
+     */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                                   RCC_CLOCKTYPE_PCLK1  | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+
+#if 0
     /* reset clock configuration to default state */
     RCC->CR = RCC_CR_HSITRIM_4 | RCC_CR_HSION;
     while (!(RCC->CR & RCC_CR_HSIRDY));
@@ -131,6 +185,9 @@ static void clock_setup(void)
     /* set PLL as system clock */
     RCC->CFGR = (RCC->CFGR & (~RCC_CFGR_SW)) | RCC_CFGR_SW_PLL;
     while (!((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_PLL));
+#else
+
+#endif
 }
 
 /** Initialize the processor hardware.
@@ -140,22 +197,29 @@ void hw_preinit(void)
     /* Globally disables interrupts until the FreeRTOS scheduler is up. */
     asm("cpsid i\n");
 
-    /* these FLASH settings enable opertion at 48 MHz */
+    /* these FLASH settings enable opertion at 72 MHz */
     __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-    __HAL_FLASH_SET_LATENCY(FLASH_LATENCY_1);
+    __HAL_FLASH_SET_LATENCY(FLASH_LATENCY_2);
 
     /* setup the system clock */
     clock_setup();
 
     /* enable peripheral clocks */
-    __GPIOA_CLK_ENABLE();
-    __GPIOB_CLK_ENABLE();
-    __USART1_CLK_ENABLE();
-    __CAN_CLK_ENABLE();
+    __GPIOE_CLK_ENABLE();
+    //__GPIOB_CLK_ENABLE();
+    //__USART1_CLK_ENABLE();
+    //__CAN_CLK_ENABLE();
+
+    GPIO_InitTypeDef gpio_init;
+    gpio_init.Pin = GPIO_PIN_9;
+    gpio_init.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio_init.Pull = GPIO_PULLUP;
+    gpio_init.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(GPIOE, &gpio_init);
 
     /* setup pinmux */
+#if 0
     GPIO_InitTypeDef gpio_init;
-
     /* USART1 pinmux on PA9 and PA10 */
     gpio_init.Mode      = GPIO_MODE_AF_PP;
     gpio_init.Pull      = GPIO_PULLUP;
@@ -175,6 +239,7 @@ void hw_preinit(void)
     HAL_GPIO_Init(GPIOB, &gpio_init);
     gpio_init.Pin       = GPIO_PIN_9;
     HAL_GPIO_Init(GPIOB, &gpio_init);
+#endif
 }
 
 }
