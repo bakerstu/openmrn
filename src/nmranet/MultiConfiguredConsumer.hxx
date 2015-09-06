a28e02a73ffc29761f464ed28dc19da9059718ce
Balazs Racz <balazs.racz@gmail.com>
2015-09-06 19:19:18 +0200

    Implements remaining functions for MultiConfiguredConsumer.

--- src/nmranet/MultiConfiguredConsumer.hxx
+++ src/nmranet/MultiConfiguredConsumer.hxx
@@ -47,17 +47,19 @@ class MultiConfiguredConsumer : public ConfigUpdateListener,
 public:
     typedef ConsumerConfig config_entry_type;
 
-    template <unsigned N>
-    MultiConfiguredConsumer(const Gpio *const pins[N],
+    template <unsigned N> __attribute__((noinline))
+    MultiConfiguredConsumer(const Gpio *const *pins, unsigned size,
                             const RepeatedGroup<config_entry_type, N> &config)
         : pins_(pins)
         , size_(N)
         , offset_(config)
     {
+        HASSERT(size_ == N);
         ConfigUpdateService::instance()->register_update_listener(this);
     }
 
-    ~MultiConfiguredConsumer() {
+    ~MultiConfiguredConsumer()
+    {
         do_unregister();
         ConfigUpdateService::instance()->unregister_update_listener(this);
     }
@@ -76,15 +78,16 @@ public:
             // is coming from a user action.
             do_unregister();
         }
-        const RepeatedGroup<config_entry_type, UINT_MAX> grp_ref(offset_.offset());
-        for (unsigned i = 0; i < size_; ++i) {
+        RepeatedGroup<config_entry_type, UINT_MAX> grp_ref(offset_.offset());
+        for (unsigned i = 0; i < size_; ++i)
+        {
             const config_entry_type cfg_ref(grp_ref.entry(i));
             EventId cfg_event_on = cfg_ref.event_on().read(fd);
             EventId cfg_event_off = cfg_ref.event_off().read(fd);
             EventRegistry::instance()->register_handler(
-                EventRegistryEntry(this, cfg_event_on, i*2), 0);
+                EventRegistryEntry(this, cfg_event_off, i * 2), 0);
             EventRegistry::instance()->register_handler(
-                EventRegistryEntry(this, cfg_event_off, i*2 + 1), 0);
+                EventRegistryEntry(this, cfg_event_on, i * 2 + 1), 0);
         }
         return REINIT_NEEDED; // Causes events identify.
     }
@@ -93,9 +96,60 @@ public:
     void factory_reset(int fd) OVERRIDE
     {
     }
-    
+
+    // Implementations for the event handler functions.
+
+    void HandleIdentifyGlobal(const EventRegistryEntry &registry_entry,
+                              EventReport *event, BarrierNotifiable *done)
+        OVERRIDE
+    {
+        if (event->dst_node && event->dst_node != node_)
+        {
+            return done->notify();
+        }
+        SendConsumerIdentified(registry_entry, done);
+    }
+
+    void HandleIdentifyConsumer(const EventRegistryEntry &registry_entry,
+                                EventReport *event, BarrierNotifiable *done)
+        OVERRIDE
+    {
+        if (event->event != registry_entry.event)
+        {
+            return done->notify();
+        }
+        SendConsumerIdentified(registry_entry, done);
+    }
+
+    void HandleEventReport(const EventRegistryEntry &registry_entry,
+                           EventReport *event, BarrierNotifiable *done) OVERRIDE
+    {
+        if (event->event != registry_entry.event)
+        {
+            return done->notify();
+        }
+        const Gpio *pin = pins_[registry_entry.user_arg >> 1];
+        const bool is_on = (registry_entry.user_arg & 1);
+        pin->write(is_on);
+        done->notify();
+    }
 
 private:
+    /// Sends out a ConsumerIdentified message for the given registration
+    /// entry.
+    void SendConsumerIdentified(const EventRegistryEntry &registry_entry,
+                                BarrierNotifiable *done)
+    {
+        Defs::MTI mti = Defs::MTI_CONSUMER_IDENTIFIED_VALID;
+        if (pins_[registry_entry.user_arg >> 1]->is_clr())
+        {
+            mti++; // INVALID
+        }
+        event_write_helper3.WriteAsync(node_, mti, WriteHelper::global(),
+                                       eventid_to_buffer(registry_entry.event),
+                                       done);
+    }
+
     /// Removed registration of this event handler from the global event
     /// registry.
     void do_unregister()
@@ -103,9 +157,11 @@ private:
         EventRegistry::instance()->unregister_handler(this);
     }
 
-    const Gpio *const *pins_;
-    size_t size_;
-    ConfigReference offset_;
+    Node *node_;              //< virtual node to export the consumer on
+    const Gpio *const *pins_; //< array of all GPIO pins to use
+    size_t size_;             //< number of GPIO pins to export
+    ConfigReference offset_;  //< Offset in the configuration space for our
+    // configs.
 };
 
 } // namespace nmranet
