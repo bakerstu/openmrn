3bdf1cb8ac45d9158f2bb8453bb15a6f8b0386be
Stuart Baker <stuart_w_baker@ymail.com>
2015-11-12 18:56:53 -0600

    fix copyright date

--- applications/js_client/client.cxx
+++ applications/js_client/client.cxx
@@ -1,5 +1,5 @@
 /** \copyright
- * Copyright (c) 2013, Balazs Racz
+ * Copyright (c) 2015, Balazs Racz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
