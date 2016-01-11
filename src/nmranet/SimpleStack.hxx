f310a092e70a05196a8085a3537c1b1488cec820
Balazs Racz <balazs.racz@gmail.com>
2016-01-11 22:25:26 +0100

    Ensures that the SNIP handler only replies to the node that it is set to reply to.

--- src/nmranet/SimpleStack.hxx
+++ src/nmranet/SimpleStack.hxx
@@ -247,6 +247,11 @@ public:
     /// Overwrites all events in the eeprom with a brand new event ID.
     void factory_reset_all_events(const InternalConfigData &ofs, int fd);
 
+    /// Accessor for clients that have their custom SNIP-like handler.
+    SimpleInfoFlow* info_flow() {
+        return &infoFlow_;
+    }
+
 private:
     static const auto PIP_RESPONSE =
         Defs::EVENT_EXCHANGE | Defs::PROTOCOL_IDENTIFICATION | Defs::DATAGRAM |
@@ -282,7 +287,7 @@ private:
     /// General flow for simple info requests.
     SimpleInfoFlow infoFlow_{&ifCan_};
     /// Handles SNIP requests.
-    SNIPHandler snipHandler_{&ifCan_, &infoFlow_};
+    SNIPHandler snipHandler_{&ifCan_, &node_, &infoFlow_};
 
     CanDatagramService datagramService_{&ifCan_,
         config_num_datagram_registry_entries(), config_num_datagram_clients()};
