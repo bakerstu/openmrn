162c541741da7c1669379e585dd820fa7838df7d
Balazs Racz <balazs.racz@gmail.com>
2016-02-07 14:18:26 -0600

    Adds a simple itoa utility.

---
+++ src/utils/format_utils.hxx
@@ -0,0 +1,44 @@
+/** \copyright
+ * Copyright (c) 2016, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file format_utils.hxx
+ *
+ * Simple codefor some trivial formatting uses.
+ *
+ * @author Balazs Racz
+ * @date 7 Feb 2016
+ */
+
+#ifndef _UTILS_FORMAT_UTILS_HXX_
+#define _UTILS_FORMAT_UTILS_HXX_
+
+/** Renders an integer to string, left-justified. @param buffer must be an at
+ * least 10 character long array. @param value will be rendered into the
+ * buffer. @returns the pointer to the null character at the end of the
+ * rendering. */
+char* integer_to_buffer(int value, char* buffer);
+
+#endif // _UTILS_FORMAT_UTILS_HXX_
