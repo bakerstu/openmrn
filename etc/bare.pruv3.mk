99188f1784d946c51cf3d627b40e4d3a45ef1ccb
Stuart Baker <stuart_w_baker@ymail.com>
2016-02-11 19:22:39 -0600

    Update some PRU build settings.

--- etc/bare.pruv3.mk
+++ etc/bare.pruv3.mk
@@ -7,6 +7,8 @@ TOOLPATH:=$(PRUCGTPATH)
 
 PREFIX = $(TOOLPATH)/bin/
 
+DEPS += TILINUXSDKPATH PRUICSSPATH
+
 AS = $(PREFIX)asmpru
 CC = $(PREFIX)clpru
 CXX = $(PREFIX)clpru
@@ -33,12 +35,13 @@ CFLAGS += -v3 -O4 --opt_for_speed=0 -g --c99 --define=am3359 --define=pru0 \
           --diag_warning=225 --display_error_number --diag_wrap=off \
           --endian=little --hardware_mac=on --obj_extension=.o \
           --emit_warnings_as_errors --verbose_diagnostics \
+          --preproc_with_compile --preproc_dependency=$*.d \
           $(INCLUDES) $(CFLAGSEXTRA) $(CFLAGSENV)
 
 LDFLAGS += -v3 -O4 --opt_for_speed=0 -g --c99 \
            --diag_warning=225 --display_error_number --diag_wrap=off \
            --endian=little --hardware_mac=on -z --reread_libs \
-           --warn_sections --rom_model target.cmd \
+           --warn_sections --rom_model target.cmd -m$(EXECUTABLE).map\
            -i$(TOOLPATH)/lib $(LDFLAGSEXTRA) $(LDFLAGSENV)
 
 SYSLIB_SUBDIRS +=
