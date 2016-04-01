c28e5fe519a4d0904bcc8471055bcc1e9bcbdcd6
Stuart Baker <stuart_w_baker@ymail.com>
2016-03-31 21:28:08 -0500

    Console CommandFlow added and 100% line coverage in Unit Test.

--- src/console/Console.hxx
+++ src/console/Console.hxx
@@ -47,15 +47,43 @@
 
 /** This class provides a console available from stdin/stdout as well as via
  * telnet.  Adding a command to the console is as simple as using the @ref
- * add_command method.  A command callback is responsible for providing its own
+ * add_command() method or instantiating a new derived instance if @ref
+ * CommandFlow.  A command callback is responsible for providing its own
  * help text.  When the command callback is called with argc = 0, this is a
  * clue to the callback to print its help information.  In this case, argv[0]
  * will still have the command name itself, but argv[1] will have an additional
  * parameter that can be used to indent and align multi-line help information.
+ *
+ * New CommandFlow instances and add_command() calls must occur either in
+ * global construction (before main() is called), or within the same executor
+ * context as the Console instance they are being attached to in order to avoid
+ * a problem with mutual exclusion.
  */
 class Console : public Service
 {
+private:
+    /** forward declaration */
+    struct Command;
+
+    /** forward declaration */
+    class Session;
+
 public:
+    /** Enumeration of recognized command callback results.
+     */
+    enum CommandStatus
+    {
+        COMMAND_OK,        /**< Command executed successfully */
+        COMMAND_NEXT,      /**< Command waiting for input */
+        COMMAND_ERROR,     /**< Command had some kind of error */
+        COMMAND_CLOSE,     /**< Command wants to close the session. */
+        COMMAND_NOT_FOUND, /**< Command not found */
+    };
+
+    /** Console command callback.
+     */
+    typedef CommandStatus (*Callback)(FILE *, int, const char *argv[], void *);
+
     /** Constructor.
      * @param executor the executor thread that the Console flows will execute
      *                 on
@@ -63,7 +91,17 @@ public:
      * @param port TCP port number to open a telnet listen socket on, -1 mean
      *        do not open a listen port.
      */
-    Console(ExecutorBase *exectutor, bool stdio, int port = -1);
+    Console(ExecutorBase *exectutor, uint16_t port);
+
+    /** Constructor.
+     * @param executor the executor thread that the Console flows will execute
+     *                 on
+     * @param fd_in input file descriptor for the session
+     * @param fd_out output file descriptor for the session
+     * @param port TCP port number to open a telnet listen socket on, -1 mean
+     *        do not open a listen port.
+     */
+    Console(ExecutorBase *exectutor, int fd_in, int fd_out, int port = -1);
 
     /** Destructor.
      */
@@ -71,18 +109,14 @@ public:
     {
     }
 
-    /** Enumeration of recognized command callback results.
+    /** Add a console session.
+     * @param fd_in input file descriptor for the session
+     * @param fd_out output file descriptor for the session
      */
-    enum
+    void add_session(int fd_in, int fd_out)
     {
-        COMMAND_OK,    /**< Command executed successfully */
-        COMMAND_ERROR, /**< Command had some kind of error */
-        COMMAND_CLOSE, /**< Command wants to close the session. */
-    };
-
-    /** Console command callback.
-     */
-    typedef int (*Callback)(FILE *, int, const char *argv[], void *);
+        open_session(fd_in, fd_out);
+    }
 
     /** Add a new command to the console.
      * @param name command name
@@ -90,6 +124,104 @@ public:
      * @param context context pointer to pass into callback
      */
     void add_command(const char *name, Callback callback, void *context = NULL);
+
+    /** Default STDIN file descriptor */
+    static const int FD_STDIN = 0;
+
+    /** Default STDOUT file descriptor */
+    static const int FD_STDOUT = 1;
+
+    /** State flow base class that handles interactive commands.
+     * This may also be used for non-interactive commands that want to use
+     * the StateFlow paradigm for responding.
+     */
+    class CommandFlow : public StateFlowBase
+    {
+    protected:
+        /** Constructor.
+         * @param console Console instance that this belongs to
+         * @param name command name
+         */
+        CommandFlow(Console *console, const char *name);
+
+        /** Destructor.
+         */
+        ~CommandFlow();
+
+        /** Entry point to command flow.
+         * @return defined by derived class
+         */
+        virtual StateFlowBase::Action entry() = 0;
+
+        /** Read a line of input
+         * @return next state wait_for_line()
+         */
+        StateFlowBase::Action read_line();
+
+        /** number of arguments on command line */
+        int argc;
+
+        /** list of arguments on command line */
+        const char **argv;
+
+        /** FILE* reference for output data */
+        FILE *fp;
+
+        /** Wait for a complete line of input
+         * @return 
+         */
+        StateFlowBase::Action wait_for_line_and_call(StateFlowBase::Callback c)
+        {
+            fflush(fp);
+            this->status = COMMAND_NEXT;
+            session->notify();
+            return wait_and_call(c);
+        }
+
+        /** Record the CommandStatus and exit the CommandFlow.
+         * @param status exit status of the command
+         */
+        StateFlowBase::Action record_status_and_exit(CommandStatus status)
+        {
+            this->status = status;
+            session->notify();
+            return STATE(exit);
+        }
+
+    private:
+        /** Start flow with the incoming data parameters */
+        CommandStatus callback(Session *session, int fd, FILE *fp, int argc, const char *argv[])
+        {
+            this->session = session;
+            this->fd = fd;
+            this->fp = fp;
+            this->argc = argc;
+            this->argv = argv;
+            start_flow(STATE(entry));
+
+            return COMMAND_NEXT;
+        }
+
+        /** Keep track of Command instance for destruction time */
+        Command *command;
+
+        /** file descriptor for input data */
+        int fd;
+
+        /** Session flow to notify when command is complete */
+        Session *session;
+
+        /** Resulting status of running command */
+        CommandStatus status;
+
+        /** Give Console class access to CommandFlow private members */
+        friend class Console;
+
+        /** Give Session class access to Commandflow private members */
+        friend class Session;
+
+        DISALLOW_COPY_AND_ASSIGN(CommandFlow);
+    };
 
 private:
     /** Maximum number of supported arguments including the command itself */
@@ -109,14 +241,37 @@ private:
             : name(name)
             , callback(callback)
             , context(context)
+            , interactive(false)
+            , next(next)
+        {
+        }
+
+        /** Construct a new command.
+         * @param name command name
+         * @param callback callback function for command
+         * @param context context pointer to pass into callback
+         * @param next next command in list
+         */
+        Command(const char *name, CommandFlow *flow, Command *next = NULL)
+            : name(name)
+            , flow(flow)
+            , interactive(true)
             , next(next)
         {
         }
 
         const char *name;  /**< command name */
-        Callback callback; /**< callback function for command */
-        void *context;     /**< context pointer to pass into callback */
-        Command *next;     /**< next Command in list */
+        union 
+        {
+            struct
+            {
+                Callback callback; /**< callback function for command */
+                void *context;     /**< context pointer to pass into callback */
+            };
+            CommandFlow *flow; /**< state flow for interactive commands */
+        };
+        bool interactive; /**< true if command is interactive */
+        Command *next;    /**< next Command in list */
     };
 
 #if defined (CONSOLE_NETWORKING)
@@ -162,16 +317,19 @@ private:
     public:
         /** Constructor.
          * @param service service instance that this session belongs to
-         * @param fd file descriptor for the session
+         * @param fd_in input file descriptor for the session
+         * @param fd_out output file descriptor for the session
          */
-        Session(Service *service, int fd)
+        Session(Service *service, int fd_in, int fd_out)
             : StateFlowBase(service)
-            , fd(fd)
-            , fp(fdopen(fd, "r+"))
+            , fdIn(fd_in)
+            , fdOut(fd_out)
+            , fp(fdopen(fd_out, "w"))
             , line((char*)malloc(64))
             , line_size(64)
             , pos(0)
             , selectHelper(this)
+            , command(nullptr)
         {
             prompt(fp);
             start_flow(STATE(entry));
@@ -181,25 +339,43 @@ private:
          */
         ~Session()
         {
+            /* should only delete sockets, which should have the same read
+             * and write fds
+             */
+            HASSERT(fdIn == fdOut);
             fclose(fp);
-            close(fd);
+            close(fdIn);
             free(line);
         }
 
     private:
-        /** Entry point to the state machine.
-         * @return next state is accept() pending an active listen socket
+        /** Entry point to the state machine.  Read some command line input.
+         * @return next state is process_read()
          */
         StateFlowBase::Action entry()
         {
-            return read_single(&selectHelper, fd, line + pos,
+            return read_single(&selectHelper, fdIn, line + pos,
                                line_size - pos, STATE(process_read));
         }
 
-        /** Entry point to the state machine.
-         * @return next state is accept() pending an active listen socket
+        /** Process the incoming command line input.
+         * @return next state is entry() to gather more data, exit_interactive()
+         *         if an interactive command is called.
          */
         StateFlowBase::Action process_read();
+
+        /** Wait for completion of an interactive command in order to cleanup
+         * based on result.
+         * @return next state is entry()
+         */
+        StateFlowBase::Action exit_interactive();
+
+        /** Process a potential callback for a given command
+         * @param argc number of arguments including the command itself
+         * @param argv array of arguments starting with the command itself
+         * @return status of the callback being called
+         */
+        CommandStatus callback(int argc, const char *argv[]);
 
         /** Print the standard prompt.
          * @param fp FILE pointer to send prompt to
@@ -210,23 +386,30 @@ private:
             fflush(fp);
         }
 
-        int fd;           /**< file descriptor of session */
+        /** Process the result of the command callback
+         * @param status the result of running a command
+         * @param name name of the command itself
+         * @return false to close the session, else true
+         */
+        bool callback_result_process(CommandStatus status, const char *name);
+
+        int fdIn;         /**< input file descriptor of the session */
+        int fdOut;        /**< output file descriptor of the session */
         FILE *fp;         /**< file pointer of session */
         char *line;       /**< current line content */
         size_t line_size; /**< current max line size */
         size_t pos;       /**< current line position */
+        const char *args[MAX_ARGS]; /**< parsed argument list */
 
         /** metadata for waiting on the listen socket to become active */
         StateFlowBase::StateFlowSelectHelper selectHelper;
 
+        /** Command instance that we are currently acting on */
+        Command *command;
+
         DISALLOW_COPY_AND_ASSIGN(Session);
     };
 
-    Command help;     /**< the "help" command instance */
-    Command helpMark; /**< the help "?" command instance */
-#if defined (CONSOLE_NETWORKING)
-    Listen listen; /**< object that will listen for incoming connections */
-#endif
     /** Print out the help menu by calling the in context helper function.
      * @param fp file pointer to console
      * @param argc number of arguments including the command itself
@@ -234,7 +417,7 @@ private:
      * @param context pointer to a Console context
      * @return COMMAND_OK
      */
-    static int help_command(FILE *fp, int argc, const char *argv[], void *context)
+    static CommandStatus help_command(FILE *fp, int argc, const char *argv[], void *context)
     {
         return static_cast<Console*>(context)->help_command(fp, argc, argv);
     }
@@ -245,7 +428,7 @@ private:
      * @param argv array of arguments starting with the command itself
      * @return COMMAND_OK
      */
-    int help_command(FILE *fp, int argc, const char *argv[]);
+    CommandStatus help_command(FILE *fp, int argc, const char *argv[]);
 
     /** Quit out of the current login session by calling the in context helper
      * function.
@@ -255,7 +438,7 @@ private:
      * @param context pointer to a Console context
      * @return COMMAND_OK
      */
-    static int quit_command(FILE *fp, int argc, const char *argv[], void *context)
+    static CommandStatus quit_command(FILE *fp, int argc, const char *argv[], void *context)
     {
         return static_cast<Console*>(context)->quit_command(fp, argc, argv);
     }
@@ -266,26 +449,29 @@ private:
      * @param argv array of arguments starting with the command itself
      * @return COMMAND_OK
      */
-    int quit_command(FILE *fp, int argc, const char *argv[]);
+    CommandStatus quit_command(FILE *fp, int argc, const char *argv[]);
 
     /** Open and initialize a new session.
-     * @param fd file descriptor belonging to session
+     * @param fd_in input file descriptor belonging to session
+     * @param fd_out output file descriptor belonging to session
      */
-    void open_session(int fd);
+    void open_session(int fd_in, int fd_out);
 
-    /** Process a potential callback for a given command
-     * @param fp file pointer to console
-     * @param argc number of arguments including the command itself
-     * @param argv array of arguments starting with the command itself
-     * @return if false, close the session
-     */
-    bool callback(FILE *fp, int argc, const char *argv[]);
+    Command help;     /**< the "help" command instance */
+    Command helpMark; /**< the help "?" command instance */
+#if defined (CONSOLE_NETWORKING)
+    Listen listen; /**< object that will listen for incoming connections */
+#endif
+    /** Give CommandFlow class access to Console private members */
+    friend class CommandFlow;
 
     /** Give Listen class access to Console private members */
     friend class Listen;
 
     /** Give Session class access to Console private members */
     friend class Session;
+
+    DISALLOW_COPY_AND_ASSIGN(Console);
 };
 
 #endif // _CONSOLE_CONSOLE_HXX_
