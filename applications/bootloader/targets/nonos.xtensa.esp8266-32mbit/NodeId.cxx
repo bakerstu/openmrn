50ccb573608892b9e5fc82e8378572b276f53d31
Balazs Racz <balazs.racz@gmail.com>
2016-06-19 11:30:39 +0200

    Copies code for implementing a bootloader for the esp8266.

---
+++ applications/bootloader/targets/nonos.xtensa.esp8266-32mbit/NodeId.cxx
@@ -0,0 +1,10 @@
+#include "nmranet/If.hxx"
+
+extern const nmranet::NodeID NODE_ID;
+const nmranet::NodeID NODE_ID = 0x050101011800ULL | NODEID_LOW_BITS;
+extern const uint16_t DEFAULT_ALIAS;
+const uint16_t DEFAULT_ALIAS = 0x400 | NODEID_LOW_BITS;
+
+#define BOOTLOADER_STREAM
+//#define BOOTLOADER_DATAGRAM
+#include "nmranet/Bootloader.hxx"
