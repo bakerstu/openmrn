370d46a9a5c87b1a1b80146a248ad678ed16a54c
Balazs Racz <balazs.racz@gmail.com>
2016-06-26 13:09:33 +0200

    Makes the low bits of the train bootloader's address configurable.

--- applications/bootloader/targets/nonos.xtensa.esp8266.bracz-deadrail-proto/NodeId.cxx
+++ applications/bootloader/targets/nonos.xtensa.esp8266.bracz-deadrail-proto/NodeId.cxx
@@ -1,6 +1,5 @@
 #include "nmranet/If.hxx"
-
-#define NODEID_LOW_BITS 0xDE
+#include "address.h"
 
 extern const nmranet::NodeID NODE_ID;
 const nmranet::NodeID NODE_ID = 0x050101011400ULL | NODEID_LOW_BITS;
