9ff9863b07d01786e1f5ca88b91cdf9a16302a49
Balazs Racz <balazs.racz@gmail.com>
2016-08-01 21:16:27 +0200

    Fixes numeric syntax error that caused strange blinking.

--- src/freertos_drivers/common/WifiDefs.hxx
+++ src/freertos_drivers/common/WifiDefs.hxx
@@ -1,7 +1,7 @@
 
 
 /// Wifi not associated to access point: continuous short blinks.
-#define WIFI_BLINK_NOTASSOCIATED  0xb1010
+#define WIFI_BLINK_NOTASSOCIATED  0b1010
 /// Waiting for IP address: double short blink, pause, double short blink, ...
 #define WIFI_BLINK_ASSOC_NOIP  0b101000
 /// Connecting to hub: long blinks
