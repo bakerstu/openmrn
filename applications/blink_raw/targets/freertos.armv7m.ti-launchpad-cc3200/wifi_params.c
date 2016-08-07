c1536a20c398042be3115622202538692d89d013
Stuart Baker <stuart_w_baker@ymail.com>
2016-08-07 09:29:33 -0500

    fix broken top level build.

---
+++ applications/blink_raw/targets/freertos.armv7m.ti-launchpad-cc3200/wifi_params.c
@@ -0,0 +1,9 @@
+/// Name of wifi accesspoint to connect to.
+char WIFI_SSID[] = "CC31xxSSID";
+/// Password of wifi connection. If empty, use no encryption.
+char WIFI_PASS[] = "testtest";
+/// Hostname at which the OpenLCB hub is at.
+char WIFI_HUB_HOSTNAME[] = "172.168.1.10";
+/// Port number of the OpenLCB hub.
+int WIFI_HUB_PORT = 12021;
+
