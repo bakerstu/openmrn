fa6ba3d41559207b3807e30a0ec2ff9d430806ee
Balazs Racz <balazs.racz@gmail.com>
2016-09-10 14:49:15 +0200

    Allows circular dependency between utils and executor directories.

--- etc/config.mk
+++ etc/config.mk
@@ -4,7 +4,7 @@ MAKE_INC_CONFIG_MK := 1
 ifneq ($(TARGET),bare.pruv3)
 CORELIBS := console utils executor os dcc nmranet
 
-LINKCORELIBS = -lconsole -lnmranet -ldcc -lexecutor -lutils -los
+LINKCORELIBS = -lconsole -lnmranet -ldcc $(STARTGROUP) -lexecutor -lutils $(ENDGROUP) -los
 endif
 
 endif # MAKE_INC_CONFIG_MK
