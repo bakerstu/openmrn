058365d66e3a2e6fa639cdb5ebd096e3c6dfe066
Balazs Racz <balazs.racz@gmail.com>
2016-09-10 15:01:25 +0200

    a different way to ensure cyclic deps between executor and utils works.

--- etc/config.mk
+++ etc/config.mk
@@ -4,7 +4,7 @@ MAKE_INC_CONFIG_MK := 1
 ifneq ($(TARGET),bare.pruv3)
 CORELIBS := console utils executor os dcc nmranet
 
-LINKCORELIBS = -lconsole -lnmranet -ldcc $(STARTGROUP) -lexecutor -lutils $(ENDGROUP) -los
+LINKCORELIBS = -lconsole -lnmranet -ldcc -lexecutor -lutils -lexecutor -los
 endif
 
 endif # MAKE_INC_CONFIG_MK
