d4089a0b0fcc846f1ebac9ccd0fa01208987be6a
Balazs Racz <balazs.racz@gmail.com>
2017-06-25 12:56:50 +0200

    Adds the hard fault hander C to the seuqence of calls that happen during a hardfault. This will save a bunch of extra debugging info for hard faults. Ensures that hw_set_to_safe gets called during hard fault. Restores the register values for r0, r1, r2, r3 before calling the breakpoint so that the disassembly and "info registers" gives correct information when tested.

--- boards/armv7m/default_handlers.h
+++ boards/armv7m/default_handlers.h
@@ -37,6 +37,8 @@
 
 #define BOARDS_DEFAULT_HANDLERS_H
 
+#include "utils/macros.h"
+
 /* prototypes */
 extern unsigned long *__stack;
 extern void reset_handler(void);
@@ -60,6 +62,7 @@ extern unsigned long __bss_section_table_end;
 /** This hardware initialization code will be called before C++ global objects
  * are initialized. */
 extern void hw_preinit(void);
+extern void resetblink(unsigned long pattern);
 
 /** Sets the hardware outputs to a safe state. Called when the program crashes
  * handler. */
@@ -119,8 +122,9 @@ void reset_handler(void)
 }
 #endif // SKIP_RESET_HANDLER
 
-static uint32_t g_saved_lr;
-static uint32_t g_saved_pc;
+void hard_fault_handler_step_2(unsigned long *hardfault_args);
+void hard_fault_handler_step_3(void);
+
 
 __attribute__((__naked__)) static void hard_fault_handler(void)
 {
@@ -130,7 +134,10 @@ __attribute__((__naked__)) static void hard_fault_handler(void)
         " ite   eq                   \n"
         " mrseq r0, msp              \n"
         " mrsne r0, psp              \n"
-        // saves our return address
+        " ldr r1, =hard_fault_handler_step_2 \n"
+        " bx r1 \n");
+#if 0
+    // saves our return address
         " mov   r2, lr               \n"
         // Simulates a BL instruction from the original PC. Moves the PC to LR,
         // overwrites PC with our return address.
@@ -150,23 +157,106 @@ __attribute__((__naked__)) static void hard_fault_handler(void)
         " bx    lr                   \n"
     );
     g_saved_lr = g_saved_pc = 0;
+#endif
+
 }
 
-void hard_fault_stub(void) {
+/** Fault handling information */
+typedef struct fault_information
+{
+    volatile unsigned long stacked_r0 ;
+    volatile unsigned long stacked_r1 ;
+    volatile unsigned long stacked_r2 ;
+    volatile unsigned long stacked_r3 ;
+    volatile unsigned long stacked_r12 ;
+    volatile unsigned long stacked_lr ;
+    volatile unsigned long stacked_pc ;
+    volatile unsigned long stacked_psr ;
+    volatile unsigned long _CFSR ;
+    volatile unsigned long _HFSR ;
+    volatile unsigned long _DFSR ;
+    volatile unsigned long _AFSR ;
+    volatile unsigned long _BFAR ;
+    volatile unsigned long _MMAR ;
+} FaultInformation;
+
+/** Global instance so that it can be added to the watch expressions */
+volatile FaultInformation faultInfo;
+
+__attribute__((optimize("-O0"),unused)) void hard_fault_handler_step_2(unsigned long *hardfault_args)
+{
+    /* force a reference in the local variables for debug */
+    volatile FaultInformation *fault_info = &faultInfo;
+
+    fault_info->stacked_r0 = ((unsigned long)hardfault_args[0]) ;
+    fault_info->stacked_r1 = ((unsigned long)hardfault_args[1]) ;
+    fault_info->stacked_r2 = ((unsigned long)hardfault_args[2]) ;
+    fault_info->stacked_r3 = ((unsigned long)hardfault_args[3]) ;
+    fault_info->stacked_r12 = ((unsigned long)hardfault_args[4]) ;
+    fault_info->stacked_lr = ((unsigned long)hardfault_args[5]) ;
+    fault_info->stacked_pc = ((unsigned long)hardfault_args[6]) ;
+    fault_info->stacked_psr = ((unsigned long)hardfault_args[7]) ;
+
+    // Configurable Fault Status Register
+    // Consists of MMSR, BFSR and UFSR
+    fault_info->_CFSR = (*((volatile unsigned long *)(0xE000ED28))) ;
+
+    // Hard Fault Status Register
+    fault_info->_HFSR = (*((volatile unsigned long *)(0xE000ED2C))) ;
+
+    // Debug Fault Status Register
+    fault_info->_DFSR = (*((volatile unsigned long *)(0xE000ED30))) ;
+
+    // Auxiliary Fault Status Register
+    fault_info->_AFSR = (*((volatile unsigned long *)(0xE000ED3C))) ;
+
+    // Read the Fault Address Registers. These may not contain valid values.
+    // Check BFARVALID/MMARVALID to see if they are valid values
+    // MemManage Fault Address Register
+    fault_info->_MMAR = (*((volatile unsigned long *)(0xE000ED34))) ;
+    // Bus Fault Address Register
+    fault_info->_BFAR = (*((volatile unsigned long *)(0xE000ED38))) ;
+
     hw_set_to_safe();
+
+    // Simulates a BL instruction from the original PC. Moves the PC to LR,
+    // overwrites PC with our return address.
+    hardfault_args[5] = hardfault_args[6];
+    hardfault_args[6] = (unsigned long)&hard_fault_handler_step_3;
+
+    resetblink(BLINK_DIE_HARDFAULT);
+
+    C_STATIC_ASSERT(((uint8_t*)&faultInfo) + 12 == ((uint8_t*)&faultInfo.stacked_r3), faultinfo_memory_layout_not_as_expected);
+
+    // Returning from here will pop the exception stack and get to
+    // hard_fault_handler_step_3.
+}
+
+void hard_fault_handler_step_3(void) {
     const uint32_t C_DEBUGEN = 0x00000001;
     uint32_t debugreg = *(volatile uint32_t*)0xE000EDF0;
     if (debugreg & C_DEBUGEN) {
-    __asm volatile(
-        " BKPT #1\n"
-        );
-    } else {
-        resetblink(BLINK_DIE_HARDFAULT);
-        __asm(
-            "   mov r1, %0 \n"
-            "   msr basepri, r1 \n"
-            :: "i" ( 0x20 ) : "r1");
+        __asm volatile(
+            " ldr r0, =faultInfo \n"
+            " ldr r3, [r0, 12]   \n"
+            " ldr r2, [r0, 8]    \n"
+            " ldr r1, [r0, 4]    \n"
+            " ldr r0, [r0, 0]    \n"
+            " BKPT #1            \n"
+            ::: "r0", "r1", "r2", "r3");
     }
+    __asm(
+        " mov r1, %0 \n"
+        " msr basepri, r1 \n"
+        " ldr r0, =faultInfo \n"
+        " ldr r3, [r0, 12]   \n"
+        " ldr r2, [r0, 8]    \n"
+        " ldr r1, [r0, 4]    \n"
+        " ldr r0, [r0, 0]    \n"
+        ".infloop:           \n"
+        " b.n .infloop        \n"
+        :: "i" ( 0x20 ) : "r0", "r1", "r2", "r3"
+        );
     while (1);
 }
 
