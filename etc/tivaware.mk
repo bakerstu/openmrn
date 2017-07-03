0bfc4bf2f6b9ecaf52f2257b85be62fe17495211
Sid McHarg <smcharg@comcast.net>
2017-07-03 15:10:26 -0700

    Build Tivaware only when possible
    Build the local Tivaware libraries only when a required file is present.  This is inc/hw_onewire.h which is missing in the 2.1.0, and thus prevents a successful local build.

--- etc/tivaware.mk
+++ etc/tivaware.mk
@@ -4,14 +4,18 @@ ifdef TIVAWAREPATH
 INCLUDES += -I$(TIVAWAREPATH) \
             -I$(OPENMRNPATH)/src/freertos_drivers/ti
 
+ifdef BUILDTIVAWARE
 # set library names when using locally built Tivaware
 SYSLIBRARIESEXTRA = -lfreertos_drivers_tivausblib -lfreertos_drivers_tivausblib_device \
 		-lfreertos_drivers_tivadriverlib
-
+else
 # set library names when using prebuilt Tivaware (remove # to activate)
-#SYSLIBRARIESEXTRA = -lusb -ldriver
-#LDFLAGSEXTRA = -L$(TIVAWAREPATH)/driverlib/gcc \
-#               -L$(TIVAWAREPATH)/usblib/gcc
+SYSLIBRARIESEXTRA = -lusb -ldriver
+LDFLAGSEXTRA = -L$(TIVAWAREPATH)/driverlib/gcc \
+		-L$(TIVAWAREPATH)/usblib/gcc
+
+endif
+
 endif
 
 DEPS += TIVAWAREPATH
