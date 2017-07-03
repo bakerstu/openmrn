0b9637cf0a15efd024c13898d5377a20f1247eee
Sid McHarg <smcharg@comcast.net>
2017-07-03 10:41:29 -0700

    Move Tivaware link options to etc/tivaware.mk
    Moves the link option required when linking to the Tivaware libaries to etc/tivaware.mk.  Previously these were local to the Makefiles.  This provides a common configuration to switch from using the prebuilt libraries to those generated locally.
    
    The freertos_tcp.mk fiel was also updated to provide similar functionality for the freertos_tcp libraries.

--- etc/tivaware.mk
+++ etc/tivaware.mk
@@ -3,6 +3,15 @@ include $(OPENMRNPATH)/etc/path.mk
 ifdef TIVAWAREPATH
 INCLUDES += -I$(TIVAWAREPATH) \
             -I$(OPENMRNPATH)/src/freertos_drivers/ti
+
+# set library names when using locally built Tivaware
+SYSLIBRARIESEXTRA = -lfreertos_drivers_tivausblib -lfreertos_drivers_tivausblib_device \
+		-lfreertos_drivers_tivadriverlib
+
+# set library names when using prebuilt Tivaware (remove # to activate)
+#SYSLIBRARIESEXTRA = -lusb -ldriver
+#LDFLAGSEXTRA = -L$(TIVAWAREPATH)/driverlib/gcc \
+#               -L$(TIVAWAREPATH)/usblib/gcc
 endif
 
 DEPS += TIVAWAREPATH
