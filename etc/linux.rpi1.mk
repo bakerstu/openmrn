99b3104a4dba56bdd2ec4263d31b3579f4aef427
Balazs Racz <balazs.racz@gmail.com>
2017-08-26 12:26:48 +0200

    Adds a hub and a target for cross-compiling to the Raspberry Pi 1 (armv6).

---
+++ etc/linux.rpi1.mk
@@ -0,0 +1,41 @@
+ifndef TOOLPATH
+TOOLPATHCOMMAND := $(shell \
+sh -c "which arm-linux-gnueabihf-gcc" \
+)
+TOOLPATH := $(dir $(TOOLPATHCOMMAND))
+endif
+
+$(info mach toolpath '$(TOOLPATH)')
+
+# Get the $(CFLAGSENV), $(CXXFLAGSENV), $(LDFLAGSENV)
+include $(OPENMRNPATH)/etc/env.mk
+
+CC = arm-linux-gnueabihf-gcc
+CXX = arm-linux-gnueabihf-g++
+AR = arm-linux-gnueabihf-ar
+LD = arm-linux-gnueabihf-g++
+OBJDUMP = arm-linux-gnueabihf-objdump
+
+AROPTS=D
+
+HOST_TARGET := 1
+
+STARTGROUP := -Wl,--start-group
+ENDGROUP := -Wl,--end-group
+
+ARCHOPTIMIZATION = -g -O3 -march=armv6 -marm
+
+CSHAREDFLAGS = -c $(ARCHOPTIMIZATION) -Wall -Werror -Wno-unknown-pragmas \
+               -MD -MP -fno-stack-protector -D_GNU_SOURCE
+
+CFLAGS = $(CSHAREDFLAGS) -std=gnu99
+
+CXXFLAGS = $(CSHAREDFLAGS) -std=c++0x -D__STDC_FORMAT_MACROS \
+           -D__STDC_LIMIT_MACROS -D__USE_LIBSTDCPP__
+
+LDFLAGS = $(ARCHOPTIMIZATION) -pg -Wl,-Map="$(@:%=%.map)"
+SYSLIB_SUBDIRS += console
+SYSLIBRARIES = -lrt -lpthread -lconsole
+
+EXTENTION =
+
