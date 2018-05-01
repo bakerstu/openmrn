6be3efbcc4887ae80ebc71e472d94061adce0b12
Balazs Racz <balazs.racz@gmail.com>
2018-05-01 13:34:17 +0200

    Adds input handling to the SPI shift register flow. Adds the producer objects for the input ports A/B.

--- applications/nucleo_io/targets/freertos.armv6m.st-stm32f091rc-nucleo-dev-board/main.cxx
+++ applications/nucleo_io/targets/freertos.armv6m.st-stm32f091rc-nucleo-dev-board/main.cxx
@@ -82,7 +82,7 @@ extern const char *const openlcb::CONFIG_FILENAME = "/dev/eeprom";
 // The size of the memory space to export over the above device.
 extern const size_t openlcb::CONFIG_FILE_SIZE =
     cfg.seg().size() + cfg.seg().offset();
-static_assert(openlcb::CONFIG_FILE_SIZE <= 1000, "Need to adjust eeprom size");
+static_assert(openlcb::CONFIG_FILE_SIZE <= 1500, "Need to adjust eeprom size");
 // The SNIP user-changeable information in also stored in the above eeprom
 // device. In general this could come from different eeprom segments, but it is
 // simpler to keep them together.
@@ -151,26 +151,76 @@ public:
     }
 } factory_reset_helper;
 
-class SpiOutputShiftRegister : public StateFlowBase
+/// Input/output handler for shift register based IO expansion. Periodically
+/// mirrors bitmaps from memory to and from the external shift registers,
+/// manipulating a latch GPIO appropriately. Uses a SPI port device. There is
+/// no limit on the number of shift registers daisy chained. The input and
+/// output shift registers should have independent daisy chains. It is okay if
+/// there is no CS wired up for the shift registers as the latch operation will
+/// cause all unrelated transfers to be ignored by the shift registers..
+///
+/// This state flow is blocking the executor during the SPI transfers and
+/// should not be used on the main executor.
+class SpiIOShiftRegister : public StateFlowBase
 {
 public:
-    SpiOutputShiftRegister(Service *dedicated_executor, const char *port,
-        AsyncMutex *mutex, const Gpio *latch, uint32_t *storage, unsigned len_bytes,
-        unsigned delay_msec = 50)
+    /// Constructor.
+    /// 
+    /// @param dedicated_executor is a serivce that is NOT on the main
+    /// executor.
+    /// @param port is the character device name for the SPI port
+    /// @param mutex if not null, this (async) mutex will be acquired before
+    /// the SPI port is touched. Allows mutual exclusion of multiple stateflows
+    /// of this class.
+    /// @param latch is the IO pin to operate the latch on the shift
+    /// registers. The expectation is that output shift registers latch on the
+    /// low to high edge of this IO, and input shift registers are shifting
+    /// while high and loading on clocks while low. This matches the 74*595 and
+    /// 74*166 series' behavior.
+    /// @param output_storage bit array of the data to output to the shift
+    /// registers. If nullptr, no output shift registers will be operated. Must
+    /// be aligned to word boundary, but the individual bytes will be output in
+    /// address-increasing order, i.e. the first byte to be output is the byte
+    /// pointed by output_storage. (This is LSB-first on little endian
+    /// architectures.)
+    /// @param output_len_bytes how many 8-bit shift registers are daisy
+    /// chained on the output.
+    /// @param input_storage bit array where to put the data input from the
+    /// shift registers. If nullptr, no input shift registers will be
+    /// operated. Must be aligned to word boundary, but the individual bytes
+    /// address-increasing order, i.e. the first byte to be output is the byte
+    /// pointed by output_storage. (This is LSB-first on little endian
+    /// architectures.)
+    /// @param input_len_bytes how many 8-bit shift registers are daisy
+    /// chained on the input. 
+    /// @param delay_msec defines the delay between consecutive refreshes of
+    /// the shift registers (inversely the refresh rate).
+    SpiIOShiftRegister(Service *dedicated_executor, const char *port,
+        AsyncMutex *mutex, const Gpio *latch, uint32_t *output_storage,
+        unsigned output_len_bytes, uint32_t *input_storage = nullptr,
+        unsigned input_len_bytes = 0, unsigned delay_msec = 50)
         : StateFlowBase(dedicated_executor)
         , asyncMutex_(mutex)
         , latch_(latch)
         , delayMsec_(delay_msec)
-        , storage_(storage)
-        , lenBytes_(len_bytes)
+        , outputStorage_(output_storage)
+        , outputLenBytes_(output_len_bytes)
+        , inputStorage_(input_storage)
+        , inputLenBytes_(input_len_bytes)
     {
-        fd_ = ::open(port, O_WRONLY);
+        fd_ = ::open(port, O_RDWR);
         HASSERT(fd_ >= 0);
         // test alignment
-        HASSERT((((uintptr_t)storage) & 3) == 0);
+        HASSERT((((uintptr_t)outputStorage_) & 3) == 0);
+        HASSERT((((uintptr_t)inputStorage_) & 3) == 0);
         start_flow(STATE(wait_delay));
     }
 
+    /// Executes a refresh cycle right now.
+    void trigger() {
+        timer_.ensure_triggered();
+    }
+    
 private:
     Action wait_delay()
     {
@@ -188,9 +238,24 @@ private:
     }
 
     Action locked() {
-        ::write(fd_, storage_, lenBytes_);
+        latch_->write(Gpio::CLR);
+        if (outputStorage_) {
+            auto rb = ::write(fd_, outputStorage_, outputLenBytes_);
+            HASSERT((size_t)rb == outputLenBytes_);
+        } else {
+            // need to get some clock cycles while lat==low to load the input
+            // shift register
+            uint8_t d = 0;
+            ::write(fd_, &d, 1);
+            latch_->write(Gpio::CLR);
+        }
+        usleep(2);
         latch_->write(Gpio::SET);
-        usleep(15);
+        if (inputStorage_) {
+            auto rb = ::read(fd_, inputStorage_, inputLenBytes_);
+            HASSERT((size_t)rb == inputLenBytes_);
+        }
+        usleep(2);
         latch_->write(Gpio::CLR);
         if (asyncMutex_) {
             asyncMutex_->Unlock();
@@ -209,17 +274,21 @@ private:
     /// How much time to sleep between two refreshes.
     unsigned delayMsec_;
     /// Bit stream of the data to be written. This is purposefully aligned.
-    uint32_t* storage_;
+    uint32_t* outputStorage_;
     /// How many bytes of data to shift out.
-    unsigned lenBytes_;
+    unsigned outputLenBytes_;
+    /// Bit stream of the data to be read. This is purposefully aligned.
+    uint32_t* inputStorage_;
+    /// How many bytes of data to shift in.
+    unsigned inputLenBytes_;
 };
 
 Executor<1> io_executor("io_thread", 1, 1300);
 Service io_service(&io_executor);
 
-uint32_t output_register[1] = {0x000050A0};
+uint32_t output_register[1] = {0x00000000};
 
-SpiOutputShiftRegister internal_outputs(&io_service, "/dev/spi1.ioboard", nullptr, OUT_LAT_Pin::instance(), output_register, 2);
+SpiIOShiftRegister internal_outputs(&io_service, "/dev/spi1.ioboard", nullptr, OUT_LAT_Pin::instance(), output_register, 2);
 
 constexpr const MmapGpio PORTD_LINE1(output_register, 7, true);
 constexpr const MmapGpio PORTD_LINE2(output_register, 6, true);
@@ -249,75 +318,76 @@ constexpr const Gpio *const kPortDEGpio[] = {
 openlcb::MultiConfiguredConsumer portde_consumers(stack.node(), kPortDEGpio,
     ARRAYSIZE(kPortDEGpio), cfg.seg().portde_consumers());
 
+uint32_t input_register[2] = {0};
 
-class SpiOutputShiftRegister : public StateFlowBase
-{
-public:
-    SpiOutputShiftRegister(Service *dedicated_executor, const char *port,
-        AsyncMutex *mutex, const Gpio *latch, uint32_t *output_storage,
-        unsigned output_len_bytes, uint32_t *input_storage = nullptr,
-        unsigned input_len_bytes = 0, unsigned delay_msec = 50)
-        : StateFlowBase(dedicated_executor)
-        , asyncMutex_(mutex)
-        , latch_(latch)
-        , delayMsec_(delay_msec)
-        , outputStorage_(output_storage)
-        , outputLenBytes_(len_bytes)
+SpiIOShiftRegister internal_inputs(&io_service, "/dev/spi2", nullptr, INP_LAT_Pin::instance(), nullptr, 0, input_register, 3);
+
+constexpr const MmapGpio PORTB_LINE1(input_register, 0, false);
+constexpr const MmapGpio PORTB_LINE2(input_register, 1, false);
+constexpr const MmapGpio PORTB_LINE3(input_register, 2, false);
+constexpr const MmapGpio PORTB_LINE4(input_register, 3, false);
+constexpr const MmapGpio PORTB_LINE5(input_register, 4, false);
+constexpr const MmapGpio PORTB_LINE6(input_register, 5, false);
+constexpr const MmapGpio PORTB_LINE7(input_register, 6, false);
+constexpr const MmapGpio PORTB_LINE8(input_register, 7, false);
+
+constexpr const MmapGpio PORTA_LINE1(input_register, 8, false);
+constexpr const MmapGpio PORTA_LINE2(input_register, 9, false);
+constexpr const MmapGpio PORTA_LINE3(input_register, 10, false);
+constexpr const MmapGpio PORTA_LINE4(input_register, 11, false);
+constexpr const MmapGpio PORTA_LINE5(input_register, 12, false);
+constexpr const MmapGpio PORTA_LINE6(input_register, 13, false);
+constexpr const MmapGpio PORTA_LINE7(input_register, 14, false);
+constexpr const MmapGpio PORTA_LINE8(input_register, 15, false);
+
+// Similar syntax for the producers.
+openlcb::ConfiguredProducer producer_a1(
+    stack.node(), cfg.seg().portab_producers().entry<0>(), (const Gpio*)&PORTA_LINE1);
+openlcb::ConfiguredProducer producer_a2(
+    stack.node(), cfg.seg().portab_producers().entry<1>(), (const Gpio*)&PORTA_LINE2);
+openlcb::ConfiguredProducer producer_a3(
+    stack.node(), cfg.seg().portab_producers().entry<2>(), (const Gpio*)&PORTA_LINE3);
+openlcb::ConfiguredProducer producer_a4(
+    stack.node(), cfg.seg().portab_producers().entry<3>(), (const Gpio*)&PORTA_LINE4);
+openlcb::ConfiguredProducer producer_a5(
+    stack.node(), cfg.seg().portab_producers().entry<4>(), (const Gpio*)&PORTA_LINE5);
+openlcb::ConfiguredProducer producer_a6(
+    stack.node(), cfg.seg().portab_producers().entry<5>(), (const Gpio*)&PORTA_LINE6);
+openlcb::ConfiguredProducer producer_a7(
+    stack.node(), cfg.seg().portab_producers().entry<6>(), (const Gpio*)&PORTA_LINE7);
+openlcb::ConfiguredProducer producer_a8(
+    stack.node(), cfg.seg().portab_producers().entry<7>(), (const Gpio*)&PORTA_LINE8);
+
+// Similar syntax for the producers.
+openlcb::ConfiguredProducer producer_b1(
+    stack.node(), cfg.seg().portab_producers().entry<8>(), (const Gpio*)&PORTB_LINE1);
+openlcb::ConfiguredProducer producer_b2(
+    stack.node(), cfg.seg().portab_producers().entry<9>(), (const Gpio*)&PORTB_LINE2);
+openlcb::ConfiguredProducer producer_b3(
+    stack.node(), cfg.seg().portab_producers().entry<10>(), (const Gpio*)&PORTB_LINE3);
+openlcb::ConfiguredProducer producer_b4(
+    stack.node(), cfg.seg().portab_producers().entry<11>(), (const Gpio*)&PORTB_LINE4);
+openlcb::ConfiguredProducer producer_b5(
+    stack.node(), cfg.seg().portab_producers().entry<12>(), (const Gpio*)&PORTB_LINE5);
+openlcb::ConfiguredProducer producer_b6(
+    stack.node(), cfg.seg().portab_producers().entry<13>(), (const Gpio*)&PORTB_LINE6);
+openlcb::ConfiguredProducer producer_b7(
+    stack.node(), cfg.seg().portab_producers().entry<14>(), (const Gpio*)&PORTB_LINE7);
+openlcb::ConfiguredProducer producer_b8(
+    stack.node(), cfg.seg().portab_producers().entry<15>(), (const Gpio*)&PORTB_LINE8);
+
+
+openlcb::RefreshLoop loopab(stack.node(),
     {
-        fd_ = ::open(port, O_WRONLY);
-        HASSERT(fd_ >= 0);
-        // test alignment
-        HASSERT((((uintptr_t)output_storage) & 3) == 0);
-        start_flow(STATE(wait_delay));
-    }
-
-private:
-    Action wait_delay()
-    {
-        return sleep_and_call(
-            &timer_, MSEC_TO_NSEC(delayMsec_), STATE(refresh));
-    }
-
-    Action refresh()
-    {
-        if (asyncMutex_) {
-            return allocate_and_call(STATE(locked), asyncMutex_);
-        } else {
-            return call_immediately(STATE(locked));
-        }
-    }
-
-    Action locked() {
-        ::write(fd_, outputStorage_, outputLenBytes_);
-        latch_->write(Gpio::SET);
-        usleep(15);
-        latch_->write(Gpio::CLR);
-        if (asyncMutex_) {
-            asyncMutex_->Unlock();
-        }
-        return call_immediately(STATE(wait_delay));
-    }
-
-    /// Helper structure.
-    StateFlowTimer timer_{this};
-    /// File descriptor for the SPI port.
-    int fd_;
-    /// Async mutex if the SPI port needs it
-    AsyncMutex* asyncMutex_;
-    /// Latch to trigger activating the output.
-    const Gpio* latch_;
-    /// How much time to sleep between two refreshes.
-    unsigned delayMsec_;
-    /// Bit stream of the data to be written. This is purposefully aligned.
-    uint32_t* outputStorage_;
-    /// How many bytes of data to shift out.
-    unsigned outputLenBytes_;
-    /// Bit stream of the data to be read. This is purposefully aligned.
-    uint32_t* inputStorage_;
-    /// How many bytes of data to shift in.
-    unsigned inputLenBytes_;
-};
-
+        producer_a1.polling(), producer_a2.polling(), //
+        producer_a3.polling(), producer_a4.polling(), //
+        producer_a5.polling(), producer_a6.polling(), //
+        producer_a7.polling(), producer_a8.polling(), //
+        producer_b1.polling(), producer_b2.polling(), //
+        producer_b3.polling(), producer_b4.polling(), //
+        producer_b5.polling(), producer_b6.polling(), //
+        producer_b7.polling(), producer_b8.polling()  //
+    });
 
 /** Entry point to application.
  * @param argc number of command line arguments
