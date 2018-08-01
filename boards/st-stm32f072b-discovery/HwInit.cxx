b383fb44e520b444be413acededd6ad9fc2c8579
Balazs Racz <balazs.racz@gmail.com>
2018-08-01 12:05:59 -0700

    Fix blinker timer interrupts not working on stm32.

--- boards/st-stm32f072b-discovery/HwInit.cxx
+++ boards/st-stm32f072b-discovery/HwInit.cxx
@@ -174,6 +174,7 @@ void hw_preinit(void)
 
     /* setup pinmux */
     GPIO_InitTypeDef gpio_init;
+    memset(&gpio_init, 0, sizeof(gpio_init));
 
     /* USART1 pinmux on PA9 and PA10 */
     gpio_init.Mode = GPIO_MODE_AF_PP;
@@ -199,6 +200,7 @@ void hw_preinit(void)
 
     /* Initializes the blinker timer. */
     TIM_HandleTypeDef TimHandle;
+    memset(&TimHandle, 0, sizeof(TimHandle));
     TimHandle.Instance = TIM14;
     TimHandle.Init.Period = configCPU_CLOCK_HZ / 10000 / 8;
     TimHandle.Init.Prescaler = 10000;
