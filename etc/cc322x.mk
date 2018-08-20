e7b4cc7501e7bb01baf9efa708a225e634a678b8
Stuart Baker <stuart_w_baker@ymail.com>
2018-08-19 22:45:08 -0500

    cleanup Makefile.

--- etc/cc322x.mk
+++ etc/cc322x.mk
@@ -4,12 +4,11 @@ DEPS += TICC3220SDKPATH
 
 ifdef TICC3220SDKPATH
 INCLUDES += -DSL_PLATFORM_MULTI_THREADED -DSL_FULL -DTARGET_IS_CC3220 \
-            -I$(OPENMRNPATH)/src/freertos_drivers/ti/CC3200_compat \
             -I$(OPENMRNPATH)/src/freertos_drivers/ti \
             -I$(OPENMRNPATH)/src/freertos_drivers/net_cc32xx \
-            -I$(TICC3220SDKPATH)/source/ti/devices/cc32xx \
-            -I$(TICC3220SDKPATH)/source/ti/posix/gcc \
             -I$(TICC3220SDKPATH)/source/ti/devices/cc32xx/driverlib \
+            -I$(TICC3220SDKPATH)/source/ti/devices/cc32xx \
+            -I$(TICC3220SDKPATH)/source/ti/posix/gcc
 
 ifneq ($(SPIFFSPATH),)
 INCLUDES += -I$(SPIFFSPATH)/src \
@@ -31,18 +30,10 @@ endif
 INCLUDES += -I$(OPENMRNPATH)/src/freertos_drivers/net_cc322x \
             -I$(TICC3220SDKPATH)/source
 
-
-ifeq ($(wildcard $(TICC3220SDKPATH)/source/ti/drivers/net/wifi/sys/errno.h),)
-#new version of SDK
-#INCLUDES += -DSIMPLELINK_SDK_V1_4
-endif
-
 ifndef EXCLUDESDKINCLUDES
 INCLUDES += -idirafter $(TICC3220SDKPATH)/source/ti/drivers/net/wifi \
             -I$(TICC3220SDKPATH)/source/
 endif
-
-#  ??   -I$(TICC3220SDKPATH)/oslib \
 
 TiDrivers.o : CFLAGS+= -Wno-strict-prototypes
 
