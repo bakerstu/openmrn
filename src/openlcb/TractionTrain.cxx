8c938ac156ac1c7f35e61f6416ada0f1d0b70bb0
Balazs Racz <balazs.racz@gmail.com>
2018-09-02 00:19:41 +0200

    Updates logic of when to return ALREADY EXISTS error back to the caller. Fixes unittests after order preserving consist code.

--- src/openlcb/TractionTrain.cxx
+++ src/openlcb/TractionTrain.cxx
@@ -369,10 +369,14 @@ struct TrainService::Impl
                             Defs::ERROR_INVALID_ARGS_MESSAGE_TOO_SHORT);
                     NodeID target = data_to_node_id(payload() + 3);
                     uint8_t flags = payload()[2];
-                    bool resp = train_node()->add_consist(target, flags);
+                    if (target == train_node()->node_id()) {
+                        return init_and_send_response(
+                            TractionDefs::consist_add_response(
+                                target, Defs::ERROR_OPENMRN_ALREADY_EXISTS));
+                    }
+                    train_node()->add_consist(target, flags);
                     return init_and_send_response(
-                        TractionDefs::consist_add_response(target,
-                            resp ? 0 : Defs::ERROR_OPENMRN_ALREADY_EXISTS));
+                        TractionDefs::consist_add_response(target, 0));
                 }
                 case TractionDefs::CNSTREQ_DETACH_NODE:
                 {
