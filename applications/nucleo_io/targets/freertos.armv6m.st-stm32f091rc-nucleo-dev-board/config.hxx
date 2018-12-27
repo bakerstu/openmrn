c4e484e8f9cf80ad41e3f5563ec865c570655c68
Dakota Szabo <dakotajszabo@gmail.com>
2018-12-27 12:02:25 -0800

    Change servo percentage config fields to int16_t.

--- applications/nucleo_io/targets/freertos.armv6m.st-stm32f091rc-nucleo-dev-board/config.hxx
+++ applications/nucleo_io/targets/freertos.armv6m.st-stm32f091rc-nucleo-dev-board/config.hxx
@@ -42,12 +42,16 @@ CDI_GROUP_ENTRY(event_rotate_max, EventConfigEntry, //
 	Name("Maximum Rotation Event ID"),
 	Description("Receiving this event ID will rotate the servo to its maximum "
                 "configured point."));
-CDI_GROUP_ENTRY(servo_min_percent, Uint8ConfigEntry, Default(0), Min(0),
-	Max(100), Name("Servo Minimum Stop Point Percentage"),
-	Description("Low-end stop point of the servo, 0-100."));
-CDI_GROUP_ENTRY(servo_max_percent, Uint8ConfigEntry, Default(100), Min(0),
-	Max(100), Name("Servo Maximum Stop Point Percentage"),
-	Description("High-end stop point of the servo, 0-100."));
+#define SERVO_DESCRIPTION_SUFFIX \
+	"stop point of the servo, as a percentage: generally 0-100. " \
+	"May be under/over-driven by setting a percentage value "  \
+	"of -99 to 200, respectively."
+CDI_GROUP_ENTRY(servo_min_percent, Int16ConfigEntry, Default(0), Min(-99),
+	Max(200), Name("Servo Minimum Stop Point Percentage"),
+	Description("Low-end " SERVO_DESCRIPTION_SUFFIX));
+CDI_GROUP_ENTRY(servo_max_percent, Int16ConfigEntry, Default(100), Min(-99),
+	Max(200), Name("Servo Maximum Stop Point Percentage"),
+	Description("High-end " SERVO_DESCRIPTION_SUFFIX));
 CDI_GROUP_END(); // ServoConsumerConfig
 
 /// Declares a repeated group of a given base group and number of repeats. The
