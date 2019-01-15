29f91a2e4964edc9781daee57e47e111f57ed336
atanisoft <m_dunston@comcast.net>
2019-01-15 15:51:54 -0800

    review comments for libify.sh

--- arduino/libify.sh
+++ arduino/libify.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
 TARGET_LIB_DIR=$1
-OPENMRN_ROOT=$2
+OPENMRNPATH=$2
 
 if [ "${TARGET_LIB_DIR}x" == "x" ]; then
     echo "TARGET_LIB_DIR NOT DEFINED"
 fi
 
-if [ "${OPENMRN_ROOT}x" == "x" ]; then
-    echo "OPENMRN_ROOT NOT DEFINED"
+if [ "${OPENMRNPATH}x" == "x" ]; then
+    echo "OPENMRNPATH NOT DEFINED"
 fi
 
-cp ${OPENMRN_ROOT}/arduino/library.json \
-    ${OPENMRN_ROOT}/arduino/library.properties \
-    ${OPENMRN_ROOT}/arduino/keywords.txt \
+cp ${OPENMRNPATH}/arduino/library.json \
+    ${OPENMRNPATH}/arduino/library.properties \
+    ${OPENMRNPATH}/arduino/keywords.txt \
     ${TARGET_LIB_DIR}
-cp -r ${OPENMRN_ROOT}/arduino/examples \
+cp -r ${OPENMRNPATH}/arduino/examples \
     ${TARGET_LIB_DIR}
 
 mkdir ${TARGET_LIB_DIR}/src
-cp ${OPENMRN_ROOT}/arduino/OpenMRN.cpp \
-    ${OPENMRN_ROOT}/arduino/OpenMRN.h \
-    ${OPENMRN_ROOT}/include/can_frame.h \
-    ${OPENMRN_ROOT}/include/nmranet_config.h \
-    ${OPENMRN_ROOT}/include/freertos/endian.h \
+cp ${OPENMRNPATH}/arduino/OpenMRN.cpp \
+    ${OPENMRNPATH}/arduino/OpenMRN.h \
+    ${OPENMRNPATH}/include/can_frame.h \
+    ${OPENMRNPATH}/include/nmranet_config.h \
+    ${OPENMRNPATH}/include/freertos/endian.h \
     ${TARGET_LIB_DIR}/src
 
 mkdir ${TARGET_LIB_DIR}/src/dcc
-cp ${OPENMRN_ROOT}/src/dcc/*.hxx \
-    ${OPENMRN_ROOT}/src/dcc/*.h \
+cp ${OPENMRNPATH}/src/dcc/*.hxx \
+    ${OPENMRNPATH}/src/dcc/*.h \
     ${TARGET_LIB_DIR}/src/dcc
 
 mkdir ${TARGET_LIB_DIR}/src/executor
-cp ${OPENMRN_ROOT}/src/executor/*.hxx \
-    ${OPENMRN_ROOT}/src/executor/*.cxx \
+cp ${OPENMRNPATH}/src/executor/*.hxx \
+    ${OPENMRNPATH}/src/executor/*.cxx \
     ${TARGET_LIB_DIR}/src/executor
 
 mkdir ${TARGET_LIB_DIR}/src/freertos_drivers
 mkdir ${TARGET_LIB_DIR}/src/freertos_drivers/arduino
-cp ${OPENMRN_ROOT}/src/freertos_drivers/arduino/* \
-    ${OPENMRN_ROOT}/src/freertos_drivers/common/DeviceBuffer.cxx \
-    ${OPENMRN_ROOT}/src/freertos_drivers/common/DeviceBuffer.hxx \
-    ${OPENMRN_ROOT}/src/freertos_drivers/common/GpioWrapper.hxx \
+cp ${OPENMRNPATH}/src/freertos_drivers/arduino/* \
+    ${OPENMRNPATH}/src/freertos_drivers/common/DeviceBuffer.cxx \
+    ${OPENMRNPATH}/src/freertos_drivers/common/DeviceBuffer.hxx \
+    ${OPENMRNPATH}/src/freertos_drivers/common/GpioWrapper.hxx \
     ${TARGET_LIB_DIR}/src/freertos_drivers/arduino
 
 mkdir ${TARGET_LIB_DIR}/src/openlcb
-cp ${OPENMRN_ROOT}/src/openlcb/*.cxx \
-    ${OPENMRN_ROOT}/src/openlcb/*.hxx \
+cp ${OPENMRNPATH}/src/openlcb/*.cxx \
+    ${OPENMRNPATH}/src/openlcb/*.hxx \
     ${TARGET_LIB_DIR}/src/openlcb
 
 rm -f ${TARGET_LIB_DIR}/src/openlcb/CompileCdiMain.cxx \
@@ -54,20 +54,20 @@ rm -f ${TARGET_LIB_DIR}/src/openlcb/CompileCdiMain.cxx \
     ${TARGET_LIB_DIR}/src/openlcb/Stream.hxx
 
 mkdir ${TARGET_LIB_DIR}/src/os
-cp ${OPENMRN_ROOT}/src/os/*.c \
-    ${OPENMRN_ROOT}/src/os/*.h \
-    ${OPENMRN_ROOT}/src/os/*.hxx \
+cp ${OPENMRNPATH}/src/os/*.c \
+    ${OPENMRNPATH}/src/os/*.h \
+    ${OPENMRNPATH}/src/os/*.hxx \
     ${TARGET_LIB_DIR}/src/os
 
 mkdir ${TARGET_LIB_DIR}/src/sys
-cp ${OPENMRN_ROOT}/include/sys/tree.hxx \
+cp ${OPENMRNPATH}/include/sys/tree.hxx \
     ${TARGET_LIB_DIR}/src/sys
 
 mkdir ${TARGET_LIB_DIR}/src/utils
-cp ${OPENMRN_ROOT}/src/utils/*.cxx \
-    ${OPENMRN_ROOT}/src/utils/*.hxx \
-    ${OPENMRN_ROOT}/src/utils/*.c \
-    ${OPENMRN_ROOT}/src/utils/*.h \
+cp ${OPENMRNPATH}/src/utils/*.cxx \
+    ${OPENMRNPATH}/src/utils/*.hxx \
+    ${OPENMRNPATH}/src/utils/*.c \
+    ${OPENMRNPATH}/src/utils/*.h \
     ${TARGET_LIB_DIR}/src/utils
 
 rm -f ${TARGET_LIB_DIR}/src/utils/ReflashBootloader.cxx \
