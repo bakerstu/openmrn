52fae71dce7284e0f59231251ee13ed607fb7f6b
Mike Dunston <m_dunston@comcast.net>
2019-01-20 06:40:42 -0800

    fix comments

--- src/freertos_drivers/arduino/Esp32WiFiClientAdapter.hxx
+++ src/freertos_drivers/arduino/Esp32WiFiClientAdapter.hxx
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * \file Can.hxx
+ * \file Esp32WiFiClientAdapter.hxx
  *
  * ESP32 adapter code using the WiFiClient provided by the WiFiServer code
  * for interfacing with the OpenMRN stack.
