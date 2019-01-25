83c4a0669c2b458c7dfc8f152e1ce1e33b3b9263
Mike Dunston <m_dunston@comcast.net>
2019-01-24 18:04:44 -0800

    Adding CAN to the ESP32IOBoard example
    With this being added you can now turn off the WiFi usage in the
    ESP32IOBoard and it will expose only via CAN if USE_CAN is defined and
    USE_WIFI is not defined.
    
    Switched ESP32IOBoard to use a MultiConsumer instead of individual
    consumer objects

--- arduino/examples/ESP32IOBoard/ESP32IOBoard.ino
+++ arduino/examples/ESP32IOBoard/ESP32IOBoard.ino
@@ -41,15 +41,24 @@
 #include <OpenMRN.h>
 #include <openlcb/TcpDefs.hxx>
 
+#include "openlcb/MultiConfiguredConsumer.hxx"
+
 #include "config.h"
 
-/// This is the TCP/IP port which the ESP32 will listen on for incoming
-/// GridConnect formatted CAN frames.
-constexpr uint16_t OPENMRN_TCP_PORT = 12021L;
+// Pick one mode below, if you select USE_WIFI it will expose this node on WIFI
+// if you select USE_CAN, this node will be available on CAN.
+
+#define USE_WIFI
+// #define USE_CAN
 
 /// This is the node id to assign to this device, this must be unique
 /// on the CAN bus.
 static constexpr uint64_t NODE_ID = UINT64_C(0x050101011423);
+
+#if defined(USE_WIFI)
+/// This is the TCP/IP port which the ESP32 will listen on for incoming
+/// GridConnect formatted CAN frames.
+constexpr uint16_t OPENMRN_TCP_PORT = 12021L;
 
 // Configuring WiFi accesspoint name and password
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -77,6 +86,30 @@ const char *hostname = "esp32mrn";
 /// This is the TCP/IP listener on the ESP32.
 WiFiServer openMRNServer(OPENMRN_TCP_PORT);
 
+OVERRIDE_CONST(gridconnect_buffer_size, 512);
+OVERRIDE_CONST(gridconnect_buffer_delay_usec, 2000);
+
+#endif // USE_WIFI
+
+#if defined(USE_CAN)
+/// This is the ESP32 pin connected to the SN6565HVD23x/MCP2551 R (RX) pin.
+/// Recommended pins: 4, 16, 21.
+/// Note: Any pin can be used for this other than 6-11 which are connected to
+/// the onboard flash.
+/// Note: If you are using a pin other than 4 you will likely need to adjust
+/// the GPIO pin definitions for the outputs.
+constexpr gpio_num_t CAN_RX_PIN = GPIO_NUM_4;
+
+/// This is the ESP32 pin connected to the SN6565HVD23x/MCP2551 D (TX) pin.
+/// Recommended pins: 5, 17, 22.
+/// Note: Any pin can be used for this other than 6-11 which are connected to
+/// the onboard flash and 34-39 which are input only.
+/// Note: If you are using a pin other than 5 you will likely need to adjust
+/// the GPIO pin definitions for the outputs.
+constexpr gpio_num_t CAN_TX_PIN = GPIO_NUM_5;
+
+#endif // USE_CAN
+
 /// This is the primary entrypoint for the OpenMRN/LCC stack.
 OpenMRN openmrn(NODE_ID);
 
@@ -90,20 +123,31 @@ string dummystring("abcdef");
 /// layout. The argument of offset zero is ignored and will be removed later.
 static constexpr openlcb::ConfigDef cfg(0);
 
-OVERRIDE_CONST(gridconnect_buffer_size, 512);
-OVERRIDE_CONST(gridconnect_buffer_delay_usec, 2000);
-
 // Declare output pins
 // NOTE: pins 6-11 are connected to the onboard flash and can not be used for
 // any purpose and pins 34-39 are INPUT only.
-GPIO_PIN(IO0, GpioOutputSafeLow, 2);
-GPIO_PIN(IO1, GpioOutputSafeLow, 4);
-GPIO_PIN(IO2, GpioOutputSafeLow, 5);
-GPIO_PIN(IO3, GpioOutputSafeLow, 16);
-GPIO_PIN(IO4, GpioOutputSafeLow, 17);
-GPIO_PIN(IO5, GpioOutputSafeLow, 18);
-GPIO_PIN(IO6, GpioOutputSafeLow, 19);
-GPIO_PIN(IO7, GpioOutputSafeLow, 23);
+GPIO_PIN(IO0, GpioOutputSafeLow, 15);
+GPIO_PIN(IO1, GpioOutputSafeLow, 2);
+GPIO_PIN(IO2, GpioOutputSafeLow, 16);
+GPIO_PIN(IO3, GpioOutputSafeLow, 17);
+GPIO_PIN(IO4, GpioOutputSafeLow, 13);
+GPIO_PIN(IO5, GpioOutputSafeLow, 12);
+GPIO_PIN(IO6, GpioOutputSafeLow, 14);
+GPIO_PIN(IO7, GpioOutputSafeLow, 27);
+
+// List of GPIO objects that will be used for the output pins. You should keep
+// the constexpr declaration, because it will produce a compile error in case
+// the list of pointers cannot be compiled into a compiler constant and thus
+// would be placed into RAM instead of ROM.
+constexpr const Gpio *const kOutputGpio[] = {
+    IO0_Pin::instance(), IO1_Pin::instance(), //
+    IO2_Pin::instance(), IO3_Pin::instance(), //
+    IO4_Pin::instance(), IO5_Pin::instance(), //
+    IO6_Pin::instance(), IO7_Pin::instance()  //
+};
+
+openlcb::MultiConfiguredConsumer gpio_consumers(openmrn.stack()->node(), kOutputGpio,
+    ARRAYSIZE(kOutputGpio), cfg.seg().consumers());
 
 // Declare input pins, these are using analog pins as digital inputs
 // NOTE: pins 25 and 26 can not safely be used as analog pins while
@@ -116,23 +160,6 @@ GPIO_PIN(IO12, GpioInputPU, 36);
 GPIO_PIN(IO13, GpioInputPU, 39);
 GPIO_PIN(IO14, GpioInputPU, 25);
 GPIO_PIN(IO15, GpioInputPU, 26);
-
-openlcb::ConfiguredConsumer IO0_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<0>(), IO0_Pin());
-openlcb::ConfiguredConsumer IO1_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<1>(), IO1_Pin());
-openlcb::ConfiguredConsumer IO2_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<2>(), IO2_Pin());
-openlcb::ConfiguredConsumer IO3_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<3>(), IO3_Pin());
-openlcb::ConfiguredConsumer IO4_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<4>(), IO4_Pin());
-openlcb::ConfiguredConsumer IO5_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<5>(), IO5_Pin());
-openlcb::ConfiguredConsumer IO6_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<6>(), IO6_Pin());
-openlcb::ConfiguredConsumer IO7_consumer(
-    openmrn.stack()->node(), cfg.seg().consumers().entry<7>(), IO7_Pin());
 
 openlcb::ConfiguredProducer IO8_producer(
     openmrn.stack()->node(), cfg.seg().producers().entry<0>(), IO8_Pin());
@@ -174,6 +201,7 @@ void setup()
 {
     Serial.begin(115200L);
 
+#if defined(USE_WIFI)
     printf("\nConnecting to: %s\n", ssid);
     WiFi.begin(ssid, password);
     uint8_t attempts = 30;
@@ -207,6 +235,15 @@ void setup()
     printf("\nWiFi connected, IP address: %s\n",
         WiFi.localIP().toString().c_str());
 
+    // Start the TCP/IP listener
+    openMRNServer.setNoDelay(true);
+    openMRNServer.begin();
+
+    // Start the mDNS subsystem
+    MDNS.begin(hostname);
+
+#endif // USE_WIFI
+
     // Initialize the SPIFFS filesystem as our persistence layer
     if (!SPIFFS.begin())
     {
@@ -237,21 +274,28 @@ void setup()
     // have performance impact.
     // openmrn.stack()->print_all_packets();
 
-    // Start the TCP/IP listener
-    openMRNServer.setNoDelay(true);
-    openMRNServer.begin();
+#if defined(USE_CAN)
+    // When using the hardware can device it is recommended to also utilize the
+    // background task on the ESP32 rather than call openmrn.loop() from the
+    // loop method.
+    openmrn.start_background_task();
 
-    // Start the mDNS subsystem
-    MDNS.begin(hostname);
+    // Add the hardware CAN device as a bridge
+    openmrn.add_can_port(
+        new Esp32HardwareCan("esp32can", CAN_RX_PIN, CAN_TX_PIN));
+#endif // USE_CAN
 
+#if defined(USE_WIFI)
     // Broadcast this node's hostname with the mDNS service name
     // for a TCP GridConnect endpoint.
     MDNS.addService(openlcb::TcpDefs::MDNS_SERVICE_NAME_GRIDCONNECT_CAN,
         openlcb::TcpDefs::MDNS_PROTOCOL_TCP, OPENMRN_TCP_PORT);
+#endif // USE_WIFI
 }
 
 void loop()
 {
+#if defined(USE_WIFI)
     // if the TCP/IP listener has a new client accept it and add it
     // as a new GridConnect port.
     if (openMRNServer.hasClient())
@@ -262,8 +306,13 @@ void loop()
             openmrn.add_gridconnect_port(new Esp32WiFiClientAdapter(client));
         }
     }
+#endif // USE_WIFI
 
+    // if we are using the CAN driver the following lines are not
+    // required as the background task will have been started already.
+#if !defined(USE_CAN)
     // Call the OpenMRN executor, this needs to be done as often
     // as possible from the loop() method.
     openmrn.loop();
+#endif
 }
