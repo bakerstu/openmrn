33b96496040912c38707b941d444630ecb0d7da0
Balazs Racz <balazs.racz@gmail.com>
2019-02-02 01:41:32 -0500

    Fix comments

--- src/freertos_drivers/spiffs/SPIFFS.cxx
+++ src/freertos_drivers/spiffs/SPIFFS.cxx
@@ -155,14 +155,13 @@ int SPIFFS::close(File *file)
 {
     spiffs_file fd = file->privInt;
 
+    file->dirty = false;
     int result = SPIFFS_close(&fs_, fd);
 
     if (result != SPIFFS_OK)
     {
         return -errno_translate(result);
     }
-
-    file->dirty = 0;
     
     return 0;
 }
@@ -215,8 +214,8 @@ ssize_t SPIFFS::write(File *file, const void *buf, size_t count)
 
     {
         AtomicHolder h(this);
-        file->dirty = 1;
-        anyDirty_ = 1;
+        file->dirty = true;
+        anyDirty_ = true;
     }
     
     return result;
@@ -341,12 +340,13 @@ int SPIFFS::stat(const char *path, struct stat *stat)
 int SPIFFS::fsync(File *file)
 {
     spiffs_file fd = file->privInt;
+    file->dirty = false;
     int result = SPIFFS_fflush(&fs_, fd);
     if (result < 0)
     {
+        file->dirty = true;
         return -errno_translate(result);
     }
-    file->dirty = 0;
     return 0;
 }
 
