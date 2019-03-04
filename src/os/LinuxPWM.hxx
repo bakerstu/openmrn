a08773da60d7c8eb6021d783a438f795b9d59f9d
Balazs Racz <balazs.racz@gmail.com>
2019-03-04 20:22:35 +0100

    Updates LinuxPWM to simplify the code. Reformats the code. Makes it conform to the style guide. Adds comments to the code.

--- src/os/LinuxPWM.hxx
+++ src/os/LinuxPWM.hxx
@@ -31,134 +31,165 @@
  * \file LinuxPWM.hxx
  *
  * Defines PWM pins using the Linux sysfs ABI.
- * 
+ *
  * \section HOWTOUSE How to use
  * TBD
- * 
+ *
  * @author Robert Heller
  * @date 19 Feburary 2019
  */
 
-#ifndef __LINUXPWM_HXX
-#define __LINUXPWM_HXX
+#ifndef _OS_LINUXPWM_HXX_
+#define _OS_LINUXPWM_HXX_
 
-#include "freertos_drivers/common/PWM.hxx"
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
 
+#include "freertos_drivers/common/PWM.hxx"
+#include "utils/FileUtils.hxx"
+#include "utils/StringPrintf.hxx"
+#include "utils/format_utils.hxx"
+
+/// Implementation of a single PWM channel using the linux sysfs API.
 class LinuxPWM : public PWM
 {
 public:
-    LinuxPWM(int chip, int channel) : chip_(chip) , channel_(channel)
+    /// Constructor.
+    /// @param chip is an integer, the index in the kernel where the PWM chip is
+    /// registered.
+    /// @param channel the index of the output channel on the specific chip to
+    /// use.
+    LinuxPWM(int chip, int channel)
+        : chip_(chip)
+        , channel_(channel)
+        , pwmdir_(
+              StringPrintf("/sys/class/pwm/pwmchip%d/pwm%d/", chip, channel))
     {
-        snprintf(pwmdir,sizeof(pwmdir),
-                 "/sys/class/pwm/pwmchip%d/pwm%d",
-                 chip_,channel_);
     }
-    void exportPin()
+
+    /// Call this once after instantiating the object but before using any of
+    /// the PWM apis.
+    void export_pin()
     {
-        char exportname[50];
-        strcpy(exportname,pwmdir);
-        strcat(exportname,"/period");
-        if (access(exportname, F_OK) != -1)
+        if (access((pwmdir_ + PERIOD_FILE).c_str(), F_OK) != -1)
         {
             set_period(1);
             return;
         }
-        char *p = strrchr(exportname,'/');
-        *p = '\0';
-        p = strrchr(exportname,'/');
-        p++;
-        strcpy(p,"export");
-        set_sysfs_file_value(exportname,channel_);
+        string export_file =
+            StringPrintf("/sys/class/pwm/pwmchip%d/export", chip_);
+        write_string_to_file(export_file, integer_to_string(channel_) + "\n");
         set_period(1);
-    }       
+    }
+
     void set_period(uint32_t counts) override
     {
-        char periodname[60];
-        strcpy(periodname,pwmdir);
-        strcat(periodname,"/period");
-        set_sysfs_file_value(periodname,counts);
-        if (counts > 0) {
+        set_sysfs_file_value(PERIOD_FILE, counts);
+        if (counts > 0)
+        {
             enable();
-        } else {
+        }
+        else
+        {
             disable();
         }
     }
+
     uint32_t get_period() override
     {
-        char periodname[60];
-        strcpy(periodname,pwmdir);
-        strcat(periodname,"/period");
-        return get_sysfs_file_value(periodname);
+        return get_sysfs_file_value(PERIOD_FILE);
     }
+
     void set_duty(uint32_t counts) override
     {
-        char duty_cyclename[60];
-        strcpy(duty_cyclename,pwmdir);
-        strcat(duty_cyclename,"/duty_cycle");
-        set_sysfs_file_value(duty_cyclename,counts);
-        if (counts > 0) {
+        set_sysfs_file_value(DUTY_CYCLE_FILE, counts);
+        if (counts > 0)
+        {
             enable();
-        } else {
+        }
+        else
+        {
             disable();
         }
     }
+
     uint32_t get_duty() override
     {
-        char duty_cyclename[60];
-        strcpy(duty_cyclename,pwmdir);
-        strcat(duty_cyclename,"/duty_cycle");
-        return get_sysfs_file_value(duty_cyclename);
+        return get_sysfs_file_value(DUTY_CYCLE_FILE);
     }
+
     uint32_t get_period_max() override
     {
         return 0xffffffff;
     }
+
     uint32_t get_period_min() override
     {
         return 1;
     }
-    void enable() {
-        char enablename[60];
-        strcpy(enablename,pwmdir);
-        strcat(enablename,"/enable");
-        set_sysfs_file_value(enablename,1);
+
+    /// Enables the output PWM waveform.
+    void enable()
+    {
+        set_sysfs_file_value(ENABLE_FILE, 1);
     }
-    void disable() {
-        char enablename[60];
-        strcpy(enablename,pwmdir);
-        strcat(enablename,"/enable");
-        set_sysfs_file_value(enablename,0);
+
+    /// Disables the output PWM waveform.
+    void disable()
+    {
+        set_sysfs_file_value(ENABLE_FILE, 0);
     }
-    bool enabled() {
-        char enablename[60];
-        strcpy(enablename,pwmdir);
-        strcat(enablename,"/enable");
-        return get_sysfs_file_value(enablename) == 1;
+
+    /// Returns whether the output PWM waveform is enabled or not.
+    bool enabled()
+    {
+        return get_sysfs_file_value(ENABLE_FILE) == 1;
     }
+
 private:
+    /// Basename for sysfs file for enabling the channel.
+    static constexpr const char *ENABLE_FILE = "enable";
+    /// Basename for sysfs file for setting the period.
+    static constexpr const char *PERIOD_FILE = "period";
+    /// Basename for sysfs file for setting the duty cycle.
+    static constexpr const char *DUTY_CYCLE_FILE = "duty_cycle";
+
+    /// Kernel number of the chip.
     const uint32_t chip_;
+    /// Number of the channel in the chip.
     const uint32_t channel_;
-    char pwmdir[40];
-    uint32_t get_sysfs_file_value(const char* basename) {
-        uint32_t value;
-        FILE *fp = fopen(basename,"r");
-        fscanf(fp,"%d",&value);
+    /// Diretory path of the channel, with trailing /
+    const string pwmdir_;
+
+    /// Returns a single file's content from the channel directory on sysfs.
+    /// @param basename is the name of the file (e.g. "period")
+    /// @return the contents of the file converted to integer.
+    uint32_t get_sysfs_file_value(const char *basename)
+    {
+        string filename(pwmdir_);
+        filename += basename;
+        uint32_t value = 0;
+        FILE *fp = fopen(filename.c_str(), "r");
+        fscanf(fp, "%d", &value);
         fclose(fp);
         return value;
     }
-    void set_sysfs_file_value(const char* basename, uint32_t value) {
-        FILE *fp = fopen(basename,"w");
-        fprintf(fp,"%d\n",value);
+
+    /// Writes a single integer into a sysfs file of the channel directory.
+    /// @param basename is the name of the file (e.g. "period")
+    /// @param value is the number to write.
+    void set_sysfs_file_value(const char *basename, uint32_t value)
+    {
+        string filename(pwmdir_);
+        filename += basename;
+        FILE *fp = fopen(filename.c_str(), "w");
+        fprintf(fp, "%d\n", value);
         fclose(fp);
     }
 };
-        
 
-
-#endif // __LINUXPWM_HXX
-
+#endif // _OS_LINUXPWM_HXX_
