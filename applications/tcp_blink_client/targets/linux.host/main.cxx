af08cbac5b9ea381c867385d1645948c1d91a41c
Balazs Racz <balazs.racz@gmail.com>
2019-03-23 23:58:10 +0100

    Adds instntiation of the necessary components for the Tcp example.

--- applications/tcp_blink_client/targets/linux.host/main.cxx
+++ applications/tcp_blink_client/targets/linux.host/main.cxx
@@ -34,10 +34,100 @@
 
 #define LOGLEVEL INFO
 
-#include "openlcb/IfTcp.hxx"
 #include "executor/Executor.hxx"
+#include "nmranet_config.h"
+#include "openlcb/BlinkerFlow.hxx"
+#include "openlcb/DefaultNode.hxx"
+#include "openlcb/EventService.hxx"
+#include "openlcb/IfTcp.hxx"
+#include "openlcb/NodeInitializeFlow.hxx"
+#include "openlcb/ProtocolIdentification.hxx"
+#include "utils/HubDeviceSelect.hxx"
+#include "utils/SocketClient.hxx"
+#include "utils/SocketClientParams.hxx"
 
 const openlcb::NodeID NODE_ID = 0x050101011876;
+const openlcb::EventId EVENT_ID = 0x0501010118760000;
 
 Executor<5> g_executor("executor", 0, 2048);
+Executor<1> g_connect_executor("connect_executor", 0, 2048);
 Service g_service(&g_executor);
+HubFlow g_device(&g_service);
+openlcb::IfTcp g_if(NODE_ID, &g_device, config_local_nodes_count());
+openlcb::InitializeFlow g_init_flow(&g_if);
+openlcb::DefaultNode g_node(&g_if, NODE_ID);
+openlcb::EventService g_event_service(&g_if);
+constexpr uint64_t PIP_SUPPORT = openlcb::Defs::EVENT_EXCHANGE;
+openlcb::ProtocolIdentificationHandler g_pip_handler(&g_node, PIP_SUPPORT);
+
+BlinkerFlow blinker(&g_node, EVENT_ID);
+
+int upstream_port = 12000;
+const char *upstream_host = "localhost";
+
+void usage(const char *e)
+{
+    fprintf(stderr, "Usage: %s [-u upstream_host] [-q upstream_port] \n\n", e);
+    fprintf(stderr,
+        "TCP client demo.\nConnects to a TCP protocol hub, and sends some "
+        "example events.\n\nArguments:\n");
+    fprintf(stderr,
+        "\t-u upstream_host   is the host name for an upstream "
+        "hub.\n");
+    fprintf(stderr,
+        "\t-q upstream_port   is the port number for the upstream hub.\n");
+    exit(1);
+}
+
+void parse_args(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, "hu:q:")) >= 0)
+    {
+        switch (opt)
+        {
+            case 'h':
+                usage(argv[0]);
+                break;
+            case 'u':
+                upstream_host = optarg;
+                break;
+            case 'q':
+                upstream_port = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Unknown option %c\n", opt);
+                usage(argv[0]);
+        }
+    }
+}
+
+void connect_callback(int fd, Notifiable *on_error)
+{
+    LOG(INFO, "Connected to hub.");
+    // we leak this.
+    new HubDeviceSelect<HubFlow>(&g_device, fd, on_error);
+    // will delete itself
+    new openlcb::ReinitAllNodes(&g_if);
+}
+
+/** Entry point to application.
+ * @param argc number of command line arguments
+ * @param argv array of command line arguments
+ * @return 0, should never return
+ */
+int appl_main(int argc, char *argv[])
+{
+    parse_args(argc, argv);
+    SocketClient socket_client(&g_service, &g_connect_executor,
+        &g_connect_executor,
+        SocketClientParams::from_static(upstream_host, upstream_port),
+        &connect_callback);
+
+    while (1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
