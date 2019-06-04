961629d5d8c5554d83bb2b5a03bd9d64d79701db
Balazs Racz <balazs.racz@gmail.com>
2019-06-04 14:23:23 +0200

    Fixes missing include that made this file not compilable by itself.

--- src/freertos_drivers/common/WifiDefs.hxx
+++ src/freertos_drivers/common/WifiDefs.hxx
@@ -1,6 +1,8 @@
 #ifndef _FREERTOS_DRIVERS_COMMON_WIFIDEFS_HXX_
 #define _FREERTOS_DRIVERS_COMMON_WIFIDEFS_HXX_
 
+#include <stdint.h>
+
 /// Wifi not associated to access point: continuous short blinks.
 #define WIFI_BLINK_NOTASSOCIATED  0b1010
 /// Waiting for IP address: double short blink, pause, double short blink, ...
