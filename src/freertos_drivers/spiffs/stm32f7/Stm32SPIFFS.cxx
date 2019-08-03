eba76aae2590f754647e45623d1fa88326b6ca01
Balazs Racz <balazs.racz@gmail.com>
2019-08-03 23:27:48 +0200

    Adds SPIFFS to the larger nucleo boards (#300)
    - Adds SPIFFS for stm32f0..f3 and stm32f7.
    - switches nucleo-767zi to use spiffs instead of eepromemulation.
    - Refactors SPIFFS so that we don't need to include spffs.h in SPIFFS.hxx. This allows compiling the target HwInit.cxx without needing to worry about -I arguments to find spiffs source.
    
    Some other minor fixes to drivers.
    - stm32f767 nucleo: fix bug in serial port hardware clock not being enabled.
    - enables using UART3 IRQ on the stm32f7
    - adds a common include instead of stm32f#_hal_conf.h
    
    ===
    
    * Forks SPIFFS configs for the stm32 targets.
    
    * Adds implementation for STM32 flash write/erase to supprot SPIFFS.
    
    * Adds spiffs path to path.mk and target build directory.
    
    * Correctly capitalize filenames.
    
    * Fixes compilation errors.
    
    * rename directory.
    
    * Makes a SPIFFS FileSystem not require the include of spiffs.h transitively
    when compiling HwInit.cxx. This avoids having to tamper with -I arguments in
    the target directory.
    
    * Adds a general proxy for including the appropriate stm32f_hal_conf.h
    
    * Fixes compilation error.
    
    * Fixes compilation of SPIFFS for the 303xe.
    
    * Fixes missing initializations.
    
    * Fixes missing initialization.
    
    * Forks stm32F3 SPIFFS to stm32F7.
    
    * Fixes spiffs writes for the F7.
    
    * Adds spiffs to the stm32F767ZI nucleo.
    
    * Adds exc target for the 767 spiffs.
    
    * compiles spiffs for 767.
    
    * Fixes review comments.
    
    * Fix formatting.

--- src/freertos_drivers/spiffs/stm32f7/Stm32SPIFFS.cxx
+++ src/freertos_drivers/spiffs/stm32f7/Stm32SPIFFS.cxx
@@ -1,10 +1,10 @@
 /** @copyright
- * Copyright (c) 2018, Stuart W Baker
+ * Copyright (c) 2019, Balazs Racz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are  permitted provided that the following conditions are met:
- * 
+ *
  *  - Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *
@@ -24,36 +24,36 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @file CC32x0SFSPIFFS.cxx
- * This file implements a SPIFFS FLASH driver specific to CC32xx.
+ * @file Stm32SPIFFS.cxx
  *
- * @author Stuart W. Baker
- * @date 1 January 2018
+ * This file implements a SPIFFS FLASH driver specific to STM32 F7 flash
+ * API.
+ *
+ * @author Balazs Racz
+ * @date 13 July 2019
  */
 
-#include "CC32x0SFSPIFFS.hxx"
-
+#include "Stm32SPIFFS.hxx"
 #include "spiffs.h"
-#include "inc/hw_types.h"
-#include "driverlib/flash.h"
+#include "stm32f_hal_conf.hxx"
 
 //
-// CC32x0SFSPIFFS::flash_read()
+// Stm32SPIFFS::flash_read()
 //
-int32_t CC32x0SFSPIFFS::flash_read(uint32_t addr, uint32_t size, uint8_t *dst)
+int32_t Stm32SPIFFS::flash_read(uint32_t addr, uint32_t size, uint8_t *dst)
 {
     HASSERT(addr >= fs_->cfg.phys_addr &&
             (addr + size) <= (fs_->cfg.phys_addr  + fs_->cfg.phys_size));
 
-    memcpy(dst, (void*)addr, size);
+    memcpy(dst, (void *)addr, size);
 
     return 0;
 }
 
 //
-// CC32x0SFSPIFFS::flash_write()
+// Stm32SPIFFS::flash_write()
 //
-int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
+int32_t Stm32SPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
 {
     union WriteWord
     {
@@ -64,6 +64,8 @@ int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
     HASSERT(addr >= fs_->cfg.phys_addr &&
             (addr + size) <= (fs_->cfg.phys_addr  + fs_->cfg.phys_size));
 
+    HAL_FLASH_Unlock();
+
     if ((addr % 4) && ((addr % 4) + size) < 4)
     {
         // single unaligned write in the middle of a word.
@@ -72,8 +74,11 @@ int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
 
         memcpy(ww.data + (addr % 4), src, size);
         ww.data_word &= *((uint32_t*)(addr & (~0x3)));
-        HASSERT(FlashProgram(&ww.data_word, addr & (~0x3), 4) == 0);
+        HASSERT(HAL_OK ==
+            HAL_FLASH_Program(
+                FLASH_TYPEPROGRAM_WORD, addr & (~0x3), ww.data_word));
 
+        HAL_FLASH_Lock();
         return 0;
     }
 
@@ -86,7 +91,9 @@ int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
 
         memcpy(&ww.data_word, src + size - misaligned, misaligned);
         ww.data_word &= *((uint32_t*)((addr + size) & (~0x3)));
-        HASSERT(FlashProgram(&ww.data_word, (addr + size) & (~0x3), 4) == 0);
+        HASSERT(HAL_OK ==
+            HAL_FLASH_Program(
+                FLASH_TYPEPROGRAM_WORD, (addr + size) & (~0x3), ww.data_word));
 
         size -= misaligned;
     }
@@ -100,7 +107,9 @@ int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
 
         memcpy(ww.data + misaligned, src, 4 - misaligned);
         ww.data_word &= *((uint32_t*)(addr & (~0x3)));
-        HASSERT(FlashProgram(&ww.data_word, addr & (~0x3), 4) == 0);
+        HASSERT(HAL_OK ==
+            HAL_FLASH_Program(
+                FLASH_TYPEPROGRAM_WORD, addr & (~0x3), ww.data_word));
         addr += 4 - misaligned;
         size -= 4 - misaligned;
         src  += 4 - misaligned;
@@ -112,37 +121,49 @@ int32_t CC32x0SFSPIFFS::flash_write(uint32_t addr, uint32_t size, uint8_t *src)
     if (size)
     {
         // the rest of the aligned data
-        uint8_t *flash = (uint8_t*)addr;
+        uint8_t *flash = (uint8_t *)addr;
         for (uint32_t i = 0; i < size; i += 4)
         {
             src[i + 0] &= flash[i + 0];
             src[i + 1] &= flash[i + 1];
             src[i + 2] &= flash[i + 2];
             src[i + 3] &= flash[i + 3];
+            HASSERT(HAL_OK ==
+                HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr + i,
+                    *(unsigned long *)(src + i)));
         }
-
-        HASSERT(FlashProgram((unsigned long*)src, addr, size) == 0);
     }
 
+    HAL_FLASH_Lock();
 
     return 0;
 }
 
 //
-// CC32x0SFSPIFFS::flash_erase()
+// Stm32SPIFFS::flash_erase()
 //
-int32_t CC32x0SFSPIFFS::flash_erase(uint32_t addr, uint32_t size)
+int32_t Stm32SPIFFS::flash_erase(uint32_t addr, uint32_t size)
 {
+    extern char __flash_fs_start;
+    extern char __flash_fs_sector_start;
     HASSERT(addr >= fs_->cfg.phys_addr &&
             (addr + size) <= (fs_->cfg.phys_addr  + fs_->cfg.phys_size));
-    HASSERT((size % ERASE_PAGE_SIZE) == 0);
 
-    while (size)
-    {
-        HASSERT(FlashErase(addr) == 0);
-        addr += ERASE_PAGE_SIZE;
-        size -= ERASE_PAGE_SIZE;
-    }
+    FLASH_EraseInitTypeDef erase_init;
+    memset(&erase_init, 0, sizeof(erase_init));
+
+    erase_init.TypeErase = TYPEERASE_SECTORS;
+    unsigned offset = addr - (unsigned)&__flash_fs_start;
+    HASSERT((size % ERASE_PAGE_SIZE) == 0);
+    HASSERT((offset % ERASE_PAGE_SIZE) == 0);
+    unsigned sector = offset / ERASE_PAGE_SIZE;
+    erase_init.Sector = ((unsigned)(&__flash_fs_sector_start)) + sector;
+    erase_init.NbSectors = size / ERASE_PAGE_SIZE;
+    erase_init.VoltageRange = FLASH_VOLTAGE_RANGE_3; // 3.3 to 3.6 volts powered.
+    HAL_FLASH_Unlock();
+    uint32_t sector_error;
+    HASSERT(HAL_OK == HAL_FLASHEx_Erase(&erase_init, &sector_error));
+    HAL_FLASH_Lock();
 
     return 0;
 }
