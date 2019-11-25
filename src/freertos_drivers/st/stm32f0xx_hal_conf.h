df8be9cf1d50079ade148c3342a805a463e1a49a
Stuart W Baker <stuart_w_baker@ymail.com>
2019-11-25 17:48:31 -0600

    Bakerstu stm32f0 bare metal build (#335)
    * Add build support for stm32f0 bare metal.
    
    * fix-up builds.
    
    * fix review comments.

--- src/freertos_drivers/st/stm32f0xx_hal_conf.h
+++ src/freertos_drivers/st/stm32f0xx_hal_conf.h
@@ -104,6 +104,11 @@
 /// Enable module
 #define HAL_WWDG_MODULE_ENABLED
 
+/* Workaround for the broken UNUSED macro */
+#include "stm32f0xx_hal_def.h"
+#undef UNUSED
+#define UNUSED(x) ((void)((uint32_t)(x)))
+
 /* ######################### Oscillator Values adaptation ################### */
 /**
   * @brief Adjust the value of External High Speed oscillator (HSE) used in your application.
