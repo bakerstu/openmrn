747b573f8d868080df9eb1bd2c9d51e193f655b0
Balazs Racz <balazs.racz@gmail.com>
2019-12-29 20:38:23 +0100

    Fixes reported data races with executor shutdown in unit tests.

--- src/executor/Executor.hxx
+++ src/executor/Executor.hxx
@@ -37,6 +37,7 @@
 #define _EXECUTOR_EXECUTOR_HXX_
 
 #include <functional>
+#include <atomic>
 
 #include "executor/Executable.hxx"
 #include "executor/Notifiable.hxx"
@@ -231,9 +232,9 @@ private:
 
     /** Set to 1 when the executor thread has exited and it is safe to delete
      * *this. */
-    unsigned done_ : 1;
+    std::atomic_uint_fast8_t done_;
     /// 1 if the executor is already running
-    unsigned started_ : 1;
+    std::atomic_uint_fast8_t started_;
     /// How many executables we schedule blindly before calling a select() in
     /// order to find more data to read/write in the FDs being waited upon.
     unsigned selectPrescaler_ : 5;
