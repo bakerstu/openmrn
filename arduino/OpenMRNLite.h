a6fb565d5da15ef6649e38cede2a18ab581e42a9
Robert Heller <heller@deepsoft.com>
2020-02-02 11:12:59 -0500

    Add missing NUL (EOS) to CDI config in OpenMRNLite.h

--- arduino/OpenMRNLite.h
+++ arduino/OpenMRNLite.h
@@ -463,7 +463,7 @@ public:
         ConfigDef cfg(config.offset());
         cfg.config_renderer().render_cdi(&cdi_string);
         
-        cdi_string += '\n';   // Add missing but needed newline  RPH
+        cdi_string += '\0';   // Add missing but needed NUL (EOS)  RPH
 
         bool need_write = false;
         FILE *ff = fopen(filename, "rb");
