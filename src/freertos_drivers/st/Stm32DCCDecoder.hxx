698ff690e365b637d689e947a35175254dd32785
Balazs Racz <balazs.racz@gmail.com>
2020-04-10 20:40:28 +0200

    Implements the STM32 based DCC decoder module.

--- src/freertos_drivers/st/Stm32DCCDecoder.hxx
+++ src/freertos_drivers/st/Stm32DCCDecoder.hxx
@@ -24,37 +24,27 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * \file TivaNRZ.hxx
+ * \file Stm32DCCDecoder.hxx
  *
- * Device driver for TivaWare to decode DCC track signal.
+ * Device driver module for STM32 to decode a DCC track signal.
  *
  * @author Balazs Racz
  * @date 29 Nov 2014
  */
 
-#include "TivaDCC.hxx"  // for FixedQueue
-#include "TivaGPIO.hxx" // for pin definitions
+#include "Stm32Gpio.hxx" // for pin definitions
 #include "freertos_drivers/common/DccDecoder.hxx"
+#include "FreeRTOSConfig.h"
+
+#include "stm32f_hal_conf.hxx"
 
 typedef DummyPin PIN_RailcomCutout;
 
 /**
-  Device driver for decoding a DCC signal on a TI Tiva class microcontroller.
+  Helper module for decoding a DCC signal on an STM32 class microcontroller.
  
-  This driver exports a filesystem device node, but that file node is not
-  usable for reading or writing anything at the moment. The only feature
-  supported by this device driver right now is that it is able to tell a
-  RailcomDriver when the railcom cutout is supposed to start, when we're in the
-  middle and when it is over. This is necessary for the correct functionality
-  of the railcom driver.
-
-  @todo: implement actual packet decoding and sending back to the application
-  layer.
-
-  Usage: Define a structure declaring your hardware information. See below for
-  what you need to define in there. Instantiate the device driver and pass the
-  pointer to the railcom driver to the constructor. There is no need to touch
-  the device from the application layer.
+  Usage: Define a structure `HW` declaring certain constants around your timer
+  usage.
 
   Example hardware definitions:
 
@@ -82,7 +72,7 @@ struct DCCDecode
  */
 
 
-template <class HW> class TivaDccTimerModule
+template <class HW> class Stm32DccTimerModule
 {
 public:
     /// Exports the input pin to the driver on the module interface.
@@ -93,8 +83,9 @@ public:
     /// This is the counter from which the timer starts counting down. When the
     /// timer overflows, it starts from this value.
     static constexpr uint32_t TIMER_MAX_VALUE = HW::TIMER_MAX_VALUE;
-    /// After how many timer counts we should take one sample.
-    static constexpr uint32_t SAMPLE_PERIOD_CLOCKS = HW::SAMPLE_PERIOD_CLOCKS;
+    /// After how many timer counts we should take one sample for occupancy
+    /// feedback.
+    static constexpr uint32_t SAMPLE_PERIOD_CLOCKS = HW::SAMPLE_PERIOD_TICKS;
     /// Length of the device queue.
     static constexpr unsigned Q_SIZE = HW::Q_SIZE;
     
@@ -109,7 +100,7 @@ public:
 
     /// Calls a software interrupt.
     static void trigger_os_interrupt() {
-        MAP_IntPendSet(HW::OS_INTERRUPT);
+        NVIC_SetPendingIRQ(HW::OS_IRQn);
     }
 
     /// hook
@@ -156,30 +147,33 @@ public:
     static void set_cap_timer_delay_usec(int usec)
     {
         Debug::DccPacketDelay::toggle();
-        uint32_t new_match_v = usec * 80;
-        MAP_TimerMatchSet(HW::TIMER_BASE, HW::RCOM_TIMER, 0xfffe - new_match_v);
-        MAP_TimerPrescaleMatchSet(HW::TIMER_BASE, HW::RCOM_TIMER, 0);
+        // This code handles underflow of the timer correctly. We cannot wait
+        // longer than one full cyucle though (65 msec -- typical RailCom waits
+        // are 20-500 usec).
+        uint32_t new_match_v = __HAL_TIM_GET_COUNTER(usec_timer_handle()) +
+                                TIMER_MAX_VALUE + 1 - usec;
+        new_match_v &= 0xffff;
+        __HAL_TIM_SET_COMPARE(
+            usec_timer_handle(), HW::USEC_CHANNEL, new_match_v);
+        __HAL_TIM_CLEAR_IT(usec_timer_handle(), HW::USEC_IF);
+        __HAL_TIM_ENABLE_IT(usec_timer_handle(), HW::USEC_IF);
     }
 
     /// Sets the timer to capture mode. Needed for the digitization of DCC
     /// signal bits.
     static void set_cap_timer_capture()
     {
-        MAP_TimerDisable(HW::TIMER_BASE, HW::TIMER);
-        MAP_TimerIntDisable(
-            HW::TIMER_BASE, HW::TIMER_CAP_EVENT);
-        MAP_TimerIntClear(
-            HW::TIMER_BASE, HW::TIMER_CAP_EVENT);
+        TIM_IC_InitTypeDef     channel_init;
+        memset(&channel_init, 0, sizeof(channel_init));
+        channel_init.ICPolarity = TIM_ICPOLARITY_BOTHEDGE;
+        channel_init.ICSelection = TIM_ICSELECTION_DIRECTTI;
+        channel_init.ICPrescaler = TIM_ICPSC_DIV1;
+        channel_init.ICFilter = HW::CAPTURE_FILTER;
+        HASSERT(HAL_TIM_IC_ConfigChannel(capture_timer_handle(), &channel_init,
+                    HW::CAPTURE_CHANNEL) == HAL_OK);
 
-        MAP_TimerConfigure(
-            HW::TIMER_BASE, HW::CFG_TIM_CAPTURE | HW::CFG_RCOM_TIMER);
-        MAP_TimerControlEvent(
-            HW::TIMER_BASE, HW::TIMER, TIMER_EVENT_BOTH_EDGES);
-        MAP_TimerLoadSet(HW::TIMER_BASE, HW::TIMER, HW::TIMER_MAX_VALUE);
-        MAP_TimerPrescaleSet(HW::TIMER_BASE, HW::TIMER, HW::PS_MAX);
-
-        MAP_TimerIntEnable(HW::TIMER_BASE, HW::TIMER_CAP_EVENT);
-        MAP_TimerEnable(HW::TIMER_BASE, HW::TIMER);
+        HASSERT(
+            HAL_TIM_IC_Start_IT(capture_timer_handle(), HW::CAPTURE_CHANNEL) == HAL_OK);
     }
 
     /// Sets the timer to oneshot (timer) mode. Called once, then
@@ -187,99 +181,192 @@ public:
     /// each to deliver an rcom_interrupt().
     static void set_cap_timer_time()
     {
-        MAP_TimerDisable(HW::TIMER_BASE, HW::RCOM_TIMER);
+        TIM_OC_InitTypeDef channel_init;
+        memset(&channel_init, 0, sizeof(channel_init));
+        channel_init.OCMode = TIM_OCMODE_TIMING; // frozen -- no output
+        channel_init.Pulse = (__HAL_TIM_GET_COUNTER(usec_timer_handle()) + 1) &
+            0xffff; // will be reloaded in the delay_usec function.
+        // the rest are irrelevant.
+        channel_init.OCPolarity = TIM_OCPOLARITY_HIGH;
+        channel_init.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
+        channel_init.OCFastMode   = TIM_OCFAST_DISABLE;
+        channel_init.OCIdleState  = TIM_OCIDLESTATE_RESET;
+        channel_init.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+        HASSERT(HAL_TIM_OC_ConfigChannel(usec_timer_handle(), &channel_init,
+                    HW::USEC_CHANNEL) == HAL_OK);
 
-        MAP_TimerIntDisable(
-            HW::TIMER_BASE, HW::TIMER_RCOM_MATCH);
-        MAP_TimerIntClear(
-            HW::TIMER_BASE, HW::TIMER_RCOM_MATCH);
-        HW::clr_tim_mrsu();
-        MAP_TimerLoadSet(HW::TIMER_BASE, HW::RCOM_TIMER, 0xfffe);
-        MAP_TimerPrescaleSet(HW::TIMER_BASE, HW::RCOM_TIMER, 0);
-
-        MAP_TimerIntEnable(HW::TIMER_BASE, HW::TIMER_RCOM_MATCH);
-        MAP_TimerEnable(HW::TIMER_BASE, HW::RCOM_TIMER);
+        HASSERT(HAL_TIM_OC_Start_IT(usec_timer_handle(), HW::USEC_CHANNEL) ==
+            HAL_OK);
+        // Disable interrupt until the delay_usec() is called.
+        __HAL_TIM_DISABLE_IT(usec_timer_handle(), HW::USEC_IF);
     }
 
     /// Called once inline in an interrupt. Signals that the delay timer is not
     /// needed anymore.
     static void stop_cap_timer_time()
     {
-        MAP_TimerDisable(HW::TIMER_BASE, HW::RCOM_TIMER);
+        __HAL_TIM_DISABLE_IT(usec_timer_handle(), HW::USEC_IF);
+        TIM_CCxChannelCmd(
+            usec_timer(), HW::USEC_CHANNEL, TIM_CCx_DISABLE);
     }
 
-    DISALLOW_COPY_AND_ASSIGN(TivaDccTimerModule);
+
+private:
+    static TIM_HandleTypeDef captureTimerHandle_;
+    static TIM_HandleTypeDef usecTimerHandle_;
+
+    /// Initializes a timer resource (shared for all channels).
+    /// @param handle pointer to the HAL timer handle.
+    /// @param instance pointer to the timer registers, such as TIM3.
+    static void init_timer(TIM_HandleTypeDef *handle, TIM_TypeDef *instance)
+    {
+        memset(handle, 0, sizeof(*handle));
+        handle->Instance = instance;
+        handle->Init.Period = 0xFFFF;
+        // 1 usec per tick
+        handle->Init.Prescaler = configCPU_CLOCK_HZ / 1000000;
+        handle->Init.ClockDivision = 0;
+        handle->Init.CounterMode = TIM_COUNTERMODE_DOWN;
+        handle->Init.RepetitionCounter = 0;
+        handle->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+        HASSERT(HAL_TIM_IC_Init(handle) == HAL_OK);
+    }
+
+    /// @return the hardware pointer to the capture timer.
+    static TIM_TypeDef* capture_timer() {
+        return reinterpret_cast<TIM_TypeDef*>(HW::CAPTURE_TIMER);
+    }
+    /// @return the HAL handle to the capture timer.
+    static TIM_HandleTypeDef* capture_timer_handle() {
+        return &captureTimerHandle_;
+    }
+    
+    /// @return the hardware pointer to the usec timer.
+    static TIM_TypeDef* usec_timer() {
+        return reinterpret_cast<TIM_TypeDef*>(HW::USEC_TIMER);
+    }
+    /// @return the HAL handle to the usec timer (which may alias to the
+    /// capture timer handle).
+    static TIM_HandleTypeDef* usec_timer_handle() {
+        if (shared_timers()) {
+            return capture_timer_handle();
+        }
+        return &usecTimerHandle_;
+    }
+    
+    /// @return true if the same timer resource (on different channels) is used
+    /// for both the capture and the usec timer.
+    static bool shared_timers() {
+        return HW::CAPTURE_TIMER == HW::USEC_TIMER;
+    }
+    
+    /// Private constructor. This class cannot be instantiated.
+    Stm32DccTimerModule();
+    
+    DISALLOW_COPY_AND_ASSIGN(Stm32DccTimerModule);
 };
 
 template <class HW>
-void TivaDccTimerModule<HW>::module_init()
+TIM_HandleTypeDef Stm32DccTimerModule<HW>::captureTimerHandle_;
+template <class HW> TIM_HandleTypeDef Stm32DccTimerModule<HW>::usecTimerHandle_;
+
+template <class HW>
+void Stm32DccTimerModule<HW>::module_init()
 {
-    MAP_SysCtlPeripheralEnable(HW::TIMER_PERIPH);
-    MAP_GPIOPadConfigSet(HW::NRZ_Pin::GPIO_BASE, HW::NRZ_Pin::GPIO_PIN,
-                         GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    memset(&captureTimerHandle_, 0, sizeof(captureTimerHandle_));
+    memset(&usecTimerHandle_, 0, sizeof(usecTimerHandle_));
+    NRZ_Pin::hw_init();
 }
 
-template <class HW> void TivaDccTimerModule<HW>::module_enable()
+template <class HW> void Stm32DccTimerModule<HW>::module_enable()
 {
-    MAP_TimerClockSourceSet(HW::TIMER_BASE, TIMER_CLOCK_SYSTEM);
-    MAP_TimerControlStall(HW::TIMER_BASE, HW::TIMER, true);
+    init_timer(capture_timer_handle(), capture_timer());
+    
+    GPIO_InitTypeDef gpio_init;
+    memset(&gpio_init, 0, sizeof(gpio_init));
+    gpio_init.Mode = GPIO_MODE_AF_PP;
+    gpio_init.Pull = GPIO_PULLUP;
+    gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+    gpio_init.Alternate = HW::CAPTURE_AF_MODE;
+    gpio_init.Pin = HW::NRZ_Pin::pin();
+    HAL_GPIO_Init(HW::NRZ_Pin::port(), &gpio_init);
 
     set_cap_timer_capture();
 
-    MAP_TimerIntEnable(HW::TIMER_BASE, HW::TIMER_CAP_EVENT);
+    if (!shared_timers())
+    {
+        init_timer(usec_timer_handle(), usec_timer());
+    }
 
-    MAP_IntPrioritySet(HW::TIMER_INTERRUPT, 0x20);
-    MAP_IntPrioritySet(HW::RCOM_INTERRUPT, 0x20);
-    MAP_IntPrioritySet(HW::OS_INTERRUPT, configKERNEL_INTERRUPT_PRIORITY);
-    MAP_IntEnable(HW::OS_INTERRUPT);
-    MAP_IntEnable(HW::TIMER_INTERRUPT);
-    MAP_IntEnable(HW::RCOM_INTERRUPT);
+#if defined(GCC_ARMCM0)    
+    HAL_NVIC_SetPriority(HW::CAPTURE_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(HW::TIMER_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(HW::OS_IRQn, 3, 0);
+#elif defined(GCC_ARMCM3)    
+    NVIC_SetPriority(HW::CAPTURE_IRQn, 0x20);
+    NVIC_SetPriority(HW::TIMER_IRQn, 0x20);
+    NVIC_SetPriority(HW::OS_IRQn, configKERNEL_INTERRUPT_PRIORITY);
+#else
+#error not defined how to set interrupt priority
+#endif    
+    
+    NVIC_EnableIRQ(HW::CAPTURE_IRQn);
+    NVIC_EnableIRQ(HW::TIMER_IRQn);
+    NVIC_EnableIRQ(HW::OS_IRQn);
 }
 
-template <class HW> void TivaDccTimerModule<HW>::module_disable()
+template <class HW> void Stm32DccTimerModule<HW>::module_disable()
 {
-    MAP_IntDisable(HW::TIMER_INTERRUPT);
-    MAP_IntDisable(HW::RCOM_INTERRUPT);
-    MAP_IntDisable(HW::OS_INTERRUPT);
-    MAP_TimerDisable(HW::TIMER_BASE, HW::TIMER);
-    MAP_TimerDisable(HW::TIMER_BASE, HW::RCOM_TIMER);
+    NVIC_DisableIRQ(HW::CAPTURE_IRQn);
+    NVIC_DisableIRQ(HW::TIMER_IRQn);
+    NVIC_DisableIRQ(HW::OS_IRQn);
+    HASSERT(HAL_TIM_IC_Stop_IT(capture_timer_handle(), HW::CAPTURE_CHANNEL) ==
+        HAL_OK);
+
+    HASSERT(
+        HAL_TIM_OC_Stop_IT(usec_timer_handle(), HW::USEC_CHANNEL) == HAL_OK);
+    if (!shared_timers())
+    {
+        HASSERT(HAL_TIM_IC_DeInit(usec_timer_handle()) == HAL_OK);
+    }
+    HASSERT(HAL_TIM_IC_DeInit(capture_timer_handle()) == HAL_OK);
 }
 
 template <class HW>
-bool TivaDccTimerModule<HW>::int_get_and_clear_capture_event()
+bool Stm32DccTimerModule<HW>::int_get_and_clear_capture_event()
 {
-    // get masked interrupt status
-    auto status = MAP_TimerIntStatus(HW::TIMER_BASE, true);
-    // TODO(balazs.racz): Technically it is possible that the timer reload
-    // happens between the event match and the interrupt entry. In this case we
-    // will incorrectly add a full cycle to the event length.
-    if (status & HW::TIMER_CAP_EVENT)
-    {
+    if (__HAL_TIM_GET_FLAG(capture_timer_handle(), HW::CAPTURE_IF)) {
         //Debug::DccDecodeInterrupts::toggle();
         HW::cap_event_hook();
-        MAP_TimerIntClear(HW::TIMER_BASE, HW::TIMER_CAP_EVENT);
+        __HAL_TIM_CLEAR_FLAG(capture_timer_handle(), HW::CAPTURE_IF);
+        // TODO(balazs.racz): Technically it is possible that the timer reload
+        // happens between the event match and the interrupt entry. In this
+        // case we will incorrectly add a full cycle to the event length.
         return true;
     }
     return false;
 }
 
 template <class HW>
-uint32_t TivaDccTimerModule<HW>::get_capture_counter()
+uint32_t Stm32DccTimerModule<HW>::get_capture_counter()
 {
-    return MAP_TimerValueGet(HW::TIMER_BASE, HW::TIMER);
+    return HAL_TIM_ReadCapturedValue(
+        capture_timer_handle(), HW::CAPTURE_CHANNEL);
 }
 
 template <class HW>
-bool TivaDccTimerModule<HW>::int_get_and_clear_delay_event()
+bool Stm32DccTimerModule<HW>::int_get_and_clear_delay_event()
 {
-    Debug::DccDecodeInterrupts::set(true);
-    auto status = MAP_TimerIntStatus(HW::TIMER_BASE, true);
-    if (status & HW::TIMER_RCOM_MATCH)
-    {
-        MAP_TimerIntClear(HW::TIMER_BASE, HW::TIMER_RCOM_MATCH);
+    if (__HAL_TIM_GET_FLAG(usec_timer_handle(), HW::USEC_IF)) {
+        Debug::DccDecodeInterrupts::set(true);
+        __HAL_TIM_CLEAR_IT(usec_timer_handle(), HW::USEC_IF);
+        // we also disable the interrupt until it is reenabled by loading a new
+        // usec delay target.
+        __HAL_TIM_DISABLE_IT(usec_timer_handle(), HW::USEC_IF);
         return true;
     }
     return false;
 }
 
-template<class HW> using TivaDccDecoder = DccDecoder<TivaDccTimerModule<HW> >;
+template<class HW> using Stm32DccDecoder = DccDecoder<Stm32DccTimerModule<HW> >;
