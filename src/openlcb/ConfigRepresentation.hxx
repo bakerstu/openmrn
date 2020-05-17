161b55c5f07da5d90e3edbfa20aea9ea6a0ba71f
Balazs Racz <balazs.racz@gmail.com>
2020-05-17 15:55:11 +0200

    Adds support for templated classes in the group options. (#391)

--- src/openlcb/ConfigRepresentation.hxx
+++ src/openlcb/ConfigRepresentation.hxx
@@ -205,7 +205,10 @@ public:
     static constexpr typename decltype(                                        \
         TYPE::config_renderer())::OptionsType NAME##_options()                 \
     {                                                                          \
-        return decltype(TYPE::config_renderer())::OptionsType(__VA_ARGS__);    \
+        using SelfType = TYPE;                                                 \
+        using OptionsType =                                                    \
+            typename decltype(SelfType::config_renderer())::OptionsType;       \
+        return OptionsType(__VA_ARGS__);                                       \
     }                                                                          \
     static void render_content_cdi(                                            \
         const openlcb::EntryMarker<LINE> &, std::string *s)                    \
