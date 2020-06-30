94ef0903de59132ede319137546428f9b0e42eaf
Balazs Racz <balazs.racz@gmail.com>
2020-07-01 00:32:55 +0200

    Adds implementation of write_string_to_file for node.js

--- src/utils/FileUtils.cxx
+++ src/utils/FileUtils.cxx
@@ -51,6 +51,14 @@ string read_file_to_string(const string &filename)
     return contents;
 }
 
+void write_string_to_file(const string &filename, const string &data)
+{
+    using emscripten::val;
+    EM_ASM(var fs = require('fs'); Module.fs = fs;);
+    val fs = val::module_property("fs");
+    fs.call<void>("writeFileSync", string(filename), string(data));
+}
+
 #else
 
 #include <stdio.h>
