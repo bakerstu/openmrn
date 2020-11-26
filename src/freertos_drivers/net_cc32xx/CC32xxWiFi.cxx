6f3a419c7acc2f4a4acd3ac2569dd9f40e1223eb
Balazs Racz <balazs.racz@gmail.com>
2020-11-26 22:49:36 +0100

    Adds API call to set the wifi MAC address.

--- src/freertos_drivers/net_cc32xx/CC32xxWiFi.cxx
+++ src/freertos_drivers/net_cc32xx/CC32xxWiFi.cxx
@@ -566,6 +566,16 @@ void CC32xxWiFi::wlan_mac(uint8_t mac[6])
 }
 
 /*
+ * CC32xxWiFi::wlan_set_mac()
+ */
+void CC32xxWiFi::wlan_set_mac(uint8_t mac[6])
+{
+    uint16_t len = 6;
+
+    sl_NetCfgSet(SL_NETCFG_MAC_ADDRESS_SET, 1, 6, mac);
+}
+
+/*
  * CC32xxWiFi::test_mode_start()
  */
 void CC32xxWiFi::test_mode_start()
