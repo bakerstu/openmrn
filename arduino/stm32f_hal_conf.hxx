7e78b2326bccb092ed09e4da592b41c887418a1e
Balazs Racz <balazs.racz@gmail.com>
2021-04-06 00:17:10 +0200

    Adds missing include guard for STM32 arduino header. (#531)

--- arduino/stm32f_hal_conf.hxx
+++ arduino/stm32f_hal_conf.hxx
@@ -1,3 +1,5 @@
+#ifndef _OPENMRN_ARDUINO_STM32F_HAL_CONF_HXX
+#define _OPENMRN_ARDUINO_STM32F_HAL_CONF_HXX
 
 #include <stm32yyxx_hal_conf.h>
 
@@ -9,3 +11,5 @@ static inline void SetInterruptPriority(uint32_t irq, uint8_t priority)
 #ifndef configKERNEL_INTERRUPT_PRIORITY
 #define configKERNEL_INTERRUPT_PRIORITY 0xA0
 #endif
+
+#endif // _OPENMRN_ARDUINO_STM32F_HAL_CONF_HXX
