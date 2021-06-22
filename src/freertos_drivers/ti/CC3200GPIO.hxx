d0c51ceeb71c5588bfd6fdaa7de645e554e02557
Balazs Racz <balazs.racz@gmail.com>
2021-06-22 20:54:39 +0200

    Slow down the GPIO writes a bit.

--- src/freertos_drivers/ti/CC3200GPIO.hxx
+++ src/freertos_drivers/ti/CC3200GPIO.hxx
@@ -171,6 +171,7 @@ public:
         volatile uint8_t *ptr = reinterpret_cast<uint8_t *>(
             GPIO_BASE + (((unsigned)GPIO_PIN) << 2));
         *ptr = value ? 0xff : 0;
+        __asm__ volatile("\t nop\n");
     }
     /// @return current value of the input pin: if true HIGH.
     static bool __attribute__((always_inline)) get()
