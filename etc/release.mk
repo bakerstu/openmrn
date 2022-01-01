81708afebf6c5ca8730fafdca8b1014b3457d807
Balazs Racz <balazs.racz@gmail.com>
2022-01-01 21:16:26 +0100

    Adds javascript release binary targets. Adds license.txt to the release zips.
    Makes the js_hub be compatible with node.js pkg tool.

--- etc/release.mk
+++ etc/release.mk
@@ -27,3 +27,37 @@ endef
 define RELEASE_BIN_template
 $(eval $(call RELEASE_BIN_template_helper,$(1),$(2)))
 endef
+
+
+### Call this template for each JS application that should be built for a
+### release. The call site should be in the toplevel makefile.
+###
+### Arguments: app-name target-path
+###
+### example: $(call RELEASE_JS_template,openmrn-bootloader-client,applications/bootloader_client/targets/js.emscripten)
+define RELEASE_JS_template_helper
+
+release-js-all: $(JSRELDIR)/win/$(1)-win.exe
+
+$(JSRELDIR)/win/$(1)-win.exe: $(2)/$(1)-win.exe
+	mkdir -p $(JSRELDIR)/win $(JSRELDIR)/macos
+	cp $(2)/$(1)-win.exe $(JSRELDIR)/win/$(1)-win.exe
+	cp $(2)/$(1)-macos $(JSRELDIR)/macos/$(1)-macos
+
+$(2)/$(1)-win.exe:
+	+$(MAKE) -C $(2)
+	+$(MAKE) -C $(2) release
+
+release-clean: release-clean-$(1)
+
+release-clean-$(1):
+	$(MAKE) -C $(2) clean rclean
+	rm -rf $(2)/$(1)-win.exe $(2)/$(1)-macos $(2)/$(1)-linux
+
+endef
+
+define RELEASE_JS_template
+$(eval $(call RELEASE_JS_template_helper,$(1),$(2)))
+endef
+
+
