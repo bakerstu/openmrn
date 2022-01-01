52f305faaad616d8b4f0a31d2512465e4bae2210
Balazs Racz <balazs.racz@gmail.com>
2022-01-02 00:32:41 +0100

    Adds binary release script. (#599)
    Adds script (in the makefile) to compile a binary release for OpenMRN.
    The binary release contains the standalone applications that are useful
    outside of OpenMRN. Currently this list is as follows:
    - hub
    - bootloader_client
    - memconfig_utils
    - send_datagram
    
    The binary release script compiles these applications using the host g++ compiler, and packages it as a zip file.
    
    Also added is a release-js script which compiles two applications using node.js package for win.exe and macos binaries.
    These are:
    - js_hub
    - bootloader_client
    
    ===
    
    * Adds binary release script.
    
    Adds script (in the makefile) to compile a binary release for OpenMRN.
    The binary release contains the standalone applications that are useful
    outside of OpenMRN. Currently this list is as follows:
    - hub
    - bootloader_client
    - memconfig_utils
    - send_datagram
    
    The binary release script compiles these applications using the host g++ compiler, and packages it as a zip file.
    
    * renames the zipfile to "applications.OS.CPU.zip"
    
    * Adds javascript release binary targets.
    Adds license.txt to the release zips.
    
    Makes the js_hub be compatible with node.js pkg tool.
    
    * Adds documentation on how to do a release.
    
    * Adds documentation of release numbers.

---
+++ etc/release.mk
@@ -0,0 +1,63 @@
+# Helper makefile for building releases of OpenMRN.
+
+
+### Call this template for each binary application that should be built for a
+### release. The call site should be in the toplevel makefile.
+###
+### Arguments: app-name target-path
+###
+### example: $(call RELEASE_BIN_template,hub,applications/hub/targets/linux.x86)
+define RELEASE_BIN_template_helper
+
+release-bin-all: $(RELDIR)/$(1)
+
+$(RELDIR)/$(1): $(2)/$(1)
+	strip -o $$@ $$<
+
+$(2)/$(1):
+	$(MAKE) -C $(2)
+
+release-clean: release-clean-$(1)
+
+release-clean-$(1):
+	$(MAKE) -C $(2) clean rclean
+
+endef
+
+define RELEASE_BIN_template
+$(eval $(call RELEASE_BIN_template_helper,$(1),$(2)))
+endef
+
+
+### Call this template for each JS application that should be built for a
+### release. The call site should be in the toplevel makefile.
+###
+### Arguments: app-name target-path
+###
+### example: $(call RELEASE_JS_template,openmrn-bootloader-client,applications/bootloader_client/targets/js.emscripten)
+define RELEASE_JS_template_helper
+
+release-js-all: $(JSRELDIR)/win/$(1)-win.exe
+
+$(JSRELDIR)/win/$(1)-win.exe: $(2)/$(1)-win.exe
+	mkdir -p $(JSRELDIR)/win $(JSRELDIR)/macos
+	cp $(2)/$(1)-win.exe $(JSRELDIR)/win/$(1)-win.exe
+	cp $(2)/$(1)-macos $(JSRELDIR)/macos/$(1)-macos
+
+$(2)/$(1)-win.exe:
+	+$(MAKE) -C $(2)
+	+$(MAKE) -C $(2) release
+
+release-clean: release-clean-$(1)
+
+release-clean-$(1):
+	$(MAKE) -C $(2) clean rclean
+	rm -rf $(2)/$(1)-win.exe $(2)/$(1)-macos $(2)/$(1)-linux
+
+endef
+
+define RELEASE_JS_template
+$(eval $(call RELEASE_JS_template_helper,$(1),$(2)))
+endef
+
+
