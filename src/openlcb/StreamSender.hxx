12ca30158d472421b868f3d8ccfec785e8775628
Balazs Racz <balazs.racz@gmail.com>
2022-04-18 22:33:43 +0200

    Adds code for sending stream data to a CAN network.

---
+++ src/openlcb/StreamSender.hxx
@@ -0,0 +1,362 @@
+/** \copyright
+ * Copyright (c) 2022, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file StreamSender.hxx
+ *
+ * Implementation flow for the Stream Service that sends data to a remote
+ * destination using the stream protocol.
+ *
+ * @author Balazs Racz
+ * @date 17 Apr 2022
+ */
+
+#ifndef _OPENLCB_STREAMSENDER_HXX_
+#define _OPENLCB_STREAMSENDER_HXX_
+
+#include "executor/StateFlow.hxx"
+#include "openlcb/IfCan.hxx"
+#include "openlcb/CanDefs.hxx"
+#include "openlcb/StreamDefs.hxx"
+#include "openlcb/DatagramDefs.hxx"
+#include "utils/ByteBuffer.hxx"
+#include "utils/LimitedPool.hxx"
+
+namespace openlcb
+{
+
+/// Helper class for sending stream data to a CAN interface.
+/// @todo add progress report API.
+class StreamSenderCan : public StateFlow<ByteBuffer, QList<1> >
+{
+public:
+    Action entry()
+    {
+        if (requestInit_) {
+            /// @todo get the destination address somehow.
+            requestInit_ = 0;
+            return call_immediately(STATE(initiate_stream));
+        }
+        if (!streamWindowRemaining_)
+        {
+            // We ran out of the current stream window size.
+            return wait_and_call(STATE(wait_for_stream_proceed));
+        }
+        if (!remaining())
+        {
+            // We ran out of the current chunk of stream payload from the
+            // source.
+            return release_and_exit();
+        }
+        return call_immediately(STATE(allocate_can_buffer));
+    }
+
+private:
+    Action initiate_stream()
+    {
+        return allocate_and_call(
+            node_->iface()->addressed_message_write_flow(),
+            STATE(send_init_stream));
+    }
+
+    Action send_init_stream()
+    {
+        auto *b = get_allocation_result(
+            node_->iface()->addressed_message_write_flow());
+        b->data()->reset(Defs::MTI_STREAM_INITIATE_REQUEST, node_->node_id(),
+            dst_,
+            StreamDefs::create_initiate_request(
+                StreamDefs::MAX_PAYLOAD, false, localStreamId_));
+
+        node_->iface()->dispatcher()->register_handler(
+            &streamInitiateReplyHandler_, Defs::MTI_STREAM_INITIATE_REPLY,
+            Defs::MTI_EXACT);
+        
+        node_->iface()->addressed_message_write_flow()->send(b);
+        sleeping_ = true;
+        return sleep_and_call(&timer_, SEC_TO_NSEC(STREAM_INIT_TIMEOUT_SEC),
+            STATE(received_init_stream));
+    }
+
+    void stream_initiate_replied(Buffer<GenMessage> *message)
+    {
+        auto rb = get_buffer_deleter(message);
+        if (message->data()->dstNode != node_ ||
+            !node_->iface()->matching_node(dst_, message->data()->src))
+        {
+            // Not for me.
+            return;
+        }
+        const auto &payload = message->data()->payload;
+        if (payload.size() < 6 || payload[4] != localStreamId_)
+        {
+            // Talking about another stream or incorrect data.
+            return;
+        }
+        dstStreamId_ = payload[5];
+        streamFlags_ = payload[2];
+        streamAdditionalFlags_ = payload[3];
+        streamWindowSize_ = (payload[0] << 8) | payload[1];
+
+        // We save the remote alias here if we haven't got any yet.
+        if (message->data()->src.alias)
+        {
+            dst_.alias = message->data()->src.alias;
+        }
+        timer_.trigger();
+    }
+
+    Action received_init_stream()
+    {
+        sleeping_ = false;
+        node_->iface()->dispatcher()->unregister_handler(
+            &streamInitiateReplyHandler_, Defs::MTI_STREAM_INITIATE_REPLY,
+            Defs::MTI_EXACT);
+        if (!(streamFlags_ & StreamDefs::FLAG_ACCEPT))
+        {
+            if (streamFlags_ & StreamDefs::FLAG_PERMANENT_ERROR)
+            {
+                return return_error(
+                    DatagramDefs::PERMANENT_ERROR | streamAdditionalFlags_,
+                    "Stream initiate request was denied (permanent error).");
+            }
+            else
+            {
+                return return_error(
+                    Defs::ERROR_TEMPORARY | streamAdditionalFlags_,
+                    "Stream initiate request was denied (temporary error).");
+            }
+        }
+        if (!streamWindowSize_)
+        {
+            return return_error(DatagramDefs::PERMANENT_ERROR,
+                "Inconsistency: zero buffer length but "
+                "accepted stream request.");
+        }
+        streamWindowRemaining_ = streamWindowSize_;
+        node_->iface()->dispatcher()->register_handler(
+            &streamProceedHandler_, Defs::MTI_STREAM_PROCEED, Defs::MTI_EXACT);
+        return entry();
+    }
+    
+    /// Allocates a buffer for a CAN frame.
+    Action allocate_can_buffer()
+    {
+        return allocate_and_call(
+            ifCan_->frame_write_flow(), STATE(got_frame), &canFramePool_);
+    }
+
+    /// Got a buffer for an output frame.
+    Action got_frame()
+    {
+        auto *b = get_allocation_result(ifCan_->frame_write_flow());
+
+        uint32_t can_id;
+        NodeAlias local_alias =
+            ifCan_->local_aliases()->lookup(node_->node_id());
+        NodeAlias remote_alias = dst_.alias;
+        CanDefs::set_datagram_fields(
+            &can_id, local_alias, remote_alias, CanDefs::STREAM_DATA);
+        auto *frame = b->data()->mutable_frame();
+        SET_CAN_FRAME_ID_EFF(*frame, can_id);
+
+        size_t len = compute_next_can_length();
+
+        frame->can_dlc = len + 1;
+        frame->data[0] = dstStreamId_;
+        memcpy(&frame->data[1], payload(), len);
+        advance(len);
+
+        ifCan_->frame_write_flow()->send(b);
+        return entry();
+    }
+
+    Action wait_for_stream_proceed()
+    {
+        if (streamWindowRemaining_)
+        {
+            // received early stream_proceed response
+            return call_immediately(STATE(stream_proceed_timeout));
+        }
+        sleeping_ = true;
+        return sleep_and_call(&timer_, SEC_TO_NSEC(STREAM_PROCEED_TIMEOUT_SEC),
+            STATE(stream_proceed_timeout));
+    }
+
+    /// Callback from the handler flow.
+    void stream_proceed_received(Buffer<GenMessage> *message)
+    {
+        auto rb = get_buffer_deleter(message);
+        if (message->data()->dstNode != node_ ||
+            !node_->iface()->matching_node(dst_, message->data()->src))
+        {
+            // Not for me.
+            return;
+        }
+
+        const auto &payload = message->data()->payload;
+        if (payload.size() < 2 || payload[0] != localStreamId_)
+        {
+            // Talking about another stream or incorrect data.
+            return;
+        }
+
+        /// @todo add progress callback API
+
+        streamWindowRemaining_ += streamWindowSize_;
+        if (sleeping_)
+        {
+            timer_.trigger();
+        }
+    }
+
+    Action stream_proceed_timeout()
+    {
+        sleeping_ = false;
+        if (!streamWindowRemaining_) // no proceed arrived
+        {
+            /// @todo (balazs.racz) somehow merge these two actions: remember
+            /// that we timed out and close the stream.
+            return return_error(Defs::ERROR_TEMPORARY,
+                "Times out waiting for stream proceed message.");
+            //return call_immediately(STATE(close_stream));
+        }
+        return entry();
+    }
+
+private:
+    size_t compute_next_can_length()
+    {
+        size_t ret = remaining();
+        // Cannot exceed CAN frame max payload.
+        if (ret > MAX_BYTES_PAYLOAD_PER_CAN_FRAME)
+        {
+            ret = MAX_BYTES_PAYLOAD_PER_CAN_FRAME;
+        }
+        // Cannot exceed remaining bytes in stream window.
+        if (ret > streamWindowRemaining_)
+        {
+            ret = streamWindowRemaining_;
+        }
+        return ret;
+    }
+
+    /// @return the number of bytes available in the current chunk.
+    size_t remaining()
+    {
+        return message()->data()->size_;
+    }
+
+    /// @return pointer to the beginning of the data to send.
+    uint8_t *payload()
+    {
+        return message()->data()->data_;
+    }
+
+    /// Consumes a certain number of bytes from the beginning of the data to
+    /// send.
+    /// @param num_bytes how much data to consume.
+    void advance(size_t num_bytes)
+    {
+        message()->data()->advance(num_bytes);
+        totalByteCount_ += num_bytes;
+        streamWindowRemaining_ -= num_bytes;
+    }
+
+    Action return_error(uint32_t code, string message) {
+        errorCode_ = code;
+        /// @todo mark that we are in an error state.
+        return release_and_exit();
+    }
+    
+    /// How many seconds for waiting for a stream proceed before we give up
+    /// with a timeout.
+    static constexpr size_t STREAM_PROCEED_TIMEOUT_SEC = 20;
+
+    /// How many seconds for waiting for a stream init before we give up
+    /// with a timeout.
+    static constexpr size_t STREAM_INIT_TIMEOUT_SEC = 20;
+    
+    /// How many bytes payload we can copy into a single CAN frame.
+    static constexpr size_t MAX_BYTES_PAYLOAD_PER_CAN_FRAME = 7;
+
+    /// How many CAN frames should we allocate at a given time.
+    static constexpr size_t MAX_FRAMES_IN_FLIGHT = 4;
+
+    /// How many bytes the allocation of a single CAN frame should be.
+    static constexpr size_t CAN_FRAME_ALLOC_SIZE =
+        sizeof(CanFrameWriteFlow::message_type);
+
+    /// Handles incoming stream proceed messages.
+    MessageHandler::GenericHandler streamProceedHandler_ {
+        this, &StreamSenderCan::stream_proceed_received};
+    /// Handles incoming stream initiate reply messages.
+    MessageHandler::GenericHandler streamInitiateReplyHandler_ {
+        this, &StreamSenderCan::stream_initiate_replied};
+
+    /// CAN-bus interface.
+    IfCan *ifCan_;
+    /// Which node are we sending the outgoing data from. This is a local
+    /// virtual node.
+    Node *node_;
+    /// Destination node that we are sending to. It is important that the alias
+    /// is filled in here.
+    NodeHandle dst_;
+    /// How many bytes we have transmitted in this stream so far.
+    size_t totalByteCount_;
+    /// Stream ID at the source node. @todo fill in
+    uint8_t localStreamId_;
+    /// Stream ID at the destination node. @todo fill in
+    uint8_t dstStreamId_;
+    /// True if we are waiting for the timer.
+    uint8_t sleeping_ : 1;
+    /// 1 if there is a pending close request.
+    uint8_t requestClose_ : 1;
+    /// 1 if there is a pending initialize request.
+    uint8_t requestInit_ : 1;
+    /// Flags from the remote node that we got in stream initiate reply
+    uint8_t streamFlags_;
+    /// More flags from the remote node that we got in stream initiate reply
+    uint8_t streamAdditionalFlags_;
+    /// Total stream window size. @todo fill in
+    uint16_t streamWindowSize_;
+    /// Remaining stream window size. @todo fill in
+    uint16_t streamWindowRemaining_;
+    /// When the stream process fails, this variable contains an error code.
+    uint32_t errorCode_;
+    /// Source of buffers for outgoing CAN frames. Limtedpool is allocating and
+    /// releasing to the mainBufferPool, but blocks when we exceed a certain
+    /// number of allocations until some buffers get freed.
+    LimitedPool canFramePool_ {CAN_FRAME_ALLOC_SIZE, MAX_FRAMES_IN_FLIGHT};
+    /// Helper object for timeouts.
+    StateFlowTimer timer_ {this};
+};
+
+class StreamRendererCan : public StateFlow<ByteBuffer, QList<1>>
+{ };
+
+} // namespace openlcb
+
+#endif // _OPENLCB_STREAMSENDER_HXX_
