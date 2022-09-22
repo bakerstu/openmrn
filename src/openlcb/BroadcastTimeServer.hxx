164d7ded45d8350c8f2504ff67ec0b4ffe2de3e5
Stuart Baker <stuart_w_baker@ymail.com>
2022-09-22 00:13:28 -0500

    Add a Broadcast Time sync when a time consumer (range) is identified.

--- src/openlcb/BroadcastTimeServer.hxx
+++ src/openlcb/BroadcastTimeServer.hxx
@@ -164,6 +164,16 @@ private:
                                     EventReport *event,
                                     BarrierNotifiable *done) override;
 
+
+    /// Called on another node sending ConsumerRangeIdentified. @param event
+    /// stores information about the incoming message. Filled: event id, mask
+    /// (!= 1), src_node. Not filled: state.  @param registry_entry gives the
+    /// registry entry for which the current handler is being called. @param
+    /// done must be notified when the processing is done.
+    void handle_consumer_range_identified(
+        const EventRegistryEntry &registry_entry, EventReport *event,
+        BarrierNotifiable *done) override;
+
     /// Handle an incoming event report.
     /// @param entry registry entry for the event range
     /// @param event information about the incoming message
