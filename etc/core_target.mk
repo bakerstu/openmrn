4434c2ab8eda9bfed25f3d3788410bc587ec703c
Balazs Racz <balazs.racz@gmail.com>
2023-08-29 20:18:48 -0400

    Fixes bug when `make -jN tests` does not re-link the test targets after changing a source file in openmrn. We had to run make tests twice.

--- etc/core_target.mk
+++ etc/core_target.mk
@@ -19,3 +19,12 @@ SUBDIRS = $(CORELIBS) $(SYSLIB_SUBDIRS)
 MKSUBDIR_OPENMRNINCLUDE=lib.mk
 
 include $(OPENMRNPATH)/etc/recurse.mk
+
+# lib/timestamp is a dependency to all test binaries. This rule has to have a
+# body to execute, or else make will take the mtime of this file too early in
+# the execution process. The body shall not actually touch the file, as the
+# touches happen in the BUILDDIRS recursions. This workaround ensures that when
+# a source file changes in openmrn, the tests are all re-linked and
+# re-run. Otherwise you need to run tests twice to actually execute them.
+lib/timestamp: $(BUILDDIRS)
+	true
