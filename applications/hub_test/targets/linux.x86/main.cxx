df9671407f97db7a130e3444b52dd7dd2ae6c1b4
Balazs Racz <balazs.racz@gmail.com>
2023-12-29 10:16:22 +0100

    hub-test: Implements links and prints statistics about sent data.

--- applications/hub_test/targets/linux.x86/main.cxx
+++ applications/hub_test/targets/linux.x86/main.cxx
@@ -32,14 +32,23 @@
  * @date 23 Dec 2023
  */
 
+#include <sys/ioctl.h>
+
 #include "nmranet_config.h"
 #include "os/os.h"
+#include "os/sleep.h"
 
+#include "utils/Buffer.hxx"
 #include "utils/ClientConnection.hxx"
 #include "utils/LimitedPool.hxx"
 #include "utils/StringPrintf.hxx"
+#include "utils/Stats.hxx"
 
 OVERRIDE_CONST(gc_generate_newlines, 1);
+OVERRIDE_CONST(gridconnect_bridge_max_outgoing_packets, 2);
+OVERRIDE_CONST(gridconnect_tcp_snd_buffer_size, 1024);
+OVERRIDE_CONST(gridconnect_tcp_rcv_buffer_size, 8192);
+
 
 Executor<1> g_executor("g_executor", 0, 1024);
 Service g_service(&g_executor);
@@ -52,15 +61,13 @@ struct Link
     const char *device_path = nullptr;
     /// Config (cmdline) TCP target port number
     int upstream_port = 12021;
-    /// Config (cmdline) TCP target hostname 
+    /// Config (cmdline) TCP target hostname
     const char *upstream_host = nullptr;
     /// The CAN hub for this link.
     std::unique_ptr<CanHubFlow> can_hub {new CanHubFlow(&g_service)};
     /// The receiver object keeping track of the inputs and stats. Will be
     /// created for every link, but only used for the input ports.
     std::unique_ptr<Receiver> receiver;
-
-    Link();
 };
 
 /// We generate packets to this interface.
@@ -173,115 +180,6 @@ void parse_args(int argc, char *argv[])
     }
 }
 
-
-class Receiver : public CanHubPortInterface
-{
-public:
-    Receiver(CanHubFlow *hub)
-    { }
-
-    void send(message_type *buf, unsigned prio) override
-    {
-        auto rb = get_buffer_deleter(buf);
-        const struct can_frame &f = buf->data()->frame();
-        if (f.can_dlc != 8 || !IS_CAN_FRAME_EFF(f))
-        {
-            // not interesting frame
-            ++numUnknownFrames_;
-            return;
-        }
-        auto id = GET_CAN_FRAME_ID_EFF(f);
-        if (id != SEND_HEADER)
-        {
-            // not interesting frame
-            ++numUnknownFrames_;
-            return;
-        }
-        if (memcmp(f.data, SEND_PAYLOAD, 4) != 0)
-        {
-            // not interesting frame
-            ++numUnknownFrames_;
-            return;
-        }
-        ++numFrames_;
-        uint32_t cnt = 0;
-        memcpy(&cnt, f.data + 4, 4);
-        cnt = be32toh(cnt);
-        if (cnt == nextPacket_)
-        {
-            ++numInOrder_;
-            ++nextPacket_;
-        }
-        else if (cnt > nextPacket_)
-        {
-            numMissed_ = cnt - nextPacket_;
-            nextPacket_ = cnt + 1;
-        }
-        else
-        {
-            numOutOfOrder_++;
-            // we don't adjust next packet here
-        }
-    }
-
-    /// Gets a stats line for this input.
-    string get_stats()
-    {
-        string ret;
-        ret += StringPrintf("\tReceiver: +%d recv, +%d unk, +%d OK, +%d "
-                            "out-of-order, +%d missing\n",
-            numFrames_, numUnknownFrames_, numInOrder_, numOutOfOrder_,
-            numMissed_);
-        numFrames_ = numUnknownFrames_ = numInOrder_ = numOutOfOrder_ =
-            numMissed_ = 0;
-        return ret;
-    }
-
-public:
-    /// Number of incoming frames that we don't recognize.
-    unsigned numUnknownFrames_ = 0;
-    /// Number of successful frames received.
-    unsigned numFrames_ = 0;
-    /// Number of in-order frames.
-    unsigned numInOrder_ = 0;
-    /// Number of frames missed. These may be lost or will arrive later out of
-    /// order.
-    unsigned numMissed_ = 0;
-    /// Number of frames that arrived late / out of order.
-    unsigned numOutOfOrder_ = 0;
-
-private:
-    /// Which packet index are we expecting next.
-    uint32_t nextPacket_ = 1;
-};
-
-Link::Link()
-    : receiver {new Receiver(can_hub.get())}
-{ }
-
-/// Establishes a new connection.
-void add_link(Link *link, bool is_receiver)
-{
-    static int id = 0;
-    if (link->upstream_host)
-    {
-        connections.emplace_back(
-            new UpstreamConnectionClient(StringPrintf("upstream%d", id),
-                link->can_hub.get(), link->upstream_host, link->upstream_port));
-    }
-    else if (link->device_path)
-    {
-        connections.emplace_back(
-            new DeviceConnectionClient(StringPrintf("device%d", id),
-                link->can_hub.get(), link->device_path));
-    }
-    else
-    {
-        usage(arg0);
-    }
-    ++id;
-}
-
 /// Call this function when a packet has all its receivers confirmed.
 /// @param index packet number.
 void packet_complete(unsigned index);
@@ -331,6 +229,132 @@ private:
 
 std::map<unsigned, PacketInfo> g_packet_data;
 
+class Receiver : public CanHubPortInterface
+{
+public:
+    Receiver(CanHubFlow *hub)
+    {
+        hub->register_port(this);
+    }
+
+    void send(message_type *buf, unsigned prio) override
+    {
+        auto ts = os_get_time_monotonic();
+        auto rb = get_buffer_deleter(buf);
+        const struct can_frame &f = buf->data()->frame();
+        if (f.can_dlc != 8 || !IS_CAN_FRAME_EFF(f))
+        {
+            // not interesting frame
+            ++numUnknownFrames_;
+            return;
+        }
+        auto id = GET_CAN_FRAME_ID_EFF(f);
+        if (id != SEND_HEADER)
+        {
+            // not interesting frame
+            ++numUnknownFrames_;
+            return;
+        }
+        if (memcmp(f.data, SEND_PAYLOAD, 4) != 0)
+        {
+            // not interesting frame
+            ++numUnknownFrames_;
+            return;
+        }
+        ++numFrames_;
+        uint32_t cnt = 0;
+        memcpy(&cnt, f.data + 4, 4);
+        cnt = be32toh(cnt);
+        if (cnt == nextPacket_)
+        {
+            ++numInOrder_;
+            ++nextPacket_;
+        }
+        else if (cnt > nextPacket_)
+        {
+            numMissed_ = cnt - nextPacket_;
+            nextPacket_ = cnt + 1;
+        }
+        else
+        {
+            numOutOfOrder_++;
+            // we don't adjust next packet here
+        }
+        auto it = g_packet_data.find(cnt);
+        if (it != g_packet_data.end())
+        {
+            long long rtt_usec = NSEC_TO_USEC(ts - it->second.confirmTs_);
+            rttUsec_.add(rtt_usec);
+            it->second.notify_reception();
+        }
+    }
+
+    /// Gets a stats line for this input.
+    string get_stats()
+    {
+        string ret;
+        ret += StringPrintf("\tReceiver: +%d recv, +%d unk, +%d OK, +%d "
+                            "out-of-order, +%d missing",
+            numFrames_, numUnknownFrames_, numInOrder_, numOutOfOrder_,
+            numMissed_);
+        numFrames_ = numUnknownFrames_ = numInOrder_ = numOutOfOrder_ =
+            numMissed_ = 0;
+
+        ret += StringPrintf("|RTT %.1f msec +- %.1f\n",
+            rttUsec_.favg()/1000, rttUsec_.stddev() / 1000);
+
+        rttUsec_.clear();
+        return ret;
+    }
+
+public:
+    /// Number of incoming frames that we don't recognize.
+    unsigned numUnknownFrames_ = 0;
+    /// Number of successful frames received.
+    unsigned numFrames_ = 0;
+    /// Number of in-order frames.
+    unsigned numInOrder_ = 0;
+    /// Number of frames missed. These may be lost or will arrive later out of
+    /// order.
+    unsigned numMissed_ = 0;
+    /// Number of frames that arrived late / out of order.
+    unsigned numOutOfOrder_ = 0;
+
+private:
+    /// Which packet index are we expecting next.
+    uint32_t nextPacket_ = 1;
+    /// Statistics about the RTT of the packet (from send-confirm to
+    /// receive).
+    Stats rttUsec_;
+};
+
+/// Establishes a new connection.
+void add_link(Link *link, bool is_receiver)
+{
+    static int id = 0;
+    if (link->upstream_host)
+    {
+        connections.emplace_back(
+            new UpstreamConnectionClient(StringPrintf("upstream%d", id),
+                link->can_hub.get(), link->upstream_host, link->upstream_port));
+    }
+    else if (link->device_path)
+    {
+        connections.emplace_back(
+            new DeviceConnectionClient(StringPrintf("device%d", id),
+                link->can_hub.get(), link->device_path));
+    }
+    else
+    {
+        usage(arg0);
+    }
+    if (is_receiver)
+    {
+        link->receiver.reset(new Receiver(link->can_hub.get()));
+    }
+    ++id;
+}
+
 void packet_complete(unsigned index)
 {
     g_packet_data.erase(index);
@@ -339,9 +363,9 @@ void packet_complete(unsigned index)
 struct SendPacketRequest
 {
     /// Packet number to output (sequence number).
-    unsigned index;
+    unsigned index = 0;
     /// When was this packet generated by the timer.
-    uint64_t generateTsNsec;
+    uint64_t generateTsNsec = 0;
 };
 
 /// Implements the state machine to acquire a buffer and send an outgoing
@@ -355,6 +379,17 @@ public:
 
     Action entry() override
     {
+        auto it = g_packet_data.find(message()->data()->index);
+        if (it != g_packet_data.end())
+        {
+            LOG(FATAL,
+                "duplicate packet index %d, %d, old send Ts %lld, new send ts "
+                "%" PRId64,
+                message()->data()->index, it->second.index_,
+                it->second.timerTs_, message()->data()->generateTsNsec);
+            DIE("duplicate packet");
+        }
+        HASSERT(it == g_packet_data.end());
         pinfo_ = &g_packet_data[message()->data()->index];
         pinfo_->index_ = message()->data()->index;
         pinfo_->flowTs_ = os_get_time_monotonic();
@@ -365,7 +400,8 @@ public:
 
     Action have_buffer()
     {
-        BufferType *b = get_allocation_result(output_port.can_hub.get());
+        g_pending_buffers++;
+        auto *b = get_allocation_result(output_port.can_hub.get());
         b->set_done(&pinfo_->bn_);
         auto &f = *b->data()->mutable_frame();
         SET_CAN_FRAME_ID_EFF(f, SEND_HEADER);
@@ -374,16 +410,16 @@ public:
         uint32_t idx_be = htobe32(pinfo_->index_);
         memcpy(f.data + 4, &idx_be, 4);
         b->data()->skipMember_ = nullptr;
-        output_port.can_hub->send(b);
-        pinfo_->sendTs_ = os_get_time_monotonic();
         pinfo_->pendingReceivers_ = std::max(0, g_num_live_links - 1);
         g_num_packets_sent++;
+        pinfo_->sendTs_ = os_get_time_monotonic();
+        output_port.can_hub->send(b);
         return release_and_exit();
     }
 
 private:
-    using BufferType = CanHubFlow::buffer_type;
-    LimitedPool pool_ {sizeof(BufferType), SEND_PARALLELISM};
+    LimitedPool pool_ {
+        sizeof(Buffer<HubContainer<CanFrameContainer>>), SEND_PARALLELISM};
     PacketInfo *pinfo_;
 } g_send_flow;
 
@@ -399,6 +435,7 @@ public:
     long long timeout() override
     {
         auto *b = g_send_flow.alloc();
+        HASSERT(b);
         b->data()->index = g_next_packet++;
         b->data()->generateTsNsec = this->schedule_time();
         g_send_flow.send(b);
@@ -413,9 +450,7 @@ class StatsTimer : public ::Timer
 public:
     StatsTimer()
         : Timer(g_executor.active_timers())
-    {
-        start(SEC_TO_NSEC(1));
-    }
+    { }
 
     long long timeout() override
     {
@@ -428,12 +463,18 @@ public:
         string ret;
         ret += "Send: ";
         unsigned d = update_diff(g_next_packet, &next_packet);
-        ret += StringPrintf("+%" PRIu32 " gen", d);
+        ret += StringPrintf("|+%" PRIu32 " gen", d);
         d = update_diff(g_num_packets_accepted, &num_packets_accepted);
-        ret += StringPrintf(" +%d send complete", d);
+        ret += StringPrintf("|+%d send complete", d);
         d = update_diff(g_num_packets_all_received, &num_packets_all_received);
-        ret += StringPrintf(" +%d recv complete", d);
+        ret += StringPrintf("|+%d recv complete", d);
+        int send_q = -2;
+        ::ioctl(connections[0]->fd(), TIOCOUTQ, &send_q);
         // @todo add stats about send queues.
+        ret += StringPrintf(
+            "||sendq %d|pending buf %u| sent>acc %u| acc>recv %d|", send_q,
+            g_pending_buffers, g_num_packets_sent - g_num_packets_accepted,
+            g_num_packets_accepted - g_num_packets_all_received);
         ret += "\n";
         for (const auto &lnk : input_ports)
         {
@@ -482,14 +523,21 @@ int appl_main(int argc, char *argv[])
 {
     arg0 = argv[0];
     parse_args(argc, argv);
+    add_link(&output_port, false);
+    for (auto &l : input_ports)
+    {
+        add_link(&l, true);
+    }
 
-    g_executor.start_thread("executor_thread", 0, 5000);
+    // g_executor.start_thread("executor_thread", 0, 5000);
 
     if (pkt_per_sec > 0)
     {
         long long diff = SEC_TO_NSEC(1) / pkt_per_sec;
         pkt_gen_timer.start(diff);
     }
+    microsleep(100);
+    stats_timer.start(SEC_TO_NSEC(1));
 
     while (1)
     {
