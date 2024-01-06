691205874cd4cd05ecc6f1607c21dbaa4a9bb456
Balazs Racz <balazs.racz@gmail.com>
2024-01-06 22:37:31 +0100

    Uses the user address decoding function in the DccDebug code.
    Adjusts the dccdebug for the new RCN-213 and DCC 9.2.1 standard encoding.

--- src/dcc/DccDebug.cxx
+++ src/dcc/DccDebug.cxx
@@ -237,23 +237,13 @@ string packet_to_string(const DCCPacket &pkt, bool bin_payload)
         bool is_activate = cmd & 1;
         cmd >>= 1;
         accy_address |= ((~cmd) & 0b111) << 9;
-        unsigned user_address = accy_address >> 1;
-        if (user_address < 4)
-        {
-            user_address += 2044;
-        }
-        else
-        {
-            user_address -= 3;
-        }
-        string user_string = accy_address >= 4094
-            ? "broadcast"
-            : StringPrintf("%u", user_address);
+        unsigned user_address =
+            Defs::accy_address_binary_to_user(accy_address >> 1);
         const char *n_r =
             accy_address & 1 ? "closed/normal/on" : "thrown/reverse/off";
         const char* a_d = is_activate ? "activate" : "deactivate";
-        options += StringPrintf(" Accy %u (user %s %s) %s", accy_address,
-            user_string.c_str(), n_r, a_d);
+        options += StringPrintf(
+            " Accy %u (user %u %s) %s", accy_address, user_address, n_r, a_d);
     }
     else if (is_unknown_packet || is_svc_packet)
     {
