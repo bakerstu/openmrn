76866e60e1754364fcb7a70d4f44d99d98ba38ef
Balazs Racz <balazs.racz@gmail.com>
2024-07-31 21:08:10 +0200

    Makes bootloader writes shorter to reduce the memory pressure.

--- applications/bootloader/targets/nonos.xtensa.esp8266.bracz-deadrail-proto/NodeId.cxx
+++ applications/bootloader/targets/nonos.xtensa.esp8266.bracz-deadrail-proto/NodeId.cxx
@@ -6,7 +6,7 @@ const openlcb::NodeID NODE_ID = 0x050101011400ULL | NODEID_LOW_BITS;
 extern const uint16_t DEFAULT_ALIAS;
 const uint16_t DEFAULT_ALIAS = 0x400 | NODEID_LOW_BITS;
 
-#define WRITE_BUFFER_SIZE 4096
+#define WRITE_BUFFER_SIZE 1024
 #define BOOTLOADER_STREAM
 //#define BOOTLOADER_DATAGRAM
 #include "openlcb/Bootloader.hxx"
