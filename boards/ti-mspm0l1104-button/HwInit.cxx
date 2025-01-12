801c063b06394af0f8e343eed804af798f511ce2
Balazs Racz <balazs.racz@gmail.com>
2025-01-12 14:15:48 +0100

    First working blink_raw application for the MSPM0.

--- boards/ti-mspm0l1104-button/HwInit.cxx
+++ boards/ti-mspm0l1104-button/HwInit.cxx
@@ -6,15 +6,24 @@
 #include <ti/driverlib/m0p/dl_core.h>
 
 #include "utils/blinker.h"
+#include "freertos_drivers/ti/MSPM0SpiPixelStrip.hxx"
 
 #define POWER_STARTUP_DELAY (16)
 
 #define CPUCLK_FREQ 32000000
 
+uint8_t pix[3] = {3, 3, 3};
+
+SpiPixelStrip strip0(SPI0, 1, pix);
+
 extern "C" {
 
-void resetblink(uint32_t)
-{ }
+void resetblink(uint32_t p)
+{
+    pix[0] = p ? 30 : 3;
+    strip0.update_sync();
+}
+
 void setblink(uint32_t p)
 {
     resetblink(p);
@@ -37,11 +46,49 @@ extern uint64_t current_time_msec()
 void hw_set_to_safe(void)
 { }
 
+static const DL_SPI_Config gSPI_0_config = {
+    .mode        = DL_SPI_MODE_CONTROLLER,
+    .frameFormat = DL_SPI_FRAME_FORMAT_MOTO4_POL0_PHA0,
+    .parity      = DL_SPI_PARITY_NONE,
+    .dataSize    = DL_SPI_DATA_SIZE_15,
+    .bitOrder    = DL_SPI_BIT_ORDER_LSB_FIRST,
+    .chipSelectPin = DL_SPI_CHIP_SELECT_NONE,
+};
+
+static const DL_SPI_ClockConfig gSPI_0_clockConfig = {
+    .clockSel    = DL_SPI_CLOCK_BUSCLK,
+    .divideRatio = DL_SPI_CLOCK_DIVIDE_RATIO_1
+};
+
+void setup_spi() {
+    DL_SPI_setClockConfig(SPI0, (DL_SPI_ClockConfig *) &gSPI_0_clockConfig);
+    DL_SPI_init(SPI0, (DL_SPI_Config *) &gSPI_0_config);
+    /*
+     * Set the bit rate clock divider to generate the serial output clock
+     *     outputBitRate = (spiInputClock) / ((1 + SCR) * 2)
+     *     500000 = (32000000)/((1 + 31) * 2)
+     *
+     * Desired bit time is 0.27 to 0.4 usec per bit, which is 3.7MHz to 2.5MHz.
+     * That is 12.8 to 8.64 multiplier for a 32MHz processor. We aim for
+     * multiplier 10, which is SCR of 4.
+     *
+     *     (32000000)/((1 + 4) * 2) = 3.2MHz. That is 0.312 usec per bit.
+     *
+     * A 0 bit will be 0.31us high 0.62us low.
+     * A 1 bit will be 0.62us high 0.31us low.
+     */
+    DL_SPI_setBitRateSerialClockDivider(SPI0, 4);
+    DL_SPI_enable(SPI0);
+}
+
 void hw_preinit(void)
 {
     DL_GPIO_reset(GPIOA);
-
     DL_GPIO_enablePower(GPIOA);
+
+    DL_SPI_reset(SPI0);
+    DL_SPI_enablePower(SPI0);
+    
     delay_cycles(POWER_STARTUP_DELAY);
 
     // SYSCFG_DL_GPIO_init(); // empty
@@ -58,6 +105,14 @@ void hw_preinit(void)
      * enables the interrupt, and starts the SysTick Timer
      */
     DL_SYSTICK_config(CPUCLK_FREQ / 1000);
+
+    setup_spi();
+
+    /// Pinmux setup.
+    DL_GPIO_initPeripheralOutputFunctionFeatures(
+        IOMUX_PINCM19 /*PA18*/, IOMUX_PINCM19_PF_SPI0_PICO,
+        DL_GPIO_INVERSION_DISABLE, DL_GPIO_RESISTOR_PULL_DOWN,
+        DL_GPIO_DRIVE_STRENGTH_LOW, DL_GPIO_HIZ_DISABLE);
 }
 
 extern int appl_main(int argc, char *argv[]);
