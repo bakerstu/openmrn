925ff25203de2bf5ce6a8a6bbbf5bbd8fbbf7cb4
Stuart Baker <stuart_w_baker@ymail.com>
2025-02-06 14:21:30 -0500

    Refactor the code modules into their own files.

---
+++ src/traction_modem/Train.hxx
@@ -0,0 +1,239 @@
+/** @copyright
+ * Copyright (c) 2025, Stuart Baker
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file Train.hxx
+ *
+ * Implements a Traction Modem Train.
+ *
+ * @author Stuart Baker
+ * @date 6 Feb 2025
+ */
+
+#ifndef _TRACTION_MODEM_TRAIN_HXX_
+#define _TRACTION_MODEM_TRAIN_HXX_
+
+/// @todo Need to prune out the "hardware.hxx" dependencies from this file.
+///       These need to be dispatched to hardware specific code somehow.
+#include "traction_modem/TractionModemDefs.hxx"
+#if !defined(GTEST)
+#include "hardware.hxx"
+#endif
+
+#include "executor/Dispatcher.hxx"
+#include "openlcb/TrainInterface.hxx"
+#include "traction_modem/TxFlow.hxx"
+#include "traction_modem/RxFlow.hxx"
+
+namespace traction_modem
+{
+
+class ModemTrain : public openlcb::TrainImpl
+{
+public:
+    ModemTrain(Service *service)
+        : txFlow_(service)
+        , rxFlow_(service)
+        , dispatcher_(service)
+        , isActive_(false)
+    {
+        rxFlow_.set_listener(&cvSpace_);
+    }
+
+    void start(int uart_fd)
+    {
+        txFlow_.start(uart_fd);
+        rxFlow_.start(uart_fd);
+    }
+
+    /// Set the active state of the wireless control.
+    /// @param is_active true if under wireless control, else false
+    void set_is_active(bool is_active)
+    {
+        if (isActive_ != is_active)
+        {
+            isActive_ = is_active;
+            send_packet(Defs::get_wireless_present_payload(isActive_));
+        }
+    }
+
+    openlcb::MemorySpace *get_cv_space()
+    {
+        return &cvSpace_;
+    }
+
+    // ====== Train interface =======
+
+    void set_speed(openlcb::SpeedType speed) override
+    {
+        set_is_active(true);
+        inEStop_ = false;
+        lastSpeed_ = speed;
+        send_packet(Defs::get_speed_set_payload(speed));
+    }
+
+    /** Returns the last set speed of the locomotive. */
+    openlcb::SpeedType get_speed() override
+    {
+        return lastSpeed_;
+    }
+
+    /** Sets the train to emergency stop. */
+    void set_emergencystop() override
+    {
+        inEStop_ = true;
+        lastSpeed_.set_mph(0); // keeps direction
+        send_packet(Defs::get_estop_payload());
+    }
+
+    bool get_emergencystop() override
+    {
+        return inEStop_;
+    }
+
+    /** Sets the value of a function.
+     * @param address is a 24-bit address of the function to set. For legacy DCC
+     * locomotives, see @ref TractionDefs for the address definitions (0=light,
+     * 1-28= traditional function buttons).
+     * @param value is the function value. For binary functions, any non-zero
+     * value sets the function to on, zero sets it to off.*/
+    void set_fn(uint32_t address, uint16_t value) override
+    {
+        set_is_active(true);
+        send_packet(Defs::get_fn_set_payload(address, value));
+        /// @todo The following switch statement is for hardware testing only.
+        ///       In production software, the main MCU should have the on/off
+        ///       logical by "output" number, and not by function number. The
+        ///       main MCU should instruct the modem of the activity state.
+        switch (address)
+        {
+            default:
+                break;
+#if !defined(GTEST)
+            case 0:
+                if (lastSpeed_.direction() == openlcb::Velocity::REVERSE)
+                {
+                    LOGIC_F0R_Pin::set(value ? 1 : 0);
+                }
+                break;
+            case 1:
+                LOGIC_F1_Pin::set(value ? 1 : 0);
+                break;
+            case 2:
+                LOGIC_F2_Pin::set(value ? 1 : 0);
+                break;
+            case 3:
+                LOGIC_F3_Pin::set(value ? 1 : 0);
+                break;
+            case 4:
+                LOGIC_F4_Pin::set(value ? 1 : 0);
+                break;
+            // F5 and F6 also overwritten with input1 and input2 values when
+            // button 7 or 8 are pressed
+            case 5:
+                LOGIC_F5_Pin::set(value ? 1 : 0);
+                break;
+            case 6:
+                LOGIC_F6_Pin::set(value ? 1 : 0);
+                break;
+#endif // !defined(GTEST)
+// Commented out since these pins cannot be defined as both inputs
+// and outputs.
+#if 0
+            case 7:
+                LOGIC_F7_Pin::set(value ? 1 : 0);
+                break;
+            case 8:
+                LOGIC_F8_Pin::set(value ? 1 : 0);
+                break;
+#endif
+#if !defined(GTEST)
+            case 7:
+            {
+                bool input1 = INPUT1_Pin::get();
+                LOGIC_F5_Pin::set(INPUT2_Pin::get());
+                LOG(INFO, "INPUT1: %u", input1);
+                break;
+            }
+            case 8:
+            {
+                bool input2 = INPUT2_Pin::get();
+                LOGIC_F6_Pin::set(INPUT2_Pin::get());
+                LOG(INFO, "INPUT2: %u", input2);
+                break;
+            }
+#endif // !defined(GTEST)
+        }
+    }
+
+    /** @returns the value of a function. */
+    uint16_t get_fn(uint32_t address) override
+    {
+        /// @todo Need to implement this.
+        return 0;
+    }
+
+    uint32_t legacy_address() override
+    {
+        /// @todo what should this be?
+        return 883;
+    }
+
+    /** @returns the type of legacy protocol in use. */
+    dcc::TrainAddressType legacy_address_type() override
+    {
+        return dcc::TrainAddressType::DCC_LONG_ADDRESS;
+    }
+
+private:
+    inline void send_packet(Defs::Payload p)
+    {
+        auto *b = txFlow_.alloc();
+        b->data()->payload = std::move(p);
+        txFlow_.send(b);
+    }
+
+    bool isRunning_ = false;
+    /// UART fd to send traffic to the device.
+    int fd_;
+
+    openlcb::SpeedType lastSpeed_ = 0.0;
+    /// True if the last set was estop, false if it was a speed.
+    bool inEStop_ = false;
+
+    /// Handles sending message frames.
+    TxFlow txFlow_;
+    /// Handles receiving message frames.
+    RxFlow rxFlow_;
+    /// Handles incoming messages fro the RX Flow.
+    DispatchFlow<Buffer<Message>, 2> dispatcher_;
+
+    CvSpace cvSpace_{&txFlow_};
+    bool isActive_;
+};
+
+} // namespace traction_modem
+
+#endif // _TRACTION_MODEM_TRAIN_HXX_
---
+++ src/traction_modem/TxFlow.hxx
@@ -0,0 +1,102 @@
+/** @copyright
+ * Copyright (c) 2025, Stuart Baker
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file TxFlow.hxx
+ *
+ * Implements the message flow for transmission.
+ *
+ * @author Stuart Baker
+ * @date 6 Feb 2025
+ */
+
+#ifndef _TRACTION_MODEM_TXFLOW_HXX_
+#define _TRACTION_MODEM_TXFLOW_HXX_
+
+#include "traction_modem/Message.hxx"
+#include "traction_modem/TractionModem.hxx"
+
+namespace traction_modem
+{
+
+/// Object responsible for writing messages to the modem interface.
+class TxFlow : public MessageFlowBase
+{
+public:
+    /// Constructor.
+    /// @param service service that the flow is bound to
+    TxFlow(Service *service)
+        : MessageFlowBase(service)
+    {
+        LOG(INFO, "[ModemTx] constructor");
+    }
+
+    /// Bind an interface to the flow to start transmitting to.
+    /// @param fd interface to transmit the messages on
+    void start(int fd)
+    {
+        LOG(INFO, "[ModemTx] fd");
+        fd_ = fd;
+    }
+
+    /// Entry point to the state flow for incoming Messages to transmit.
+    /// @return next state write_complete
+    Action entry() override
+    {
+        if (fd_ < 0)
+        {
+            LOG(INFO, "[ModemTx] no uart");
+            return release_and_exit();
+        }
+        LOG(INFO, "[ModemTx] msg len %d",
+            (int)message()->data()->payload.size());
+        return write_repeated(&helper_, fd_, message()->data()->payload.data(),
+            message()->data()->payload.size(), STATE(write_complete));
+    }
+
+    /// Finish up the write and exit
+    /// @return release_and_exit
+    Action write_complete()
+    {
+        unsigned len = message()->data()->payload.size();
+        unsigned num_sent = len - helper_.remaining_;
+        const uint8_t *d = (const uint8_t *)message()->data()->payload.data();
+        LOG(INFO, "[ModemTx] sent E%d len %u done %u %08x %04x...",
+            helper_.hasError_, len, num_sent, *(unsigned *)(d + 0),
+            (unsigned)be32toh(*(uint16_t *)(d + 4)));
+        /// @TODO check for error
+        return release_and_exit();
+    }
+
+private:
+    /// Helper for performing the writes.
+    StateFlowSelectHelper helper_ {this};
+    /// Interface fd.
+    int fd_ = -1;
+};
+
+} // namespace traction_modem
+
+#endif // _TRACTION_MODEM_TXFLOW_HXX_
