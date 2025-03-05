8375add60338d61c76122c5760160f08edb7cb39
Balazs Racz <balazs.racz@gmail.com>
2025-03-05 23:04:33 +0100

    RailcomImpl: allows overriding set_feedback_key.

--- src/freertos_drivers/common/RailcomImpl.hxx
+++ src/freertos_drivers/common/RailcomImpl.hxx
@@ -222,13 +222,12 @@ public:
         }
     }
 
-private:
+protected:
     void set_feedback_key(uint32_t key) OVERRIDE
     {
         feedbackKey_ = key;
     }
 
-protected:
     /** Takes a new empty packet at the front of the queue, fills in feedback
      * key and channel information.
      * @param channel is which channel to set the packet for.
