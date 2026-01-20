18aed55715c0284b134f7e04ea648512bd30035d
Balazs Racz <balazs.racz@gmail.com>
2026-01-20 10:24:18 +0100

    Adds client for receiving railcom occupancy events (#892)
    Adds an event handler for receiving railcom events from the RailcomBroadcastFlow. This provides entry and exit messages when locomotives enter and leave a block.
    
    ===
    
    * Add RailcomBroadcastClient to openlcb
    
    This class monitors a Railcom broadcast event range and tracks which locomotives are reported in the block.
    It implements SimpleEventHandler to listen for Event Reports and Producer Identified messages.
    It handles identifying global and consumer requests appropriately.
    Includes unit tests.
    
    * Fix style
    
    * Adds sequence number feature
    
    * Updates code to interpret event 0 as "unoccupied".
    
    * Adds support for query of current state.
    
    * Update tests for query feature.
    
    * Fixes typos and comments.

---
+++ src/openlcb/RailcomBroadcastClient.cxx
@@ -0,0 +1,203 @@
+/** \copyright
+ * Copyright (c) 2026, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file RailcomBroadcastClient.cxx
+ *
+ * Client for receiving Railcom broadcast messages about trains in a block.
+ *
+ * @author Balazs Racz
+ * @date 15 Jan 2026
+ */
+
+#include "openlcb/RailcomBroadcastClient.hxx"
+
+#include <algorithm>
+
+#include "openlcb/Defs.hxx"
+#include "openlcb/EventHandler.hxx"
+
+namespace openlcb
+{
+
+RailcomBroadcastClient::RailcomBroadcastClient(
+    Node *node, uint64_t railcom_event_base)
+    : node_(node)
+{
+    railcomEventBase_ = railcom_event_base & ~0xFFFFULL;
+    uint64_t registered_base = railcomEventBase_;
+    // Register for 65536 events (16 bits)
+    unsigned mask = EventRegistry::align_mask(&registered_base, 65536);
+    EventRegistry::instance()->register_handler(
+        EventRegistryEntry(this, registered_base), mask);
+}
+
+RailcomBroadcastClient::~RailcomBroadcastClient()
+{
+    EventRegistry::instance()->unregister_handler(this);
+}
+
+bool RailcomBroadcastClient::is_our_event(uint64_t event_id) const
+{
+    // Check if the event ID is within the range [base, base + 0xFFFF]
+    return (event_id & ~0xFFFFULL) == railcomEventBase_;
+}
+
+static openlcb::NodeID node_id_from_event(uint64_t event) {
+    uint16_t lo = event & 0xFFFF;
+    if (!lo)
+    {
+        return 0;
+    }
+
+    // Extract address from bottom 14 bits
+    uint16_t address = event & 0x3FFF;
+    NodeID id = 0;
+    if (lo & 0x8000)
+    {
+        id = TractionDefs::train_node_id_from_legacy(
+            dcc::TrainAddressType::DCC_LONG_ADDRESS, address);
+    }
+    else
+    {
+        id = TractionDefs::train_node_id_from_legacy(
+            dcc::TrainAddressType::DCC_SHORT_ADDRESS, address & 127);
+    }
+    return id;
+}
+
+void RailcomBroadcastClient::handle_event_report(
+    const EventRegistryEntry &registry_entry, EventReport *event,
+    BarrierNotifiable *done)
+{
+    AutoNotify an(done);
+    if (!is_our_event(event->event))
+    {
+        return;
+    }
+
+    NodeID id = node_id_from_event(event->event);
+    add_loco(id);
+}
+
+void RailcomBroadcastClient::add_loco(NodeID id)
+{
+    if (!id)
+    {
+        // Unoccupied.
+        if (locos_.size())
+        {
+            locos_.clear();
+            seq_++;
+        }
+        return;
+    }
+    bool found = false;
+    for (auto existing_id : locos_)
+    {
+        if (existing_id == id)
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        locos_.push_back(id);
+        seq_++;
+    }
+}
+
+void RailcomBroadcastClient::del_loco(NodeID id)
+{
+    auto it = std::remove(locos_.begin(), locos_.end(), id);
+    if (it != locos_.end())
+    {
+        locos_.erase(it, locos_.end());
+        seq_++;
+    }
+}
+
+void RailcomBroadcastClient::handle_producer_identified(
+    const EventRegistryEntry &registry_entry, EventReport *event,
+    BarrierNotifiable *done)
+{
+    if (!is_our_event(event->event))
+    {
+        return done->notify();
+    }
+
+    NodeID id = node_id_from_event(event->event);
+    if (event->state == EventState::INVALID)
+    {
+        del_loco(id);
+    }
+    else if (event->state == EventState::VALID)
+    {
+        add_loco(id);
+    }
+
+    done->notify();
+}
+
+void RailcomBroadcastClient::handle_identify_global(
+    const EventRegistryEntry &registry_entry, EventReport *event,
+    BarrierNotifiable *done)
+{
+    if (event->dst_node && event->dst_node != node_)
+    {
+        return done->notify();
+    }
+
+    // We are a consumer of these events.
+    uint64_t range = EncodeRange(railcomEventBase_, 65536);
+    event->event_write_helper<1>()->WriteAsync(node_,
+        Defs::MTI_CONSUMER_IDENTIFIED_RANGE, WriteHelper::global(),
+        eventid_to_buffer(range), done->new_child());
+    // Queries current state from the producer.
+    event->event_write_helper<2>()->WriteAsync(node_,
+        Defs::MTI_PRODUCER_IDENTIFY, WriteHelper::global(),
+        eventid_to_buffer(railcomEventBase_), done->new_child());
+    done->maybe_done();
+}
+
+void RailcomBroadcastClient::handle_identify_consumer(
+    const EventRegistryEntry &registry_entry, EventReport *event,
+    BarrierNotifiable *done)
+{
+    if (!is_our_event(event->event))
+    {
+        return done->notify();
+    }
+
+    // Report unknown state for specific events queried
+    event->event_write_helper<1>()->WriteAsync(node_,
+        Defs::MTI_CONSUMER_IDENTIFIED_UNKNOWN, WriteHelper::global(),
+        eventid_to_buffer(event->event), done->new_child());
+    done->maybe_done();
+}
+
+} // namespace openlcb
---
+++ src/openlcb/RailcomBroadcastClient.hxx
@@ -0,0 +1,118 @@
+/** \copyright
+ * Copyright (c) 2026, Balazs Racz
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are  permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \file RailcomBroadcastClient.hxx
+ *
+ * Client for receiving Railcom broadcast messages about trains in a block.
+ *
+ * @author Balazs Racz
+ * @date 15 Jan 2026
+ */
+
+#ifndef _OPENLCB_RAILCOMBROADCASTCLIENT_HXX_
+#define _OPENLCB_RAILCOMBROADCASTCLIENT_HXX_
+
+#include "openlcb/EventHandlerTemplates.hxx"
+#include "openlcb/TractionDefs.hxx"
+#include <vector>
+
+namespace openlcb
+{
+
+/// Client class for receiving event based updates for the occupancy of a
+/// railcom block. Consumes events for entry and exit reports from the railcom
+/// broadcast flow, and maintains a list of locomotives that are currently in
+/// the block.
+class RailcomBroadcastClient : public SimpleEventHandler
+{
+public:
+    /** Constructor.
+     * @param node The virtual node to send messages from (for identify
+     * responses).
+     * @param railcom_event_base The base event ID for the Railcom broadcast
+     * range. The bottom 16 bits will be cleared.
+     */
+    RailcomBroadcastClient(Node *node, uint64_t railcom_event_base);
+
+    virtual ~RailcomBroadcastClient();
+
+    /** Returns the list of locomotives currently reported in the block.
+     * The NodeIDs are computed using the legacy DCC long address format.
+     */
+    const std::vector<NodeID> &current_locos() const
+    {
+        return locos_;
+    }
+
+    /// The number returned by this function is changed every time the
+    /// current_locos() array is different. This allows a user to keep track of
+    /// whether their state is dirty or not.
+    uint16_t seq()
+    {
+        return seq_;
+    }
+
+    /// @return the event base which we are listening to.
+    uint64_t event_base()
+    {
+        return railcomEventBase_;
+    }
+
+    void handle_event_report(const EventRegistryEntry &registry_entry,
+        EventReport *event, BarrierNotifiable *done) override;
+
+    void handle_producer_identified(const EventRegistryEntry &registry_entry,
+        EventReport *event, BarrierNotifiable *done) override;
+
+    void handle_identify_global(const EventRegistryEntry &registry_entry,
+        EventReport *event, BarrierNotifiable *done) override;
+
+    void handle_identify_consumer(const EventRegistryEntry &registry_entry,
+        EventReport *event, BarrierNotifiable *done) override;
+
+private:
+    /// Checks if the given event ID falls within our monitored range.
+    bool is_our_event(uint64_t event_id) const;
+
+    /// Adds a locomotive to the locos_ array.
+    void add_loco(NodeID n);
+    /// Removes a locomotive from the locos_ array.
+    void del_loco(NodeID n);
+    
+    /// Sequence number for data version in the locos_ array.
+    uint16_t seq_{0};
+    /// OpenLCB node on which to export the consumer.
+    Node *node_;
+    /// Event ID with bottom 16 bits as zero. We are registered for this event
+    /// range.
+    uint64_t railcomEventBase_;
+    /// Locomotives that are currently active in this range.
+    std::vector<NodeID> locos_;
+};
+
+} // namespace openlcb
+
+#endif // _OPENLCB_RAILCOMBROADCASTCLIENT_HXX_
