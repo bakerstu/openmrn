108e42e07eacad420a3b62339f0c32caff27c806
Balazs Racz <balazs.racz@gmail.com>
2026-09-08 22:32:27 +0200

    Fix bugs related to groups with offsets.
    Empty groups and hidden groups did not correctly take the Offset() argument
    into account. This made it difficult to have negative jumps in the CDI, since
    negative jumps are easiest to represent with an empty or a hidden group.

--- src/openlcb/ConfigRenderer.hxx
+++ src/openlcb/ConfigRenderer.hxx
@@ -416,7 +416,9 @@ public:
 
     template <typename... Args> void render_cdi(string *s, Args... args) const
     {
-        *s += StringPrintf("<group offset='%d'/>\n", size_);
+        GroupConfigOptions opts(args...);
+        int ofs = size_ + opts.offset();
+        *s += StringPrintf("<group offset='%d'/>\n", ofs);
     }
 
 private:
@@ -446,7 +448,7 @@ public:
             if (!opts.is_segment())
             {
                 EmptyGroupConfigRenderer(Body::size() * replication_)
-                    .render_cdi(s);
+                    .render_cdi(s, opts);
             }
             return;
         }
